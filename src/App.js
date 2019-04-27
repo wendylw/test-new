bbb73965ebb0a19159f91f6c69045ed1b89d20c6
wendylw <wendylw2013@gmail.com>
2019-04-27 13:29:43 +0800

    UI: Update order list

--- src/App.js
+++ src/App.js
@@ -283,7 +283,7 @@ class App extends Component {
         <section className="table-ordering__order hide">
           <header className="header boder-botton__divider flex flex-middle flex-space-between">
             <figure className="header__image-container text-middle">
-              <img src=""></img>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
             </figure>
             <h2 className="header__title font-weight-bold text-middle">Order 48 Items</h2>
             <button className="warning__button">
