4ebe2f22b127c20fbf60bb00420001764bb67e26
wendylw <wendylw2013@gmail.com>
2019-04-27 13:24:49 +0800

    UI: order page

--- src/App.js
+++ src/App.js
@@ -293,12 +293,162 @@ class App extends Component {
               <span className="warning__label text-middle">Clear All</span>
             </button>
           </header>
+          <div className="list__container">
+            <ul className="list">
+              <li className="item boder-botton__divider flex flex-top">
+                <figure className="item__image-container">
+                  <img src=""></img>
+                </figure>
+                <div className="item__content flex flex-middle flex-space-between">
+                  <div className="item__detail">
+                    <summary className="item__title font-weight-bold">Smoked Duck Plate</summary>
+                    <p className="item__description">Option 1, Option 2</p>
+                    <span className="gray-font-opacity">RM 25.80</span>
+                  </div>
+                  <div className="item__cart-ctrl is-minuts flex flex-middle flex-space-between">
+                    <button className="cart__ctrl cart__minuts">
+                      <i className="cart__icon"></i>
+                    </button>
+                    <span className="font-weight-bold">3</span>
+                    <button className="cart__ctrl cart__add">
+                      <i className="cart__icon"></i>
+                    </button>
+                  </div>
+                </div>
+              </li>
+              <li className="item boder-botton__divider flex flex-top">
+                <figure className="item__image-container">
+                  <img src=""></img>
+                </figure>
+                <div className="item__content flex flex-middle flex-space-between">
+                  <div className="item__detail">
+                    <summary className="item__title font-weight-bold">Smoked Duck Plate</summary>
+                    <p className="item__description">Option 1, Option 2</p>
+                    <span className="gray-font-opacity">RM 25.80</span>
+                  </div>
+                  <div className="item__cart-ctrl is-minuts flex flex-middle flex-space-between">
+                    <button className="cart__ctrl cart__minuts">
+                      <i className="cart__icon"></i>
+                    </button>
+                    <span className="font-weight-bold">3</span>
+                    <button className="cart__ctrl cart__add">
+                      <i className="cart__icon"></i>
+                    </button>
+                  </div>
+                </div>
+              </li>
+              <li className="item boder-botton__divider flex flex-top">
+                <figure className="item__image-container">
+                  <img src=""></img>
+                </figure>
+                <div className="item__content flex flex-middle flex-space-between">
+                  <div className="item__detail">
+                    <summary className="item__title font-weight-bold">Smoked Duck Plate</summary>
+                    <p className="item__description">Option 1, Option 2</p>
+                    <span className="gray-font-opacity">RM 25.80</span>
+                  </div>
+                  <div className="item__cart-ctrl is-minuts flex flex-middle flex-space-between">
+                    <button className="cart__ctrl cart__minuts">
+                      <i className="cart__icon"></i>
+                    </button>
+                    <span className="font-weight-bold">3</span>
+                    <button className="cart__ctrl cart__add">
+                      <i className="cart__icon"></i>
+                    </button>
+                  </div>
+                </div>
+              </li>
+              <li className="item boder-botton__divider flex flex-top">
+                <figure className="item__image-container">
+                  <img src=""></img>
+                </figure>
+                <div className="item__content flex flex-middle flex-space-between">
+                  <div className="item__detail">
+                    <summary className="item__title font-weight-bold">Smoked Duck Plate</summary>
+                    <p className="item__description">Option 1, Option 2</p>
+                    <span className="gray-font-opacity">RM 25.80</span>
+                  </div>
+                  <div className="item__cart-ctrl is-minuts flex flex-middle flex-space-between">
+                    <button className="cart__ctrl cart__minuts">
+                      <i className="cart__icon"></i>
+                    </button>
+                    <span className="font-weight-bold">3</span>
+                    <button className="cart__ctrl cart__add">
+                      <i className="cart__icon"></i>
+                    </button>
+                  </div>
+                </div>
+              </li>
+              <li className="item boder-botton__divider flex flex-top">
+                <figure className="item__image-container">
+                  <img src=""></img>
+                </figure>
+                <div className="item__content flex flex-middle flex-space-between">
+                  <div className="item__detail">
+                    <summary className="item__title font-weight-bold">Smoked Duck Plate</summary>
+                    <p className="item__description">Option 1, Option 2</p>
+                    <span className="gray-font-opacity">RM 25.80</span>
+                  </div>
+                  <div className="item__cart-ctrl is-minuts flex flex-middle flex-space-between">
+                    <button className="cart__ctrl cart__minuts">
+                      <i className="cart__icon"></i>
+                    </button>
+                    <span className="font-weight-bold">3</span>
+                    <button className="cart__ctrl cart__add">
+                      <i className="cart__icon"></i>
+                    </button>
+                  </div>
+                </div>
+              </li>
+              <li className="item boder-botton__divider flex flex-top">
+                <figure className="item__image-container">
+                  <img src=""></img>
+                </figure>
+                <div className="item__content flex flex-middle flex-space-between">
+                  <div className="item__detail">
+                    <summary className="item__title font-weight-bold">Smoked Duck Plate</summary>
+                    <p className="item__description">Option 1, Option 2</p>
+                    <span className="gray-font-opacity">RM 25.80</span>
+                  </div>
+                  <div className="item__cart-ctrl is-minuts flex flex-middle flex-space-between">
+                    <button className="cart__ctrl cart__minuts">
+                      <i className="cart__icon"></i>
+                    </button>
+                    <span className="font-weight-bold">3</span>
+                    <button className="cart__ctrl cart__add">
+                      <i className="cart__icon"></i>
+                    </button>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <section className="billing">
+            <ul className="billing__list">
+              <li className="billing__item flex flex-middle flex-space-between">
+                <label className="gray-font-opacity">Subtotal</label>
+                <span className="gray-font-opacity">87.30</span>
+              </li>
+              <li className="billing__item flex flex-middle flex-space-between">
+                <label className="gray-font-opacity">Service Charge 10%</label>
+                <span className="gray-font-opacity">8.70</span>
+              </li>
+              <li className="billing__item flex flex-middle flex-space-between">
+                <label className="gray-font-opacity">SST 6%</label>
+                <span className="gray-font-opacity">5.20</span>
+              </li>
+              <li className="billing__item flex flex-middle flex-space-between">
+                <label className="font-weight-bold">Total</label>
+                <span className="font-weight-bold">RM 101.20</span>
+              </li>
+            </ul>
+          </section>
           <footer className="footer-operation grid flex flex-middle flex-space-between">
             <div className="footer-operation__item width-1-3">
-              <button className="button button__fill button__block dark">Back</button>
+              <button className="billing__button button button__fill button__block dark font-weight-bold">Back</button>
             </div>
             <div className="footer-operation__item width-2-3">
-              <button className="button button__fill button__block">Pay</button>
+              <button className="billing__button button button__fill button__block font-weight-bold">Pay</button>
             </div>
           </footer>
         </section>
