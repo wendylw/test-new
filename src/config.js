d1231ebb3f35dd240e3fc1037fb7f1eaa2edf0f2
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2019-04-27 23:11:20 +0800

    add .env for local host development, and .env.production for production

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const config = {
-  // backendUrl: 'http://localhost:5000/graphql',
-  backendUrl: 'http://localhost:4000/graphql',
-  // backendUrl: 'http://localhost:9002/graphql',
+  backendUrl: process.env.REACT_APP_BACKEND_URL,
   business: (d => d.length > 2 ? d.shift() : null)(window.location.hostname.split('.')),
   table: (m => m ? parseInt(m[1]) : null)(window.location.search.match(/\btable\b=(\d+)/)),
 };
