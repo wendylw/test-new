b71a48f1c0e9bed97c1d00f7572a7df07f1a6ea0
wendylw <wendylw2013@gmail.com>
2019-05-31 00:12:20 +0800

    Beep-22: Add pickup id for thank you page

--- src/containers/ThankYou.js
+++ src/containers/ThankYou.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { compose } from 'react-apollo';
 import withOrderDetail from '../libs/withOrderDetail';
 import config from '../config';
@@ -27,7 +27,7 @@ export class ThankYou extends Component {
   renderNeedReceipt() {
     const { orderId } = this.props.order;
     let text = (
-      <button className="thanks__link link text-uppercase" onClick={() => this.setState({ needReceipt: 'detail' })}>
+      <button className="thanks__link link font-weight-bold text-uppercase" onClick={() => this.setState({ needReceipt: 'detail' })}>
         Need a receipt?
       </button>
     );
@@ -44,32 +44,27 @@ export class ThankYou extends Component {
       );
     }
 
-    return (
-      <>{text}</>
-    );
+    return (text);
   }
 
-  renderSendReceipt() {
+  renderPickupId() {
+    const { pickUpId } = this.props.order;
 
-    // TODO: when email receipt API is supported, then we can remove this and render form below.
-    if (true) {
+    if (!pickUpId) {
       return null;
     }
 
     return (
-      <form className="thanks__form form">
-        <div className="input__group">
-          <input className="input input__block" type="email" placeholder="Email"></input>
-        </div>
-        <div>
-          <button className="font-weight-bold text-uppercase button button__fill button__block">Send receipt</button>
-        </div>
-      </form>
+      <div className="thanks-queue-id__container">
+        <label className="gray-font-opacity font-weight-bold text-uppercase">Order Number</label>
+        <span className="thanks-queue-id__number">{pickUpId}</span>
+      </div>
     );
   }
 
   renderMain() {
     const { history, match, order } = this.props;
+    const date = new Date();
 
     if (!order) {
       return <div>Order Not Found</div>;
@@ -82,18 +77,23 @@ export class ThankYou extends Component {
             pathname: `${Constants.ROUTER_PATHS.HOME}`,
             search: `?table=${order.additionalComments}&storeId=${order.storeId}`
           })}>
-            <img src="/img/beep-error.png" alt="Thank you" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
           </figure>
           <h2 className="header__title font-weight-bold text-middle">Order Paid</h2>
-          <span className="gray-font-opacity text-uppercase">Selk pick-up</span>
+          <span className="gray-font-opacity text-uppercase">Self pick-up</span>
         </header>
         <div className="thanks text-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>
-          <h2 className="thanks__title font-weight-bold">Thank You!</h2>
-          <h4 className="thanks__subtitle gray-font-opacity font-weight-bold">Total paid {<CurrencyNumber money={order.total} />}</h4>
+          <img src="/img/beep-thank-you.png" />
+          <h2 className="thanks__title font-weight-light">Thank You!</h2>
+          {this.renderPickupId()}
+          <p className="thanks__prompt-text">Collect your order when your number is called/displayed</p>
           {this.renderNeedReceipt()}
-          {this.renderSendReceipt()}
         </div>
+        <footer className="footer-link">
+          <ul className="flex flex-middle flex-space-between">
+            <li><span>&copy; {date.getFullYear()} </span><a className="link link__non-underline" href="https://www.storehub.com/">StoreHub</a></li>
+          </ul>
+        </footer>
       </section>
     )
   }
