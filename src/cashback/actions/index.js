3bdcdcd984a70d6f0dfb10edfd0f8ae9ead7092f
Jiwang Feng <jiwang.feng@storehub.com>
2019-06-20 19:53:44 +0800

    feat(cash-back): remove unuseful code

--- src/cashback/actions/index.js
+++ src/cashback/actions/index.js
@@ -93,8 +93,6 @@ const cashbackSendMessage = (response, history) => dispatch => {
     /* save Cash Back messages */
     'Claimed_FirstTime': `Awesome, you've earned your first cashback! 🎉 To learn how to redeem it, tap the button below.`,
     'Claimed_NotFirstTime': `You've earned more cashback! 🎉`,
-    'Claimed_SameUser': `You've already earned cashback for this receipt. `,
-    'Claimed_DifferentUser': `Someone else has already earned cashback for this receipt. 😅`,
     'Claimed_Processing': `You've earned more cashback! We'll add it once it's been processed. 😉`,
     'Claimed_Someone_Else': `Someone else has already earned cashback for this receipt. 😅`,
     'Claimed_Repeat': `You've already earned cashback for this receipt. 👍`,
