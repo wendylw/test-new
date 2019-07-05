4087d761f141ebd9c11954ced0800d0c8593efa9
wendylw <wendylw2013@gmail.com>
2019-07-05 19:13:19 +0800

    BEEP-86: Revert store file

--- src/cashback/store.js
+++ src/cashback/store.js
@@ -3,13 +3,13 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducers from './reducers';
 
 const store = createStore(
-	rootReducers,
-	compose(
-		applyMiddleware(
-			thunk,
-		),
-		(func => (func ? func() : f => f))(window.__REDUX_DEVTOOLS_EXTENSION__),
-	),
+  rootReducers,
+  compose(
+    applyMiddleware(
+      thunk,
+    ),
+    (func => (func ? func() : f => f))(window.__REDUX_DEVTOOLS_EXTENSION__),
+  ),
 );
 
 export default store;
