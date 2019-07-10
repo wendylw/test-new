a72ea851dbdc22ef2b55c5766ec6686ca2f94595
wendylw <wendylw2013@gmail.com>
2019-07-10 15:53:19 +0800

    format Boostrap.js

--- src/Bootstrap.js
+++ src/Bootstrap.js
@@ -6,37 +6,37 @@ import Constants from './Constants';
 const Loading = () => <div className="loader theme page-loader"></div>;
 
 const AsyncOrderingApp = Loadable({
-	loader: () => import("./App"),
-	loading: Loading,
+  loader: () => import("./App"),
+  loading: Loading,
 })
 
 const AsyncCashbackApp = Loadable({
-	loader: () => import("./cashback/App"),
-	loading: Loading,
+  loader: () => import("./cashback/App"),
+  loading: Loading,
 })
 
 const AsyncNotFound = Loadable({
-	loader: () => import("./containers/NotFound"),
-	loading: Loading,
+  loader: () => import("./containers/NotFound"),
+  loading: Loading,
 });
 
 class Bootstrap extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<Switch>
-					<Route exact path={Constants.ROUTER_PATHS.INDEX} render={() => {
-						return (
-							<Redirect to={Constants.ROUTER_PATHS.ORDERING} />
-						);
-					}} />
-					<Route path={Constants.ROUTER_PATHS.ORDERING} component={AsyncOrderingApp} />
-					<Route path={Constants.ROUTER_PATHS.CASHBACK} component={AsyncCashbackApp} />
-					<Route component={AsyncNotFound} />
-				</Switch>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <Switch>
+          <Route exact path={Constants.ROUTER_PATHS.INDEX} render={() => {
+            return (
+              <Redirect to={Constants.ROUTER_PATHS.ORDERING} />
+            );
+          }} />
+          <Route path={Constants.ROUTER_PATHS.ORDERING} component={AsyncOrderingApp} />
+          <Route path={Constants.ROUTER_PATHS.CASHBACK} component={AsyncCashbackApp} />
+          <Route component={AsyncNotFound} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Bootstrap;
