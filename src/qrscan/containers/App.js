a5212ce578f564ff9df7f2bee5e996b1698e4167
snzx01 <supermanguhui@gmail.com>
2019-07-19 12:56:45 +0800

    feature  complete

--- src/qrscan/containers/App.js
+++ src/qrscan/containers/App.js
@@ -3,13 +3,15 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { actions as appActions, getError } from "../redux/modules/app";
 import Error from "./components/Error";
+import Scan from "./Scan.js";
 import "../styles.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="qr-scanner-app">
-        <h1 className="demo">QR Scanner</h1>
+        {/* Router */}
+        <Scan />
         {this.renderError()}
       </div>
     );
--- src/qrscan/containers/Home.js
+++ src/qrscan/containers/Home.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    return <div />;
+    return (
+    <div>
+      
+    </div>
+    )
   }
 }
 
