d1cc9248aa63d647dc95f0ce926115b3a54f8510
snzx01 <supermanguhui@gmail.com>
2019-07-24 21:42:36 +0800

    no message

--- src/qrscan/containers/components/Scanner.js
+++ src/qrscan/containers/components/Scanner.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import QrcodeDecoder from 'qrcode-decoder';
 
 const processQR = (qrData) => new Promise((resolve, reject) => {
-  if (/^https?:/.test(qrData).trim()) {
+  if (/^https?:/.trim().test(qrData)) {
     window.location.href = qrData;
     resolve(qrData)
   } else {
