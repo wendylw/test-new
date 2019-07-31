f3e7364b02e0836f5c4e8fafc15ba093ea523110
snzx01 <supermanguhui@gmail.com>
2019-07-31 15:21:58 +0800

    BEEP-146 IOS judge

--- src/qrscan/containers/components/GetPermission.js
+++ src/qrscan/containers/components/GetPermission.js
@@ -5,7 +5,7 @@ import { withRouter } from 'react-router-dom';
 class GetPermission extends Component {
   render() {
     let ua = navigator.userAgent.toLowerCase();
-    if (/(iPhone|iPad|iPod|iOS)/i.test(ua)) {
+    if (/(iphone|ipad|ipod|ios)/i.test(ua)) {
       if (/chrome/i.test(ua) && /webkit/i.test(ua) && /mozilla/i.test(ua)) {
         this.props.history.push({
           pathname: Constants.ALL_ROUTER.notSupport,
