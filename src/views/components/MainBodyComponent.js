fec73dbf8eabb365bd0ebfc8ef12aded4072f158
wendylw <wendylw2013@gmail.com>
2019-08-08 20:10:59 +0800

    BEEP-105: Update sold out logic

--- src/views/components/MainBodyComponent.js
+++ src/views/components/MainBodyComponent.js
@@ -15,6 +15,7 @@ import Item from '../../components/Item';
 import CurrencyNumber from '../../components/CurrencyNumber';
 import ItemOperator from '../../components/ItemOperator';
 import config from '../../config';
+import Utils from '../../libs/utils';
 import Constants from '../../Constants';
 
 const { PRODUCT } = Constants.HOME_ASIDE_NAMES;
@@ -138,7 +139,7 @@ export class MainBodyComponent extends Component {
                           >
 
                             {
-                              prod.soldOut
+                              Utils.isProductSoldOut(prod.soldOut)
                                 ? <Tag text="Sold Out" className="tag__card" />
                                 : (
                                   <ItemOperator
