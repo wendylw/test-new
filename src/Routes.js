72e65b01e92653d6e5da1006594b3ad96a6b6bae
wendylw <wendylw2013@gmail.com>
2019-08-26 12:51:50 +0800

    BEEP-169: revert Route for original version

--- src/Routes.js
+++ src/Routes.js
@@ -4,79 +4,79 @@ import { Route } from "react-router-dom";
 // import Constants from "./Constants";
 import MessageModal from "./views/components/MessageModal";
 
-// const MyLoadingComponent = ({ isLoading, error }) => {
-//   // Handle the loading state
-//   if (isLoading) {
-//     return <div className="loader theme page-loader"></div>;
-//   }
-//   // Handle the error state
-//   else if (error) {
-//     console.error(error);
+const MyLoadingComponent = ({ isLoading, error }) => {
+  // Handle the loading state
+  if (isLoading) {
+    return <div className="loader theme page-loader"></div>;
+  }
+  // Handle the error state
+  else if (error) {
+    console.error(error);
 
-//     return (
-//       <div>
-//         Sorry, there was a problem loading the page.
-//       </div>
-//     );
-//   }
-//   else {
-//     return null;
-//   }
-// };
+    return (
+      <div>
+        Sorry, there was a problem loading the page.
+      </div>
+    );
+  }
+  else {
+    return null;
+  }
+};
 
-// const AsyncHome = Loadable({
-//   loader: () => import("./containers/Home"),
-//   loading: MyLoadingComponent,
-// });
+const AsyncHome = Loadable({
+  loader: () => import("./containers/Home"),
+  loading: MyLoadingComponent,
+});
 
-// const AsyncCart = Loadable({
-//   loader: () => import("./containers/Cart"),
-//   loading: MyLoadingComponent,
-// });
+const AsyncCart = Loadable({
+  loader: () => import("./containers/Cart"),
+  loading: MyLoadingComponent,
+});
 
-// const AsyncPayment = Loadable({
-//   loader: () => import("./containers/Payment"),
-//   loading: MyLoadingComponent,
-// });
+const AsyncPayment = Loadable({
+  loader: () => import("./containers/Payment"),
+  loading: MyLoadingComponent,
+});
 
-// const AsyncBankCardPayment = Loadable({
-//   loader: () => import("./containers/BraintreeBankCardPayment"),
-//   loading: MyLoadingComponent,
-// });
+const AsyncBankCardPayment = Loadable({
+  loader: () => import("./containers/BraintreeBankCardPayment"),
+  loading: MyLoadingComponent,
+});
 
-// const AsyncOnlineBankingPayment = Loadable({
-//   loader: () => import("./containers/OnlineBankingPayment"),
-//   loading: MyLoadingComponent,
-// });
+const AsyncOnlineBankingPayment = Loadable({
+  loader: () => import("./containers/OnlineBankingPayment"),
+  loading: MyLoadingComponent,
+});
 
-// const AsyncThankYou = Loadable({
-//   loader: () => import("./containers/ThankYou"),
-//   loading: MyLoadingComponent,
-// });
+const AsyncThankYou = Loadable({
+  loader: () => import("./containers/ThankYou"),
+  loading: MyLoadingComponent,
+});
 
-// const AsyncReceiptDetail = Loadable({
-//   loader: () => import("./containers/ReceiptDetail"),
-//   loading: MyLoadingComponent,
-// });
+const AsyncReceiptDetail = Loadable({
+  loader: () => import("./containers/ReceiptDetail"),
+  loading: MyLoadingComponent,
+});
 
-// const AsyncSorry = Loadable({
-//   loader: () => import("./containers/Sorry"),
-//   loading: MyLoadingComponent,
-// });
+const AsyncSorry = Loadable({
+  loader: () => import("./containers/Sorry"),
+  loading: MyLoadingComponent,
+});
 
-// const AsyncPeopleCountModal = Loadable({
-//   loader: () => import("./views/components/PeopleCountModal"),
-//   loading: MyLoadingComponent,
-// });
+const AsyncPeopleCountModal = Loadable({
+  loader: () => import("./views/components/PeopleCountModal"),
+  loading: MyLoadingComponent,
+});
 
-// const AsyncError = Loadable({
-//   loader: () => import("./views/components/Error"),
-//   loading: MyLoadingComponent,
-// });
+const AsyncError = Loadable({
+  loader: () => import("./views/components/Error"),
+  loading: MyLoadingComponent,
+});
 
 export default () =>
   <React.Fragment>
-    {/* <Route path={Constants.ROUTER_PATHS.HOME} exact component={AsyncHome} />
+    <Route path={Constants.ROUTER_PATHS.HOME} exact component={AsyncHome} />
     <Route path={Constants.ROUTER_PATHS.PORDUCTS} component={AsyncHome} />
     <Route path={Constants.ROUTER_PATHS.CART} exact component={AsyncCart} />
     <Route path={Constants.ROUTER_PATHS.PAYMENT} exact component={AsyncPayment} />
@@ -84,7 +84,7 @@ export default () =>
     <Route path={Constants.ROUTER_PATHS.THANK_YOU} exact component={AsyncThankYou} />
     <Route path={Constants.ROUTER_PATHS.RECEIPT_DETAIL} exact component={AsyncReceiptDetail} />
     <Route path={Constants.ROUTER_PATHS.SORRY} exact component={AsyncSorry} />
-    <Route path={Constants.ROUTER_PATHS.ERROR} exact component={AsyncError} /> */}
+    <Route path={Constants.ROUTER_PATHS.ERROR} exact component={AsyncError} />
     <Route path={`*/modal/:modal`} render={() => {
       return null;
     }} />
