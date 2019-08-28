6bb84f1a585ca3fb08ed82757ef2c756f3e5c00b
wendylw <wendylw2013@gmail.com>
2019-08-28 20:16:22 +0800

    BEEP-169: Update Error page

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Route } from "react-router-dom";
-// import Loadable from 'react-loadable';
-// import Constants from "./Constants";
 import MessageModal from "./views/components/MessageModal";
 
 const MyLoadingComponent = ({ isLoading, error }) => {
