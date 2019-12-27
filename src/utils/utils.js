4fe06ea00f21ecc310daf14e49272a352b108220
wendylw <wendylw2013@gmail.com>
2019-12-27 17:48:00 +0800

    FB-218: Update polyfill for Object.values

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -287,14 +287,19 @@ Utils.getUserAgentInfo = function getUserAgentInfo() {
 Utils.polyfill = function polyfill() {
   if (!Object.values) {
     Object.values = function(obj) {
-      if (obj !== Object(obj)) throw new TypeError('Object.values called on a non-object');
-      var val = [],
+      if (obj !== Object(obj)) {
+        throw new TypeError('Object.values called on a non-object');
+      }
+
+      let val = [],
         key;
+
       for (key in obj) {
         if (Object.prototype.hasOwnProperty.call(obj, key)) {
           val.push(obj[key]);
         }
       }
+
       return val;
     };
   }
