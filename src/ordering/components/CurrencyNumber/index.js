1a02de576621ea0b9a1b53cac7a9f857396825dd
wendylw <wendylw2013@gmail.com>
2019-12-30 10:53:20 +0800

    FB-218: Update Intl compatibility

--- src/ordering/components/CurrencyNumber/index.js
+++ src/ordering/components/CurrencyNumber/index.js
@@ -4,6 +4,15 @@ import { connect } from 'react-redux';
 import { getOnlineStoreInfo } from '../../redux/modules/app';
 
 class CurrencyNumber extends React.Component {
+  componentDidMount() {
+    const script = document.createElement('script');
+
+    script.src = 'https://cdn.polyfill.io/v2/polyfill.min.js?features=Intl.~locale.en,Intl.~locale.th';
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   formatChildrenAsMoney() {
     const { locale, currency, money } = this.props;
 
@@ -11,39 +20,33 @@ class CurrencyNumber extends React.Component {
       return money;
     }
 
-    return Intl.NumberFormat(
-      locale,
-      { style: 'currency', currency }
-    ).format(parseFloat(money));
+    return Intl.NumberFormat(locale, { style: 'currency', currency }).format(parseFloat(money));
   }
 
   render() {
     const { className, addonBefore } = this.props;
 
-    return <span className={className}>{`${addonBefore ? `${addonBefore} ` : ''}${this.formatChildrenAsMoney()}`}</span>;
+    return (
+      <span className={className}>{`${addonBefore ? `${addonBefore} ` : ''}${this.formatChildrenAsMoney()}`}</span>
+    );
   }
 }
 
 CurrencyNumber.propTypes = {
   className: PropTypes.string,
   addonBefore: PropTypes.string,
-  money: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-  ]),
+  money: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 CurrencyNumber.defaultProps = {
   money: 0,
 };
 
-export default connect(
-  (state) => {
-    const { locale, currency } = getOnlineStoreInfo(state) || {};
+export default connect(state => {
+  const { locale, currency } = getOnlineStoreInfo(state) || {};
 
-    return {
-      locale,
-      currency,
-    };
-  }
-)(CurrencyNumber);
+  return {
+    locale,
+    currency,
+  };
+})(CurrencyNumber);
