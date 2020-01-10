67988072e30028fc06cf2012a797b7961c818304
wendylw <wendylw2013@gmail.com>
2020-01-10 18:19:19 +0800

    FB-218: Update describe text

--- src/utils/polyfill.test.js
+++ src/utils/polyfill.test.js
@@ -1,7 +1,7 @@
 import { createPolyfill } from './polyfill';
 
-describe('Create polyfill link', () => {
-  it('Polyfill script link created successfully', () => {
+describe('utils/polyfill: Create polyfill link', () => {
+  it('Polyfill script link should be created when page loaded', () => {
     const polyfillLink =
       'https://cdn.polyfill.io/v3/polyfill.min.js?features=Object.values%2CIntl.~locale.en,Intl.~locale.th';
     jest.spyOn(global.document.body, 'appendChild');
