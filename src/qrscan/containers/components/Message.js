457cc6001333ebf5558dc4d649b6d5d58e3db22e
Wanhao Sun <supermnaguhui@.qq.com>
2020-01-15 17:19:04 +0800

    add andorid will show the message

--- src/qrscan/containers/components/Message.js
+++ src/qrscan/containers/components/Message.js
@@ -2,20 +2,10 @@ import React, { Component } from 'react';
 
 class Message extends Component {
   render() {
-    const regex = /(MSIE|Trident|(?!Gecko.+)Firefox|(?!AppleWebKit.+Chrome.+)Safari(?!.+Edge)|(?!AppleWebKit.+)Chrome(?!.+Edge)|(?!AppleWebKit.+Chrome.+Safari.+)Edge|AppleWebKit(?!.+Chrome|.+Safari)|Gecko(?!.+Firefox))(?: |\/)([\d\.apre]+)/g;
-    let showMessage = !/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent) && !navigator.userAgent.match(regex);
-
-    let device = navigator.userAgent;
-    let browser = navigator.userAgent;
-    let showMessage1 = !/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent);
-    let showMessage2 = !navigator.userAgent.match(regex);
+    let showMessage = !/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent);
 
     return (
       <div>
-        device: {device}
-        browser: {browser}
-        showMessage1: {`${showMessage1}`}
-        showMessage2: {`${showMessage2}`}
         {showMessage ? (
           <div className="top-message primary fixed">
             <div className="top-message__text">We recommend that you use Google Chrome</div>
