312b2246ebd63606b11faece77bfd67eeaa2870a
wendylw <wendylw2013@gmail.com>
2020-01-19 19:06:46 +0800

    FB-277: Completed prompt page

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Constants from '../utils/constants';
 
 import '../Common.scss';
-
+import '../App.scss';
 export class NotFound extends Component {
   render() {
     const { title, message } = this.props;
 
     return (
-      <section className="table-ordering__prompt-page">
-        <figure className="prompt-page__image-container text-center">
-          <img src="/img/beep-error.png" alt="error found" />
-        </figure>
-        <div className="prompt-page__content">
-          <h2 className="prompt-page__title text-center">{title}</h2>
-          <div className="prompt-page__paragraphs">
-            <p>{message}</p>
+      <main className="table-ordering">
+        <section className="table-ordering__prompt-page">
+          <figure className="prompt-page__image-container text-center">
+            <img src="/img/beep-error.png" alt="error found" />
+          </figure>
+          <div className="prompt-page__content">
+            <h2 className="prompt-page__title text-center">Store Not Found!</h2>
+            <div className="prompt-page__paragraphs text-center">
+              <p>This store does not exist, please check your store name and try again.</p>
+            </div>
           </div>
-        </div>
-      </section>
+
+          <div className="prompt-page__button-container">
+            <Link
+              className="button link__block link__non-underline button__fill font-weight-bold text-center text-uppercase border-radius-base"
+              to={Constants.ROUTER_PATHS.STORES_HOME}
+            >
+              Back to home
+            </Link>
+          </div>
+        </section>
+      </main>
     );
   }
 }
