e190687631eb973ca7d8327e1024e236318e676a
wendylw <wendylw2013@gmail.com>
2020-02-07 15:44:15 +0800

    Squashed commit of the following: Merge FB-277
    commit 914624d0eb734af00a02b23bdaad9924ede059a9
    Merge: 2e38168b db7453a2
    Author: wendylw <wendylw2013@gmail.com>
    Date:   Fri Feb 7 15:36:56 2020 +0800
    
        Merge remote-tracking branch 'origin/master' into FB-277
    
    commit 2e38168b1470c6892c235a9eccacc3a505d44dae
    Author: wendylw <wendylw2013@gmail.com>
    Date:   Mon Jan 20 10:41:55 2020 +0800
    
        FB-277: Clear variable for NotFound
    
    commit 821c826a1b5813aac1bf8f5ad05de05112dcb032
    Author: wendylw <wendylw2013@gmail.com>
    Date:   Sun Jan 19 19:46:46 2020 +0800
    
        FB-277: Update not found page
    
    commit 4baddd119fbc703502bcf77f19cb2462c7611658
    Author: wendylw <wendylw2013@gmail.com>
    Date:   Sun Jan 19 19:06:46 2020 +0800
    
        FB-277: Completed prompt page
    
    commit ff6652bd29eb1066bde99245f355a15a22a431c5
    Author: wendylw <wendylw2013@gmail.com>
    Date:   Sun Jan 19 17:40:45 2020 +0800
    
        FB-277: Update not found page

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,10 +1,25 @@
-import React from "react";
-import DocumentTitle from "../components/DocumentTitle";
-import Constants from "../utils/constants";
+import React, { Component } from 'react';
 
-export default () =>
-  <DocumentTitle title={Constants.DOCUMENT_TITLE.NOT_FOUND}>
-    <div className="NotFound">
-      <h3>Sorry, page not found!</h3>
-    </div>
-  </DocumentTitle>;
+import '../Common.scss';
+import '../App.scss';
+export class NotFound extends Component {
+  render() {
+    return (
+      <main className="table-ordering">
+        <section className="table-ordering__prompt-page">
+          <figure className="prompt-page__image-container text-center">
+            <img src="/img/beep-error.png" alt="error found" />
+          </figure>
+          <div className="prompt-page__content">
+            <h2 className="prompt-page__title text-center">Not Found!</h2>
+            <div className="prompt-page__paragraphs text-center">
+              <p>Sorry page not found</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default NotFound;
