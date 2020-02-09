e571219369b7459e0d1b5f912d66da01d75ceed5
yunxi <598616202@qq.com>
2020-02-09 15:32:59 +0800

    FB-275:home action finished

--- src/ordering/redux/modules/homeActions.test.js
+++ src/ordering/redux/modules/homeActions.test.js
@@ -1,4 +1,4 @@
-import { actions, initialState } from './home';
+import { actions, initialState, fetchShoppingCart, fetchOnlineCategory } from './home';
 import rootReducer from './index';
 import { HOME_TYPES as types } from '../types';
 import {
@@ -31,15 +31,89 @@ describe('src/ordering/redux/modules/app.js:actions', () => {
   afterEach(() => {
     store.clearActions();
   });
-  //   describe('action creators', () => {
-  //     it('loadProductList', () => {
-  //       const expectedAction = {
-  //         type: types.SHOW_LOGIN_PAGE,
-  //       };
-  //       return expect(actions.showLogin()).toEqual(expectedAction);
-  //     });
-  //   });
+
   describe('Async Action Creators', () => {
+    describe('loadProductList', () => {
+      it('should direction return', () => {
+        const caseStore = configureMiddlewareStore({ home: { onlineCategory: { categoryIds: [] } } });
+        expect(actions.loadProductList()(caseStore.dispatch, caseStore.getState)).toEqual(undefined);
+      });
+      it('fetchOnlineCategory', () => {
+        successMockFetch();
+        const expectedActions = [
+          { type: types.FETCH_ONLINECATEGORY_REQUEST },
+          { type: types.FETCH_ONLINECATEGORY_SUCCESS, responseGql: commonSuccessData },
+        ];
+        expectedActionsCheck(fetchOnlineCategory(), expectedActions);
+      });
+    });
+    describe('loadShoppingCart', () => {
+      successMockFetch();
+      store.clearActions();
+      const expectedActions = [
+        { type: types.FETCH_SHOPPINGCART_REQUEST },
+        { type: types.FETCH_SHOPPINGCART_SUCCESS, response: commonSuccessData, params: {} },
+      ];
+      return expectedActionsCheck(fetchShoppingCart(), expectedActions);
+    });
+    describe('removeShoppingCartItem', () => {
+      it(':Success', () => {
+        store.clearActions();
+        successMockFetch();
+        const expectedActions = [
+          { type: types.REMOVE_SHOPPINGCARTITEM_REQUEST },
+          { type: types.REMOVE_SHOPPINGCARTITEM_SUCCESS, responseGql: commonSuccessData },
+        ];
+        return expectedActionsCheck(actions.removeShoppingCartItem({}), expectedActions);
+      });
+    });
+    describe('addOrUpdateShoppingCartItem', () => {
+      it(':Success', () => {
+        successMockFetch();
+        const expectedActions = [
+          { type: types.ADDORUPDATE_SHOPPINGCARTITEM_REQUEST },
+          { type: types.ADDORUPDATE_SHOPPINGCARTITEM_SUCCESS, responseGql: commonSuccessData },
+        ];
+        const reqParams = {};
+        return expectedActionsCheck(actions.addOrUpdateShoppingCartItem(reqParams), expectedActions);
+      });
+    });
+    describe('decreaseProductInCart', () => {
+      it('should dispatch removeShoppingCartItem', () => {
+        successMockFetch();
+
+        const expectedActions = [
+          { type: types.REMOVE_SHOPPINGCARTITEM_REQUEST },
+          { type: types.REMOVE_SHOPPINGCARTITEM_SUCCESS, responseGql: commonSuccessData },
+        ];
+        const cloneProduct = JSON.parse(JSON.stringify(productParams));
+        cloneProduct.id = '5de4dc12f90ac02b38b93f28';
+
+        return expectedActionsCheck(
+          actions.decreaseProductInCart(fetchShoppingCartData, cloneProduct),
+          expectedActions
+        );
+      });
+      it('should dispatch addOrUpdateShoppingCartItem', () => {
+        successMockFetch();
+        const caseStore = configureMiddlewareStore(orderingStore);
+        const expectedActions = [
+          {
+            type: types.ADDORUPDATE_SHOPPINGCARTITEM_REQUEST,
+          },
+          {
+            type: types.ADDORUPDATE_SHOPPINGCARTITEM_SUCCESS,
+            responseGql: commonSuccessData,
+          },
+        ];
+        const cloneProduct = JSON.parse(JSON.stringify(productParams));
+        cloneProduct.id = '5de4dc12f90ac02b38b93f28';
+        cloneProduct.cartQuantity = 2;
+        return caseStore.dispatch(actions.decreaseProductInCart(fetchShoppingCartData, cloneProduct)).then(() => {
+          expect(caseStore.getActions()).toEqual(expectedActions);
+        });
+      });
+    });
     describe('increaseProductInCart', () => {
       it('should direction return', () => {
         const caseStore = configureMiddlewareStore({ home: { currentProduct: { id: '5dbfcc7f637055227dd94e58' } } });
