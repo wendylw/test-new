42b6010e881962478675faad5fea25906566bb3f
yunxi <598616202@qq.com>
2020-02-10 22:00:05 +0800

    FB-275:home reducer finish

--- src/ordering/redux/modules/homeReducer.test.js
+++ src/ordering/redux/modules/homeReducer.test.js
@@ -1,19 +1,29 @@
 import homeReducers, { initialState } from './home';
 import rootReducer from './index';
 import { HOME_TYPES as types } from '../types';
-import { getReducerNewState, configureMiddlewareStore } from '../../../utils/testHelper';
+import { getReducerNewState, configureMiddlewareStore, expectedActionsCheck } from '../../../utils/testHelper';
 
 import {
   getShoppingCart as getShoppingCartSelector,
   getCategoryProductList as getCategoryProductListSelector,
 } from './home';
 
-import { getShoppingCartSelectorResult, getCategoryProductListSelectorResult } from '../__fixtures__/home.fixture';
+import {
+  fetchShoppingCartData,
+  fetchOnlineCategoryData,
+  getShoppingCartSelectorResult,
+  getCategoryProductListSelectorResult,
+} from '../__fixtures__/home.fixture';
 const orderingState = rootReducer(undefined, {});
 
 describe('src/ordering/redux/modules/home.js:reducers', () => {
   it('should return the initial state', () => {
     expect(homeReducers(undefined, {})).toEqual(initialState);
+  });
+  describe('domProperties', () => {
+    expect(getReducerNewState(homeReducers, { type: null }, 'domProperties')).toEqual({
+      ...initialState.domProperties,
+    });
   });
   describe('currentProduct', () => {
     const nameField = 'currentProduct';
@@ -38,6 +48,99 @@ describe('src/ordering/redux/modules/home.js:reducers', () => {
     it('default', () => {
       expect(getReducerNewState(homeReducers, { type: 'default' }, nameField)).toEqual({
         ...initialState.currentProduct,
+      });
+    });
+  });
+  describe('shoppingCart', () => {
+    const nameField = 'shoppingCart';
+    let cloneFetchShoppingCartData = JSON.parse(JSON.stringify(fetchShoppingCartData));
+    const shoppingCartActionInfo = {
+      response: cloneFetchShoppingCartData,
+    };
+
+    it('should return state with empty itemIds', () => {
+      const shoppingCartActionInfo = {
+        responseGql: {
+          data: {
+            emptyShoppingCart: {
+              success: true,
+            },
+          },
+        },
+      };
+      const expectedState = {
+        ...initialState.shoppingCart,
+        isFetching: false,
+        itemIds: [],
+        unavailableItemIds: [],
+      };
+      const action = { type: null, responseGql: shoppingCartActionInfo };
+      expect(getReducerNewState(homeReducers, action, nameField)).toEqual(expectedState);
+    });
+    it('FETCH_SHOPPINGCART_REQUEST', () => {
+      expect(getReducerNewState(homeReducers, { type: types.FETCH_SHOPPINGCART_REQUEST }, nameField)).toEqual({
+        ...initialState.shoppingCart,
+        isFetching: true,
+      });
+    });
+    it('FETCH_SHOPPINGCART_SUCCESS', () => {
+      const action = {
+        type: types.FETCH_SHOPPINGCART_SUCCESS,
+        ...shoppingCartActionInfo,
+      };
+      const expectedState = {
+        ...initialState.shoppingCart,
+        isFetching: false,
+        itemIds: [
+          'd79db2cd92d785e5d4c07e046052bb34',
+          '41f0bafe441a059d8f76f2c966732b85',
+          '3c81558b01e1c770b6442b9fd0f6d763',
+        ],
+        unavailableItemIds: [],
+      };
+      expect(getReducerNewState(homeReducers, action, nameField)).toEqual(expectedState);
+    });
+    it('FETCH_SHOPPINGCART_FAILURE', () => {
+      expect(getReducerNewState(homeReducers, { type: types.FETCH_SHOPPINGCART_FAILURE }, nameField)).toEqual({
+        ...initialState.shoppingCart,
+        isFetching: false,
+      });
+    });
+    it('default', () => {
+      expect(getReducerNewState(homeReducers, { type: 'default' }, nameField)).toEqual({
+        ...initialState.shoppingCart,
+      });
+    });
+  });
+  describe('onlineCategory', () => {
+    const nameField = 'onlineCategory';
+    it('FETCH_ONLINECATEGORY_REQUEST', () => {
+      expect(getReducerNewState(homeReducers, { type: types.FETCH_ONLINECATEGORY_REQUEST }, nameField)).toEqual({
+        ...initialState.onlineCategory,
+        isFetching: true,
+      });
+    });
+    it('FETCH_ONLINECATEGORY_SUCCESS', () => {
+      const action = {
+        type: types.FETCH_ONLINECATEGORY_SUCCESS,
+        responseGql: fetchOnlineCategoryData,
+      };
+      const expectedState = {
+        ...initialState.onlineCategory,
+        isFetching: false,
+        categoryIds: ['5da845cf2d7b4244276bddb2', '5cdce13be5fea1125c716dd5', '5cd159c1fb012d7c4b1c9f9d'],
+      };
+      expect(getReducerNewState(homeReducers, action, nameField)).toEqual(expectedState);
+    });
+    it('FETCH_ONLINECATEGORY_FAILURE', () => {
+      expect(getReducerNewState(homeReducers, { type: types.FETCH_ONLINECATEGORY_FAILURE }, nameField)).toEqual({
+        ...initialState.onlineCategory,
+        isFetching: false,
+      });
+    });
+    it('default', () => {
+      expect(getReducerNewState(homeReducers, { type: 'default' }, nameField)).toEqual({
+        ...initialState.onlineCategory,
       });
     });
   });
