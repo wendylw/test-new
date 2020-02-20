686a269a126c2653f5e5af9eb92baea788a4a008
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2020-02-20 14:41:52 +0800

    FB-382: add polyfill for Object.values, Intl, Promise, Array.prototype.includes and localStorage by polyfill.io

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import './utils/polyfill';
 import 'whatwg-fetch';
 import './config'; // import here for globally init
 import './i18n';
