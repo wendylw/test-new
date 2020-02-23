3d2221edcf4405175c794941b521259a0c7c94af
penny <penny.cai.github.com>
2020-02-23 23:02:15 +0800

    FB-275: src/stores/redux/modules/home.js ADD test case

--- src/stores/redux/modules/home.js
+++ src/stores/redux/modules/home.js
@@ -6,13 +6,12 @@ import { FETCH_GRAPHQL } from '../../../redux/middlewares/apiGql';
 import { getStoreById } from '../../../redux/modules/entities/stores';
 import { getBusiness } from './app';
 
-const initialState = {
+export const initialState = {
   storeHashCode: null,
   storeIds: [],
 };
 
 export const types = {
-
   // fetch coreStores
   FETCH_CORESTORES_REQUEST: 'STORES/HOME/FETCH_CORESTORES_REQUEST',
   FETCH_CORESTORES_SUCCESS: 'STORES/HOME/FETCH_CORESTORES_SUCCESS',
@@ -22,7 +21,7 @@ export const types = {
   FETCH_STORE_HASHCODE_REQUEST: 'STORES/HOME/FETCH_STORE_HASHCODE_REQUEST',
   FETCH_STORE_HASHCODE_SUCCESS: 'STORES/HOME/FETCH_STORE_HASHCODE_SUCCESS',
   FETCH_STORE_HASHCODE_FAILURE: 'STORES/HOME/FETCH_STORE_HASHCODE_FAILURE',
-}
+};
 
 export const actions = {
   loadCoreStores: () => (dispatch, getState) => {
@@ -30,7 +29,7 @@ export const actions = {
     return dispatch(fetchCoreStores({ business }));
   },
 
-  getStoreHashData: (storeId) => ({
+  getStoreHashData: storeId => ({
     [API_REQUEST]: {
       types: [
         types.FETCH_STORE_HASHCODE_REQUEST,
@@ -38,21 +37,17 @@ export const actions = {
         types.FETCH_STORE_HASHCODE_FAILURE,
       ],
       ...Url.API_URLS.GET_STORE_HASH_DATA(storeId),
-    }
+    },
   }),
 };
 
 const fetchCoreStores = variables => ({
   [FETCH_GRAPHQL]: {
-    types: [
-      types.FETCH_CORESTORES_REQUEST,
-      types.FETCH_CORESTORES_SUCCESS,
-      types.FETCH_CORESTORES_FAILURE,
-    ],
+    types: [types.FETCH_CORESTORES_REQUEST, types.FETCH_CORESTORES_SUCCESS, types.FETCH_CORESTORES_FAILURE],
     endpoint: Url.apiGql('CoreStores'),
     variables,
-  }
-})
+  },
+});
 
 // reducer
 const reducer = (state = initialState, action) => {
@@ -79,17 +74,17 @@ const reducer = (state = initialState, action) => {
     default:
       return state;
   }
-}
+};
 
 export default reducer;
 
-export const getAllStores = (state) => {
+export const getAllStores = state => {
   return state.home.storeIds.map(id => getStoreById(state, id));
-}
+};
 
 export const getOneStoreInfo = (state, storeId) => {
   return getStoreById(state, storeId);
-}
+};
 
 export const getStoreHashCode = state => state.home.storeHashCode;
 export const showStores = state => !state.home.isFetching;
