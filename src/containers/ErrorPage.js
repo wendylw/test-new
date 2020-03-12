241d92433c2d3332b5caa145a55b8a5e863e9300
wendylw <wendylw2013@gmail.com>
2020-03-12 18:41:00 +0800

    hotfix_beep-504-page: Remove back home page button

--- src/containers/ErrorPage.js
+++ src/containers/ErrorPage.js
@@ -1,13 +1,10 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
-import config from '../config';
 import beepErrorImage from '../images/beep-error.png';
 
 export class ErrorPage extends PureComponent {
   render() {
-    const { t } = this.props;
-
     return (
       <main className="table-ordering">
         <section className="table-ordering__prompt-page flex flex-column flex-space-between">
@@ -19,17 +16,6 @@ export class ErrorPage extends PureComponent {
             <div className="prompt-page__paragraphs text-center">
               <p>We're sorry for the inconvenience caused.</p>
             </div>
-          </div>
-
-          <div className="prompt-page__button-container">
-            <button
-              className="button button__block button__fill font-weight-bold text-center text-uppercase border-radius-base"
-              onClick={() => {
-                return (window.location.href = config.qrScanPageUrl);
-              }}
-            >
-              {t('BackToHome')}
-            </button>
           </div>
         </section>
       </main>
