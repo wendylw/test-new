6f66fc9651a36149d53fb053da984188b6160fb7
Wendy <wendylw2013@gmail.com>
2020-03-13 01:13:44 +0800

    Revert "hotfix_beep-504-page" (#383)

--- src/ordering/index.js
+++ src/ordering/index.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import App from './containers/App';
-import store from './redux/store';
-import ErrorPage from '../containers/ErrorPage';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import App from "./containers/App";
+import store from "./redux/store";
 
 class index extends Component {
   render() {
     return (
-      <Provider store={store}>{process.env.REACT_APP_BLOCK_ORDERING === 'true' ? <ErrorPage /> : <App />}</Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     );
   }
 }
