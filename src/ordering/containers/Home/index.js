fdf4b271ca5542a945c397a4414d16801f2486a6
wendylw <wendylw2013@gmail.com>
2020-03-17 08:52:31 +0800

    FB-407: Update delivery hours modal UI

--- src/ordering/containers/Home/index.js
+++ src/ordering/containers/Home/index.js
@@ -3,7 +3,9 @@ import { withTranslation } from 'react-i18next';
 import qs from 'qs';
 import Footer from './components/Footer';
 import Header from '../../../components/Header';
-import { IconEdit, IconInfoOutline } from '../../../components/Icons';
+import Tag from '../../../components/Tag';
+import Image from '../../../components/Image';
+import { IconEdit, IconInfoOutline, IconMotorcycle } from '../../../components/Icons';
 import ProductDetail from './components/ProductDetail';
 import MiniCartListModal from './components/MiniCartListModal';
 import CurrentCategoryBar from './components/CurrentCategoryBar';
@@ -174,6 +176,78 @@ export class Home extends Component {
           show={viewAside === Constants.ASIDE_NAMES.CART || viewAside === Constants.ASIDE_NAMES.PRODUCT_ITEM}
           onToggle={this.handleToggleAside.bind(this, Constants.ASIDE_NAMES.CARTMODAL_HIDE)}
         />
+        <aside className="aside active">
+          <div className="store-info">
+            <i className="aside-bottom__slide-button"></i>
+
+            <div className="flex flex-top flex-space-between">
+              <Image
+                className="header__image-container text-middle"
+                src={onlineStoreInfo.logo}
+                alt={onlineStoreInfo.title}
+              />
+              <div className="header__title-container">
+                <h1 className="header__title">
+                  <span className="font-weight-bold text-middle">{onlineStoreInfo.storeName}</span>
+                  <div className="tag__card-container">
+                    <Tag text="Closed" className="tag__card warning downsize text-middle"></Tag>
+                  </div>
+                </h1>
+                <p className="store-info__address gray-font-opacity">
+                  34, Jalan Ambong 4, Kepong Baru, 52100 Kuala Lumpur
+                </p>
+                <a className="store-info__phone link link__non-underline" href="tel:+6001298765432">
+                  +60 012 98765432
+                </a>
+                <ul className="header__info-list">
+                  <li className="header__info-item">
+                    <i className="header__motor-icon text-middle">
+                      <IconMotorcycle />
+                    </i>
+                    <span className="text-middle">RM 5.00</span>
+                  </li>
+                  <li className="header__info-item">
+                    <span>Min Order. RM 20.00</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="store-info__delivery-hours flex flex-top flex-space-between">
+              <label className="font-weight-bold gray-font-opacity">Delivery Hours</label>
+              <ul className="store-info__list">
+                <li className="store-info__item flex flex-middle flex-space-between">
+                  <span>Sun</span>
+                  <time>11:00 - 22:30</time>
+                </li>
+                <li className="store-info__item flex flex-middle flex-space-between">
+                  <span>Sun</span>
+                  <time>11:00 - 22:30</time>
+                </li>
+                <li className="store-info__item flex flex-middle flex-space-between">
+                  <span>Sun</span>
+                  <time>11:00 - 22:30</time>
+                </li>
+                <li className="store-info__item flex flex-middle flex-space-between">
+                  <span>Sun</span>
+                  <time>11:00 - 22:30</time>
+                </li>
+                <li className="store-info__item flex flex-middle flex-space-between">
+                  <span>Sun</span>
+                  <time>11:00 - 22:30</time>
+                </li>
+                <li className="store-info__item flex flex-middle flex-space-between">
+                  <span>Sun</span>
+                  <time>11:00 - 22:30</time>
+                </li>
+                <li className="store-info__item flex flex-middle flex-space-between">
+                  <span>Sun</span>
+                  <time>11:00 - 22:30</time>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </aside>
         <Footer
           {...otherProps}
           onToggle={this.handleToggleAside.bind(this)}
