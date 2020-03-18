077c89afbdc07f37930a8004551308a54aa9b5d2
wendylw <wendylw2013@gmail.com>
2020-03-18 10:52:23 +0800

    FB-407: Update payment flow

--- src/ordering/containers/Customer/index.js
+++ src/ordering/containers/Customer/index.js
@@ -37,18 +37,18 @@ class Customer extends Component {
     }
 
     await Utils.setLocalStorageVariable('user.p', phone);
-    await appActions.getOtp({ phone });
+    // await appActions.getOtp({ phone });
   }
 
   async handleCreateOrder() {
     this.savePhoneNumber();
 
-    // const { history } = this.props;
+    const { history } = this.props;
 
-    // history.push({
-    //   pathname: ROUTER_PATHS.ORDERING_PAYMENT,
-    //   search: window.location.search,
-    // });
+    history.push({
+      pathname: ROUTER_PATHS.ORDERING_PAYMENT,
+      search: window.location.search,
+    });
   }
 
   handleUpdateName(e) {
