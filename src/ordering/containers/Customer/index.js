3a21b6ad4b0b56996fc17024766862ab6f1fa6f7
wendylw <wendylw2013@gmail.com>
2020-03-18 11:04:06 +0800

    FB-407: Update UI for phone number

--- src/ordering/containers/Customer/index.js
+++ src/ordering/containers/Customer/index.js
@@ -29,7 +29,7 @@ class Customer extends Component {
   };
 
   async savePhoneNumber() {
-    const { appActions } = this.props;
+    // const { appActions } = this.props;
     const { phone } = this.state;
 
     if (!isValidPhoneNumber(phone)) {
