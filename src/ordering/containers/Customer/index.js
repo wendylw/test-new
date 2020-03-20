9ddde18fc9ca3bf689a7f6c0406f355736c5ba75
wendylw <wendylw2013@gmail.com>
2020-03-20 17:58:56 +0800

    hotfix__beep-store-close: Update continue button UXZ=

--- src/ordering/containers/Customer/index.js
+++ src/ordering/containers/Customer/index.js
@@ -154,8 +154,9 @@ class Customer extends Component {
   }
 
   render() {
-    const { t, history, onlineStoreInfo, deliveryDetails } = this.props;
+    const { t, user, history, onlineStoreInfo, deliveryDetails } = this.props;
     const { asideName, formTextareaTitle } = this.state;
+    const { isFetching } = user || {};
     const { country } = onlineStoreInfo || {};
     const { type } = qs.parse(history.location.search, { ignoreQueryPrefix: true });
     let textareaValue = deliveryDetails.deliverToAddress;
@@ -267,10 +268,11 @@ class Customer extends Component {
                 (type === 'delivery' &&
                   (!Boolean(deliveryDetails.addressDetails) || !Boolean(deliveryDetails.deliverToAddress))) ||
                 !Boolean(deliveryDetails.username) ||
-                !isValidPhoneNumber(deliveryDetails.phone)
+                !isValidPhoneNumber(deliveryDetails.phone) ||
+                isFetching
               }
             >
-              {t('Continue')}
+              {isFetching ? <div className="loader"></div> : t('Continue')}
             </button>
           </div>
         </footer>
