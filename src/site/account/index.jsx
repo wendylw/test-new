760b7c5fe40d15400d4308905bc82719220c19d2
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2020-03-28 05:20:04 +0800

    beepit.co: file structure for beepit.co routes (protected routes)

--- src/site/account/index.jsx
+++ src/site/account/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AccountRoute from './Routes';
+import AuthButton from './components/AuthButton';
 
 const Account = () => {
   return (
     <div>
       <h2>Account/index.jsx pages</h2>
+
+      <AuthButton />
+
       <AccountRoute />
     </div>
   );
