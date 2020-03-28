dc0de9bc57c8e415e00eb6b663326e3a0be7e0f3
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2020-03-28 17:24:14 +0800

    beepit.co: example to pass data between pages

--- src/site/home/index.jsx
+++ src/site/home/index.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-const Home = () => <h2>home/index.jsx page</h2>;
+const Home = () => {
+  const history = useHistory();
+
+  const gotoLocationPage = () => {
+    history.push({
+      pathname: '/ordering/location',
+      state: {
+        from: history.location,
+      },
+    });
+  };
+
+  return (
+    <div>
+      <h2>home/index.jsx page</h2>
+      <button onClick={gotoLocationPage}>Go to Location</button>
+    </div>
+  );
+};
 
 export default Home;
---
+++ src/site/ordering/containers/Location/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Location = () => {
+  return <h2>Location page</h2>;
+};
+
+export default Location;
