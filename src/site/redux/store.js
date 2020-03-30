af38d3a2f441d921d16b6dd2d613b4adb8322d89
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2020-03-30 23:29:51 +0800

    MVP_beep: requestPromise.js to apply to devtools

--- src/site/redux/store.js
+++ src/site/redux/store.js
@@ -3,15 +3,15 @@ import thunk from 'redux-thunk';
 import rootReducer from './modules';
 import api from '../../redux/middlewares/api';
 import apiGql from '../../redux/middlewares/apiGql';
-import request from '../../redux/middlewares/requestPromise';
+import requestPromise from '../../redux/middlewares/requestPromise';
 
 let store;
 
 if (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION__) {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-  store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, apiGql, api)));
+  store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, apiGql, api, requestPromise)));
 } else {
-  store = createStore(rootReducer, applyMiddleware(thunk, apiGql, api, request));
+  store = createStore(rootReducer, applyMiddleware(thunk, apiGql, api, requestPromise));
 }
 
 if (process.env.NODE_ENV !== 'production') {
