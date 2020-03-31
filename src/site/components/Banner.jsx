b2457d329cb33fe2dd5fc6ce0544ded7437b3a35
wendylw <wendylw2013@gmail.com>
2020-03-31 22:52:39 +0800

    release__beep-mvp: Update banner top circle

--- src/site/components/Banner.jsx
+++ src/site/components/Banner.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Banner = ({ children, title }) => {
   return (
     <div className="site-banner padding-left-right-normal absolute-wrapper">
+      <i className="site-banner__circle"></i>
       <h2 className="site-banner__title text-size-huge text-weight-bold">{title}</h2>
       {children}
     </div>
