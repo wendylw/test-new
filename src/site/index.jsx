7c1ba4ca5bd45ccb3047ab3d1678bded9daaa1d1
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2020-03-31 23:33:49 +0800

    MVP_beep: CRM-336: add favicon

--- src/site/index.jsx
+++ src/site/index.jsx
@@ -4,12 +4,15 @@ import { withRouter } from 'react-router-dom';
 import store from './redux/store';
 import SiteApp from './containers/App';
 import './Common.scss';
+import DocumentFavicon from '../components/DocumentFavicon';
+import favicon from '../images/favicon.ico';
 
 class Site extends Component {
   render() {
     return (
       <Provider store={store}>
         <SiteApp />
+        <DocumentFavicon icon={favicon} />
       </Provider>
     );
   }
