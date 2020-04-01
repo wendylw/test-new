d49890c2d4d20c70b2a0f2f3f5837de9ef992aa0
Luke Lu <lukeupup@gmail.com>
2020-04-01 19:21:33 +0800

    add i18n-sync script (#481)

---
+++ i18n-sync.config.js
@@ -0,0 +1,35 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+module.exports = {
+  projectId: '328441',
+  adaptor: {
+    read() {
+      const dirname = path.join(__dirname, 'public/locales/en');
+      const filenames = fs.readdirSync(dirname);
+      const termsMap = {};
+      filenames.forEach(filename => {
+        const key = filename.replace('.json', '');
+        const content = fs.readJSONSync(path.join(dirname, filename));
+        termsMap[key] = content;
+      });
+      return termsMap;
+    },
+
+    write(translations) {
+      const localeDir = path.join(__dirname, 'public/locales');
+      for (const languageCode in translations) {
+        if (languageCode !== 'en') {
+          fs.ensureDirSync(path.join(localeDir, languageCode));
+          const termsMap = translations[languageCode];
+          for (const namespace in termsMap) {
+            fs.writeJSONSync(path.join(localeDir, languageCode, `${namespace}.json`), termsMap[namespace], {
+              spaces: 2,
+              flag: 'w',
+            });
+          }
+        }
+      }
+    },
+  },
+};
