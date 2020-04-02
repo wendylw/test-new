17292eebb94e89c4b0eeb981db4f6b851536e82d
wendylw <wendylw2013@gmail.com>
2020-04-02 11:28:06 +0800

    release__beep-mvp: Remove redundant code

--- src/qrscan/Routes.js
+++ src/qrscan/Routes.js
@@ -4,7 +4,6 @@ import Constants from './Constants';
 import Scan from './containers/Scan';
 import Permission from './containers/Permission';
 import NotSupport from './containers/NotSupport';
-import MvpEntry from './containers/MvpEntry';
 
 const NotFound = () => {
   return <Redirect to={'/'} />;
@@ -16,7 +15,6 @@ const BasicRoute = () => (
       <Route exact path={Constants.ALL_ROUTER.scan} component={Scan} />
       <Route exact path={Constants.ALL_ROUTER.permission} component={Permission} />
       <Route path={Constants.ALL_ROUTER.notSupport} component={NotSupport} />
-      <Route path="/mvp" component={MvpEntry} />
       <Route path={'*'} component={NotFound} />
     </Switch>
   </Router>
