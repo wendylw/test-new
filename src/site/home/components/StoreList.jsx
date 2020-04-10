26846887b2c6a8bc69abbc9513529e57cbf310f2
wendylw <wendylw2013@gmail.com>
2020-04-10 17:44:02 +0800

    CRM-398: Update cashback rate percentage

--- src/site/home/components/StoreList.jsx
+++ src/site/home/components/StoreList.jsx
@@ -45,6 +45,7 @@ class StoreList extends Component {
             enableCashback,
             cashbackRate,
           } = store || {};
+          const cashbackRatePercentage = (Number(cashbackRate) || 0) * 100;
           // const currentStoreStatus = storeStatus[isOpen ? 'open' : 'close'];
 
           return (
@@ -102,7 +103,7 @@ class StoreList extends Component {
                   <div className="flex flex-middle">
                     <IconAttachMoney className="store-info__icon-small icon icon__privacy icon__small text-middle" />
                     <span className="store-info__text text-size-small text-middle">
-                      {t('EnabledCashbackText', { cashbackRate: (Number(cashbackRate) || 0) * 100 })}
+                      {t('EnabledCashbackText', { cashbackRate: Math.round(cashbackRatePercentage * 100) / 100 })}
                     </span>
                   </div>
                 ) : null}
