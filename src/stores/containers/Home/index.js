db14bb65add1a9f6bf10856481090bb628147cd2
Zhang Lidan (Jessica Zhang) <614027581@qq.com>
2020-04-10 15:52:22 +0800

    when store is closed, should go directly to ordering page

--- src/stores/containers/Home/index.js
+++ src/stores/containers/Home/index.js
@@ -50,12 +50,12 @@ class App extends Component {
       allBusinessInfo,
     });
     const isValidTimeToOrder = Utils.isValidTimeToOrder({ validDays, validTimeFrom, validTimeTo });
-    if (isValidTimeToOrder || enablePreOrder) {
+    if (isValidTimeToOrder) {
       homeActions.setCurrentStore(storeId);
     } else {
       await homeActions.getStoreHashData(storeId);
       const { hashCode } = this.props;
-      window.location.href = `${ROUTER_PATHS.ORDERING_BASE}/?h=${hashCode || ''}&type=delivery`;
+      window.location.href = `${ROUTER_PATHS.ORDERING_BASE}/?h=${hashCode || ''}&type=delivery${enablePreOrder ? '&isPreOrder=true' : ''}`;
     }
   }
 
