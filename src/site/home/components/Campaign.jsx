a2f6eca01280e87621f5d756a19947cc89f406e8
ZHONG HUAN <huan.zhong@storehub.com>
2020-04-23 01:45:06 +0800

    CRM-421: remove the validTime function

--- src/site/home/components/Campaign.jsx
+++ src/site/home/components/Campaign.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
 import { IconClose } from '../../../components/Icons';
-import Utils from '../../../utils/utils';
 import banner from './images/Banner.jpg';
 import promoBanner from './images/PromoBanner.jpg';
 import './OfferDetails.scss';
@@ -23,26 +22,9 @@ class Campaign extends Component {
     });
   };
 
-  isCampaignValidTime = () => {
-    // example dates for test:
-    // const current = new Date('2020-04-07 23:59');
-    // const current = new Date('2020-06-01 00:00');
-    // /home?fake.date=2020-04-08 00:00
-    const fakeDate = Utils.getQueryString('fake.date');
-    const current = fakeDate ? new Date(fakeDate) : new Date();
-    const validYear = current.getFullYear() === 2020;
-    const validMonthDate =
-      current.getMonth() >= 3 && current.getDate() >= 21 && current.getMonth() <= 4 && current.getDate() <= 30;
-    return validYear && validMonthDate;
-  };
-
   render() {
     const { t } = this.props;
     const { show } = this.state;
-
-    if (!this.isCampaignValidTime()) {
-      return null;
-    }
 
     return (
       <React.Fragment>
