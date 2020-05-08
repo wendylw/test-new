ef9b3fae4d59677404ea2dfb2689ca5a81157544
wendylw <wendylw2013@gmail.com>
2020-05-08 15:48:30 +0800

    FB-1148: Update ui for contact us

--- src/ordering/containers/ThankYou/index.js
+++ src/ordering/containers/ThankYou/index.js
@@ -651,8 +651,8 @@ export class ThankYou extends PureComponent {
             }
           >
             {isTakeaway ? (
-              <button className="gray-font-opacity" onClick={this.handleNeedHelp}>
-                <span data-testid="thanks__self-pickup text-uppercase">{t('ContactUs')}</span>
+              <button className="link text-uppercase" onClick={this.handleNeedHelp}>
+                <span data-testid="thanks__self-pickup">{t('ContactUs')}</span>
               </button>
             ) : (
               <span className="gray-font-opacity">
