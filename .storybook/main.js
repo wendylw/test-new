0090b86bbce9eccdd47209a0372ccbe7b9b2fc29
Xiaoqiang Wang <xiaoqiang.wang@storehub.com>
2020-05-15 19:04:09 +0800

    FB-503: storybook: add add-on @storybook/addon-docs, to support docs on storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,21 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-storysource',
     '@storybook/addon-viewport/register',
+
+    // docs reference:
+    // https://github.com/storybookjs/storybook/blob/master/addons/docs/docs/docspage.md
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+      },
+    },
   ],
 };
