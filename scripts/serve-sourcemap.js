890dbf6f4106591872a45584fc5c1ba730f0f34d
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2020-06-28 23:02:57 +0800

    FB-453: serve source map files at localhost:3000 by proxying s3 directly

---
+++ scripts/serve-sourcemap.js
@@ -0,0 +1,58 @@
+/**
+ * Proxy *.map from S3 directly as `SourceMappingURL=http://localhost:3000/example.js.map`,
+ * since this file is called by package.json, so usage below.
+ *
+ * Usage:
+ *  > yarn serve:sourcemap --bucket beep-v2-web [--region ap-southeast-1 --port 3000 --proxy http://localhost:7890]
+ *
+ * @type {createApplication}
+ */
+const express = require('express')
+const options = require('yargs').argv
+const s3 = require('./s3')(options)
+const caches = {}; // filename => filecontent
+
+const serve = async (logger, s3, options) => {
+  logger.debug('Enter serve');
+  logger.debug('options = %j', options);
+
+  const app = express()
+  const port = options.port || 3000
+
+  app.get('/', (req, res) => res.json(options))
+
+  app.get('/*.map', (req, res, next) => {
+    const key = req.path.substring(1); // remove head / char
+    logger.debug(`${req.method} ${req.path}`)
+
+    // get from cache first, because the key is always hashed of Source Mapping File by Chunk of Create-React-App
+    if (caches[key]) {
+      logger.debug(`(cached) ${req.method} ${key}`);
+      return res
+        .set('Content-Type', 'application/json')
+        .set('Cache-Control', 'max-age: 31536000, immutable')
+        .send(caches[key]);
+    }
+
+    s3.getObject({
+      Bucket: options.bucket,
+      Key: key,
+    }, (err, data) => {
+      if (err) {
+        logger.error(err);
+        res.status(500).send(err.toString());
+      } else {
+        caches[key] = data.Body;
+        res
+          .set('Content-Type', 'application/json')
+          .set('Cache-Control', 'max-age: 31536000, immutable')
+          .send(data.Body);
+      }
+    });
+  });
+
+  app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+}
+
+
+serve(console, s3, options);
