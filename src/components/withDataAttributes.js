7fcfd62643a7577ce9bdcc624c338601129c7252
Luke Lu <lukeupup@gmail.com>
2020-07-06 18:53:04 +0800

    fix a bug with static attr is not available in hoc

--- src/components/withDataAttributes.js
+++ src/components/withDataAttributes.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import hoistNonReactStatic from 'hoist-non-react-statics';
 /**
  * This is a HOC to help to carry arbitrary data attributes (the attributes that starts with "data-") to inner component.
  */
-export default Component => props => {
-  const dataAttrs = {};
-  Object.keys(props).forEach(propName => {
-    if (/^data-/.test(propName)) {
-      dataAttrs[propName] = props[propName];
-    }
-  });
-  return <Component {...props} dataAttributes={dataAttrs} />;
+export default Component => {
+  const WithDataAttr = props => {
+    const dataAttrs = {};
+    Object.keys(props).forEach(propName => {
+      if (/^data-/.test(propName)) {
+        dataAttrs[propName] = props[propName];
+      }
+    });
+    return <Component {...props} dataAttributes={dataAttrs} />;
+  };
+  hoistNonReactStatic(WithDataAttr, Component);
+  return WithDataAttr;
 };
