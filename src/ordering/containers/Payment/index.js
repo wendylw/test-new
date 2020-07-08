a74d4f74b5734df72c77c26458a652dab1ae36f7
wendylw <wendylw2013@gmail.com>
2020-07-08 12:00:01 +0800

    FB-1376: Update check phone and name logic

--- src/ordering/containers/Payment/index.js
+++ src/ordering/containers/Payment/index.js
@@ -4,7 +4,6 @@ import qs from 'qs';
 import Header from '../../../components/Header';
 import RedirectForm from './components/RedirectForm';
 import CreateOrderButton from '../../components/CreateOrderButton';
-import { fetchDeliveryDetails } from '../Customer/utils';
 import Constants from '../../../utils/constants';
 import config from '../../../config';
 
@@ -39,16 +38,7 @@ class Payment extends Component {
   };
 
   componentDidMount = async () => {
-    const { history, payments, unavailablePaymentList } = this.props;
-    const { phone, username: name } = await fetchDeliveryDetails();
-
-    if (!phone || !name) {
-      history.push({
-        pathname: Constants.ROUTER_PATHS.ORDERING_CUSTOMER_INFO,
-        search: window.location.search,
-      });
-    }
-
+    const { payments, unavailablePaymentList } = this.props;
     const availablePayments = payments.filter(p => !unavailablePaymentList.includes(p.key));
 
     this.props.paymentActions.setCurrentPayment(availablePayments[0].label);
