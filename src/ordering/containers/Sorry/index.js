07de254f03974ebd68c0535148c71ea8ecffbdab
ZHONG HUAN <huan.zhong@storehub.com>
2020-07-14 18:31:07 +0800

    FB-1508: mapping payment failed code

--- src/ordering/containers/Sorry/index.js
+++ src/ordering/containers/Sorry/index.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
 import Constants from '../../../utils/constants';
-
+import Utils from '../../../utils/utils';
 import { connect } from 'react-redux';
 import { bindActionCreators, compose } from 'redux';
 import { actions as appActionCreators } from '../../redux/modules/app';
 import { actions as cartActionCreators, getPendingTransactionIds } from '../../redux/modules/cart';
 
+const PROVIDER_TO_METHOD = {
+  StripeFPX: 'onlineBanking',
+  CCPPMYCreditCard: 'creditCard',
+  GrabPay: 'onlineBanking',
+  CCPPTnGPay: 'onlineBanking',
+  Boost: 'onlineBanking',
+  BeepTHOnlineBanking: 'onlineBanking',
+  BeepTHCreditCard: 'creditCard',
+  BeepTHLinePay: 'onlineBanking',
+  BeepPHCreditCard: 'creditCard',
+  BeepPHCCPPGcash: 'onlineBanking',
+};
+
 class Sorry extends Component {
   async componentDidMount() {
     const { t } = this.props;
+    const description = this.getDescription();
 
     this.props.appActions.showMessageModal({
       message: t('PaymentFailed'),
-      description: t('PaymentFailedDescription'),
+      description: description,
     });
 
     this.props.history.push({
@@ -21,6 +35,30 @@ class Sorry extends Component {
       search: window.location.search,
     });
   }
+
+  getDescription = () => {
+    const params = Utils.getQueryString();
+    const { errorCode, provider } = params || {};
+    const { t } = this.props;
+    const errors = {
+      AmountTooLarge: t('AmountTooLarge'),
+      AmountTooSmall: t('AmountTooSmall'),
+      AuthenticationRequired: t('AuthenticationRequired'),
+      BalanceInsufficient: t('BalanceInsufficient'),
+      CardExpired: t('CardExpired'),
+      BankDeclined: t('BankDeclined'),
+      IncorrectCvc: t('IncorrectCvc'),
+      UnknownError: t('UnknownError'),
+    };
+    const methods = {
+      onlineBanking: t('Online Banking'),
+      creditCard: t('Credit/Debit Card'),
+    };
+    const provideMethod = PROVIDER_TO_METHOD[provider];
+    return errorCode && provider
+      ? t('Description', { paymentMethod: methods[provideMethod], error: errors[errorCode] })
+      : t('PaymentFailedDescription');
+  };
 
   shouldComponentUpdate(nextProps) {
     return !!nextProps.order;
