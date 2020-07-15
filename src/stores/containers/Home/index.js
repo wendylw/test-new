eabc90ad482dadfa04fb7912d3ccdceda7429db3
k-kangstorehub <k.kang@storehub.com>
2020-07-15 15:34:34 +0800

    hotfix-FB-1529

--- src/stores/containers/Home/index.js
+++ src/stores/containers/Home/index.js
@@ -62,7 +62,11 @@ class App extends Component {
     });
     const isValidTimeToOrder = Utils.isValidTimeToOrder({ validDays, validTimeFrom, validTimeTo });
     if (isValidTimeToOrder || enablePreOrder) {
-      window.location.href = `${window.location.href}?s=${storeId}&from=home`;
+      if (window.location.search) {
+        window.location.href = `${window.location.href}&s=${storeId}&from=home`;
+      } else {
+        window.location.href = `${window.location.href}?s=${storeId}&from=home`;
+      }
       // homeActions.setCurrentStore(storeId);
     } else {
       await homeActions.getStoreHashData(storeId);
@@ -74,7 +78,11 @@ class App extends Component {
   }
 
   gotoDine(storeId) {
-    window.location.href = `${window.location.origin}${ROUTER_PATHS.DINE}?s=${storeId}&from=home`;
+    if (window.location.search) {
+      window.location.href = `${window.location.origin}${ROUTER_PATHS.DINE}${window.location.search}&s=${storeId}&from=home`;
+    } else {
+      window.location.href = `${window.location.origin}${ROUTER_PATHS.DINE}?s=${storeId}&from=home`;
+    }
   }
 
   selectStore = storeId => {
