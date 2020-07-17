383e1c112d359fd14299f91e41754bd5455a786e
k-kangstorehub <k.kang@storehub.com>
2020-07-17 11:58:10 +0800

    FB-1460:new beep flow: update select store loic

--- src/ordering/containers/LocationAndDate/index.js
+++ src/ordering/containers/LocationAndDate/index.js
@@ -150,14 +150,23 @@ class LocationAndDate extends Component {
 
   setStoreFromSelect = async () => {
     if (this.state.search.storeid) {
-      await this.props.appActions.loadCoreBusiness(this.state.search.storeid);
-      await this.props.homeActions.getStoreHashData(this.state.search.storeid);
-      await this.props.homeActions.loadCoreStores();
-      this.setMethodsTime();
-
+      console.log(this.props, 'props');
+      if (!this.props.allStore.length) {
+        await this.props.homeActions.loadCoreStores();
+      }
       let store = this.props.allStore.filter(item => item.id === this.state.search.storeid);
+
       this.setState({
         nearlyStore: store[0],
+      });
+      await Promise.all([
+        this.props.appActions.loadCoreBusiness(this.state.search.storeid),
+        this.props.homeActions.getStoreHashData(this.state.search.storeid),
+      ]);
+
+      this.setMethodsTime();
+
+      this.setState({
         h: this.props.storeHash,
       });
     }
