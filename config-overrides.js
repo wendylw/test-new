2076d2a72c0270c7bac5d0cdd7f8fdb65948bf0e
Luke Lu <lukeupup@gmail.com>
2020-07-17 19:41:31 +0800

    test sentry

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,16 @@
-const { override } = require('customize-cra');
+const { override, addWebpackPlugin } = require('customize-cra');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
-const customization = override();
+const customization = override(
+  addWebpackPlugin(
+    new SentryWebpackPlugin({
+      include: '.',
+      ignoreFile: '.sentrycliignore',
+      ignore: ['node_modules', 'webpack.config.js'],
+      configFile: 'sentry.properties',
+    })
+  )
+);
 
 // refer to docs on https://github.com/timarney/react-app-rewired
 customization.jest = require('./jest-config-overrides');
