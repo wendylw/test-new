be94a6b3198ee380d1872a7dc7efe4160fd0aab0
wendylw <wendylw2013@gmail.com>
2020-08-19 12:43:13 +0800

    FB-503: Update original price

--- src/ordering/components/ProductItem/index.js
+++ src/ordering/components/ProductItem/index.js
@@ -34,22 +34,6 @@ export class ProductItem extends Component {
       productItemMinHeight,
       scrollContainer,
     } = this.props;
-    const PricesDom = (
-      <div className="price item__text">
-        {originalDisplayPrice ? (
-          <CurrencyNumber
-            className="text-size-small text-line-through"
-            money={originalDisplayPrice}
-            numberOnly={true}
-          />
-        ) : null}
-        <CurrencyNumber
-          className={`${originalDisplayPrice ? 'text-error' : ''}`}
-          money={price || 0}
-          numberOnly={true}
-        />
-      </div>
-    );
 
     return (
       <Item
