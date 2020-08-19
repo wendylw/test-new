8d0e0beae4cc427e59a36ea7f2e7c259ffc46a61
wendylw <wendylw2013@gmail.com>
2020-08-19 16:56:23 +0800

    FB-503: Update DeliverToBar

--- src/components/DeliverToBar.stories.js
+++ src/components/DeliverToBar.stories.js
@@ -6,9 +6,22 @@ export default {
   component: DeliverToBar,
 };
 
-const Template = args => <DeliverToBar {...args} />;
+// const Template = args => <DeliverToBar {...args} />;
 
-export const Title = Template.bind({});
-Title.args = {
-  title: 'Cookie',
-};
+// export const Title = Template.bind({});
+// Title.args = {
+//   title: 'Cookie',
+// };
+
+export const Template = () => (
+  <DeliverToBar
+    className="ordering-home__deliver-to flex__shrink-fixed"
+    content="SS2, Petaling Jaya, Selangor, Malaysia"
+    backIcon={null}
+    extraInfo="TODAY . immediate"
+    showBackButton={false}
+    icon={null}
+  >
+    {null}
+  </DeliverToBar>
+);
