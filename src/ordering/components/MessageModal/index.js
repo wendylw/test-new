695d838f54b9bb3b2dcd49097e78bd4b92ac97d0
wendylw <wendylw2013@gmail.com>
2020-08-21 17:46:18 +0800

    FB-503: Update message modal layout

--- src/ordering/components/MessageModal/index.js
+++ src/ordering/components/MessageModal/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
+import Modal from '../../components/MessageModal';
 
 class MessageModal extends Component {
   handleClickOK = () => {
@@ -11,29 +12,21 @@ class MessageModal extends Component {
     const { message, description, buttonText } = data;
 
     return (
-      <section
-        className="modal__align-middle modal flex flex-middle flex-space-between"
-        style={styles.section}
-        data-heap-name="ordering.common.message-modal.container"
-      >
-        <div className="modal__content">
-          <header className="hint-modal__header modal__header">
-            <h4 className="text-weight-bolder">{message}</h4>
-          </header>
-          <div className="modal__body">
-            <p className="modal__text">{description}</p>
-          </div>
-          <footer>
-            <button
-              className="button__fill button__block text-weight-bolder"
-              data-heap-name="ordering.common.message-modal.ok-btn"
-              onClick={this.handleClickOK}
-            >
-              {buttonText || t('OK')}
-            </button>
-          </footer>
-        </div>
-      </section>
+      <Modal data-heap-name="ordering.common.message-modal.container">
+        <Modal.Body>
+          <h4 className="text-weight-bolder">{message}</h4>
+          <p className="modal__text">{description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <button
+            className="button button__fill button__block text-weight-bolder"
+            data-heap-name="ordering.common.message-modal.ok-btn"
+            onClick={this.handleClickOK}
+          >
+            {buttonText || t('OK')}
+          </button>
+        </Modal.Footer>
+      </Modal>
     );
   }
 }
