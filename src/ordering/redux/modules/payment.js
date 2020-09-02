423012251cff8d9cc052a59601800c1c0fa49d7b
k-kangstorehub <k.kang@storehub.com>
2020-09-02 12:06:08 +0800

    add map function

--- src/ordering/redux/modules/payment.js
+++ src/ordering/redux/modules/payment.js
@@ -177,7 +177,7 @@ export const actions = {
     } else if (shippingType === DELIVERY_METHOD.DINE_IN || shippingType === DELIVERY_METHOD.TAKE_AWAY) {
       variables = {
         ...variables,
-        shippingType: shippingType === DELIVERY_METHOD.DINE_IN ? 'dine' : shippingType,
+        shippingType: Utils.mapString2camelCase(shippingType),
         contactDetail,
       };
     }
