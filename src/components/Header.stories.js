745537c4722e95aec59356455cc931a681fcc5ea
k-kangstorehub <k.kang@storehub.com>
2020-09-07 17:20:24 +0800

    update storybook [Header, Item, Modal]

---
+++ src/components/Header.stories.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Header, { HeaderComponent } from './Header';
+import { IconDelete, IconEdit, IconLeftArrow, IconLocation } from './Icons';
+import DeliverToBar from './DeliverToBar';
+
+export default {
+  title: 'Common/Header',
+  component: HeaderComponent,
+};
+
+const Template = args => <HeaderComponent {...args} />;
+
+export const PageHeader = Template.bind({});
+PageHeader.args = {
+  className: 'flex-middle border__bottom-divider',
+  isPage: true,
+  title: 'DeliveryDetails',
+  contentClassName: 'flex-middle',
+};
+
+export const PageHeaderWithContent = () => (
+  <HeaderComponent
+    className="flex-middle border__bottom-divider"
+    isPage={true}
+    title="Cart"
+    contentClassName="flex-middle"
+  >
+    <button
+      className="button flex__shrink-fixed padding-top-bottom-smaller padding-left-right-normal"
+      onClick={() => {}}
+    >
+      <IconDelete className="icon icon__normal icon__error text-middle" />
+      <span className="text-middle text-size-big text-error">Clear All</span>
+    </button>
+  </HeaderComponent>
+);
---
+++ src/components/Item.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ItemComponent } from './Item';
+import { IconDelete, IconEdit, IconLeftArrow, IconLocation } from './Icons';
+import DeliverToBar from './DeliverToBar';
+
+export default {
+  title: 'Common/Item',
+  component: ItemComponent,
+};
+
+const Template = args => <ItemComponent {...args} />;
+
+export const PeoductItem = Template.bind({});
+PeoductItem.args = {
+  className: 'flex-middle',
+  title: 'DeliveryDetails',
+  contentClassName: 'flex-middle',
+  variation: 'red, bigger',
+  tagText: 'best sell',
+  operateItemDetail: () => {},
+};
