b23e96bd983c6a5d318cc094756f4049a19b1123
k-kangstorehub <k.kang@storehub.com>
2020-09-27 16:02:54 +0800

    update Tag.stories.js

--- src/components/Tag.stories.js
+++ src/components/Tag.stories.js
@@ -13,23 +13,37 @@ export default {
 
 // The Primary slot is computed from the first user-defined story for the component.
 export const AllTags = () => (
-  <div
-    style={{
-      margin: '0 auto',
-      display: 'flex',
-      justifyContent: 'space-between',
-      flexDirection: 'column',
-      height: '500px',
-      width: '100px',
-    }}
-  >
-    <Tag text="Block Tag" className="tag tag__primary tag__block" />
-    <Tag text="Small Tag" className="tag tag__primary tag__small" />
-    <Tag text="Tag Primary" className="tag tag__primary" />
-    <Tag text="Tag Reverse Primary" className="tag tag__reverse-primary" />
-    <Tag text="Tag Primary Blue" className="tag tag__primary-blue" />
-    <Tag text="Tag Error" className="tag tag__error" />
-    <Tag text="Tag Info" className="tag tag__info" />
-    <Tag text="Tag Default" className="tag tag__default" />
+  <div>
+    <div>
+      <Tag text="Block Tag" className="tag tag__primary tag__block" />
+    </div>
+
+    <div>
+      <Tag text="Small Tag" className="tag tag__primary tag__small" />
+    </div>
+
+    <div>
+      <Tag text="Tag Primary" className="tag tag__primary" />
+    </div>
+
+    <div>
+      <Tag text="Tag Reverse Primary" className="tag tag__reverse-primary" />
+    </div>
+
+    <div>
+      <Tag text="Tag Primary Blue" className="tag tag__primary-blue" />
+    </div>
+
+    <div>
+      <Tag text="Tag Error" className="tag tag__error" />
+    </div>
+
+    <div>
+      <Tag text="Tag Info" className="tag tag__info" />
+    </div>
+
+    <div>
+      <Tag text="Tag Default" className="tag tag__default" />
+    </div>
   </div>
 );
