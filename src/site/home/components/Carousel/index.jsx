9ed0905074195cb31843cb9bc7ec1a871dc4bd85
ZHONG HUAN <huan.zhong@storehub.com>
2020-10-12 14:54:09 +0800

    CRM-808：fix some issue

--- src/site/home/components/Carousel/index.jsx
+++ src/site/home/components/Carousel/index.jsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { withRouter } from 'react-router-dom';
 import Image from '../../../../components/Image';
 import MvpStorePlaceholderImage from '../../../../images/mvp-store-placeholder.jpg';
-import { IconLocalOffer, IconWallet } from '../../../../components/Icons';
+import { IconLocalOffer, IconWallet, IconNext } from '../../../../components/Icons';
 import 'swiper/swiper.scss';
 import './index.scss';
 import { submitStoreMenu } from '../../utils';
@@ -33,6 +33,77 @@ class Carousel extends Component {
     );
   };
 
+  renderCarouselStores(stores) {
+    const { t } = this.props;
+    return (
+      <Swiper className="margin-top-bottom-normal" slidesPerView={'auto'}>
+        {(stores || []).map(store => {
+          const {
+            name,
+            avatar,
+            deliveryFee,
+            isOpen,
+            id,
+            enableFreeShipping,
+            enableCashback,
+            enablePreOrder,
+            cashbackRate,
+          } = store || {};
+
+          const cashbackRatePercentage = (Number(cashbackRate) || 0) * 100;
+
+          return (
+            <SwiperSlide
+              key={id}
+              className="carousel__item margin-top-bottom-smaller border-radius-large"
+              data-heap-name="site.home.carousel.store-item"
+              data-heap-store-name={name}
+              onClick={() => {
+                this.handleStoreClicked(store);
+              }}
+            >
+              <div className="carousel__image-container">
+                {isOpen ? null : this.renderClosedStoreTag(enablePreOrder)}
+                <Image
+                  className="carousel-store__image card__image"
+                  src={avatar}
+                  scalingRatioIndex={1}
+                  placeholderImage={MvpStorePlaceholderImage}
+                  alt={name}
+                />
+              </div>
+              <summary
+                className={`carousel-list__summary padding-left-right-small ${
+                  isOpen || enablePreOrder ? '' : 'text-opacity'
+                }`}
+              >
+                <h3 className="carousel-list__title text-size-bigger text-weight-bolder text-omit__single-line">
+                  {name}
+                </h3>
+                {enableCashback && cashbackRate ? (
+                  <div className="flex flex-middle">
+                    <IconWallet className="icon icon__privacy icon__smaller text-middle" />
+                    <span className="text-size-smaller text-middle text-capitalize">
+                      {t('EnabledCashbackText', {
+                        cashbackRate: Math.round(cashbackRatePercentage * 100) / 100,
+                      })}
+                    </span>
+                  </div>
+                ) : null}
+                {enableFreeShipping && deliveryFee ? (
+                  <div className="flex flex-middle">
+                    <IconLocalOffer className="icon icon__privacy icon__smaller text-middle" />
+                    <span className="text-size-small text-middle">{t('MvpFreeDeliveryPrompt')}</span>
+                  </div>
+                ) : null}
+              </summary>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    );
+  }
+
   render() {
     const { collections, t } = this.props;
     return (
@@ -41,10 +112,10 @@ class Carousel extends Component {
           const { name, stores, urlPath, beepCollectionId } = item;
           return (
             <section key={beepCollectionId}>
-              <div className="flex flex-space-between padding-left-right-normal">
-                <h3 className="text-size-big text-weight-bolder">{name}</h3>
+              <div className="flex flex-space-between flex-middle padding-left-right-normal">
+                <h3 className="text-size-bigger text-weight-bolder">{name}</h3>
                 <span
-                  className="carousel__see-all text-size-big text-weight-bold"
+                  className="carousel__see-all flex flex-middle"
                   data-heap-name="site.home.carousel.see-all-btn"
                   onClick={() => {
                     this.props.history.push({
@@ -52,74 +123,11 @@ class Carousel extends Component {
                     });
                   }}
                 >
-                  {t('SeeAll')} &gt;
+                  <span className="text-size-big text-weight-bolder">{t('SeeAll')}</span>
+                  <IconNext className="icon icon__privacy" />
                 </span>
               </div>
-              <Swiper className="margin-top-bottom-normal" slidesPerView={'auto'}>
-                {(stores || []).map(store => {
-                  const {
-                    name,
-                    avatar,
-                    deliveryFee,
-                    isOpen,
-                    id,
-                    enableFreeShipping,
-                    enableCashback,
-                    enablePreOrder,
-                    cashbackRate,
-                  } = store || {};
-
-                  const cashbackRatePercentage = (Number(cashbackRate) || 0) * 100;
-
-                  return (
-                    <SwiperSlide
-                      key={id}
-                      className="carousel__item border-radius-large"
-                      data-heap-name="site.home.carousel.store-item"
-                      data-heap-store-name={name}
-                      onClick={() => {
-                        this.handleStoreClicked(store);
-                      }}
-                    >
-                      <div className="carousel__image-container">
-                        {isOpen ? null : this.renderClosedStoreTag(enablePreOrder)}
-                        <Image
-                          className="carousel-store__image card__image"
-                          src={avatar}
-                          scalingRatioIndex={1}
-                          placeholderImage={MvpStorePlaceholderImage}
-                          alt={name}
-                        />
-                      </div>
-                      <summary
-                        className={`carousel-list__summary padding-left-right-small ${
-                          isOpen || enablePreOrder ? '' : 'text-opacity'
-                        }`}
-                      >
-                        <h3 className="carousel-list__title text-size-bigger text-weight-bolder text-omit__single-line">
-                          {name}
-                        </h3>
-                        {enableCashback && cashbackRate ? (
-                          <div className="flex flex-middle">
-                            <IconWallet className="icon icon__privacy icon__smaller text-middle" />
-                            <span className="text-size-smaller text-middle text-capitalize">
-                              {t('EnabledCashbackText', {
-                                cashbackRate: Math.round(cashbackRatePercentage * 100) / 100,
-                              })}
-                            </span>
-                          </div>
-                        ) : null}
-                        {enableFreeShipping && deliveryFee ? (
-                          <div className="flex flex-middle">
-                            <IconLocalOffer className="icon icon__privacy icon__smaller text-middle" />
-                            <span className="text-size-small text-middle">{t('MvpFreeDeliveryPrompt')}</span>
-                          </div>
-                        ) : null}
-                      </summary>
-                    </SwiperSlide>
-                  );
-                })}
-              </Swiper>
+              {stores && this.renderCarouselStores(stores)}
             </section>
           );
         })}
