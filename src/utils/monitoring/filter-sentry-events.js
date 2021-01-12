a9d8c76e78c0fc155f76cd76498dfeb23bfca6ae
Luke Lu <lukeupup@gmail.com>
2021-01-12 18:48:18 +0800

    add sentry error filters

--- src/utils/monitoring/filter-sentry-events.js
+++ src/utils/monitoring/filter-sentry-events.js
@@ -6,11 +6,13 @@ export const getErrorMessageFromHint = ({ originalException, syntheticException 
 };
 
 const isInfiniteScrollerBug = (event, hint) => {
+  // This error happens when the user make a fast slide and navigate to another page before the animation
+  // stops. This seems to be a bug on ios safari. And it won't affect real user.
   try {
     const err = event.exception.values[0];
     return (
       /null is not an object \(evaluating '\w+\.scrollHeight'\)/.test(err.value) &&
-      err.mechanism.data.handler === 'bound scrollListener'
+      event.extra.arguments[0].type === 'scroll'
     );
   } catch {
     return false;
@@ -18,6 +20,8 @@ const isInfiniteScrollerBug = (event, hint) => {
 };
 
 const isCrossStorageBug = (event, hint) => {
+  // Some browsers doesn't support cross storage. This is a known issue and will be fixed soon. We
+  // turn off the log to prevent it taking the sentry quota.
   try {
     const message = getErrorMessageFromHint(hint);
     return /CrossStorage/i.test(message);
@@ -26,9 +30,36 @@ const isCrossStorageBug = (event, hint) => {
   }
 };
 
+const isPromiseAllBug = (event, hint) => {
+  // we are keeping receiving this Promise.all is not a function, but it seems that the clients are
+  // very likely spiders (mostly from United States and Ireland). So just ignore the error.
+  try {
+    const err = event.exception.values[0];
+    return /Promise\.all is not a function/.test(err.value);
+  } catch {
+    return false;
+  }
+};
+
+const isBlockFrameBug = (event, hint) => {
+  // Facebook browser seems not supporting cross storage. We will abandon cross storage anyway. We
+  // turn off the log to prevent it taking the sentry quota.
+  try {
+    const err = event.exception.values[0];
+    return /Blocked a frame with origin.*from accessing a cross-origin frame/.test(err.value);
+  } catch {
+    return false;
+  }
+};
+
 const shouldFilter = (event, hint) => {
   try {
-    return isInfiniteScrollerBug(event, hint) || isCrossStorageBug(event, hint);
+    return (
+      isInfiniteScrollerBug(event, hint) ||
+      isCrossStorageBug(event, hint) ||
+      isPromiseAllBug(event, hint) ||
+      isBlockFrameBug(event, hint)
+    );
   } catch {
     return false;
   }
