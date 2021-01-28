1f95dbd49e8f8502f9479a991f8dae78d0e4dda6
liuhuaicheng <huaicheng.liu@storehub.com>
2021-01-28 17:29:39 +0800

    remove propTypes in HybridHeader

--- src/components/HybridHeader.js
+++ src/components/HybridHeader.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import withDataAttributes from './withDataAttributes';
 import './Header.scss';
 import WebHeader from './WebHeader';
@@ -13,23 +12,4 @@ function HybridHeader(props) {
   }
 }
 
-HybridHeader.propTypes = {
-  className: PropTypes.string,
-  style: PropTypes.object,
-  isPage: PropTypes.bool,
-  title: PropTypes.string,
-  navFunc: PropTypes.func,
-  rightContent: PropTypes.object,
-};
-
-HybridHeader.defaultProps = {
-  className: '',
-  style: {},
-  isPage: false,
-  title: '',
-  navFunc: () => {},
-  rightContent: null,
-};
-
-export const HybridHeaderComponent = HybridHeader;
 export default withDataAttributes(HybridHeader);
