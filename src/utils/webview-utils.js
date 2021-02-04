fc45e47ca5baee2d9e171092624107b7447c5f0a
Kaipeng Lu <kaipenglu@gmail.com>
2021-02-04 20:10:00 +0800

    Release beep app 1.3 (#1301)
    * CRM-1336
    
    * CRM-1336: transfrom longitude & latitude
    
    * CRM-1336: fix some bug
    
    * CRM-1336: fix bugs
    
    * add countryCode in beepAddress
    
    * add countryCode
    
    * fix bug
    
    * update
    
    * update grpc proto
    
    * add countryCode in getConsumerBeepAddress
    
    * Fix empty token (#1294)
    
    * Fix empty app token issue
    
    * remove logout
    
    * add test
    
    * add log
    
    * add console.error
    
    * update
    
    * remove beepBrandName
    
    * add debug log
    
    * add debug log
    
    * add debud log
    
    * fix bug
    
    * remove debug code
    
    * add debug log
    
    * remove debug log
    
    * support to show native savedAddress
    
    * FB-2486: Beep bff supports beep app to send otp
    
    * fix add new address bug
    
    * update grpc proto
    
    * remove useless comment
    
    * update addressInfo obj
    
    Co-authored-by: eason.chen <eason.chen@storehub.com>
    Co-authored-by: ZHONG HUAN <huan.zhong@storehub.com>
    Co-authored-by: Huan Zhong <huan_zhong2@126.com>
    Co-authored-by: JiwangFeng <jiwang.feng@storehub.com>

--- src/utils/webview-utils.js
+++ src/utils/webview-utils.js
@@ -1,3 +1,5 @@
+import Utils from './utils';
+
 export const gotoHome = () => {
   if (window.androidInterface) {
     window.androidInterface.gotoHome();
@@ -13,3 +15,42 @@ export const gotoHome = () => {
     }
   }
 };
+
+const getAddressFromNative = () => {
+  try {
+    if (Utils.isAndroidWebview()) {
+      return JSON.parse(window.androidInterface.getAddress());
+    }
+    if (Utils.isIOSWebview()) {
+      return JSON.parse(window.prompt('getAddress'));
+    }
+  } catch {
+    return new Error('Failed to get address from native ');
+  }
+};
+
+export const getLoginStatusFromNative = () => {
+  if (Utils.isAndroidWebview()) {
+    return window.androidInterface.isLogin();
+  } else if (Utils.isIOSWebview()) {
+    try {
+      let iosLoginStr = window.prompt('isLogin');
+      return JSON.parse(iosLoginStr);
+    } catch (e) {
+      console.error('Get login status from app is failed');
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
+
+const hasNativeSavedAddress = () => {
+  if (Utils.isWebview() && sessionStorage.getItem('addressIdFromNative')) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export default { getAddressFromNative, getLoginStatusFromNative, hasNativeSavedAddress };
