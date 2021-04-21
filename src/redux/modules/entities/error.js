0f6ba92a6785657172b8d7a868a466e9ab02c4b9
Luke Lu <lukeupup@gmail.com>
2021-04-21 19:23:54 +0800

    make error #40008 to be shown as popup

--- src/redux/modules/entities/error.js
+++ src/redux/modules/entities/error.js
@@ -6,7 +6,6 @@ const reducer = (state = initialState, action) => {
   switch (code) {
     case '40004':
     case '40005':
-    case 40008:
     case '40011':
       return { ...state, code, message };
     default:
