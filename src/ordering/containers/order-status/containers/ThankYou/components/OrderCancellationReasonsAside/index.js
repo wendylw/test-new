7ae64a184907436e18f545c47bc68ea37e0b0a03
Huaicheng Liu <huaicheng.liu@storehub.com>
2021-04-27 19:43:18 +0800

    add order cancellation reason aside

--- src/ordering/containers/order-status/containers/ThankYou/components/OrderCancellationReasonsAside/index.js
+++ src/ordering/containers/order-status/containers/ThankYou/components/OrderCancellationReasonsAside/index.js
@@ -1,8 +1,60 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Radio from '../../../../../../../components/Radio';
+import { ORDER_CANCELLATION_REASONS } from '../../constants';
 import '../OrderCancellationReasonsAside.scss';
 
+const orderCancellationReasons = [
+  {
+    value: ORDER_CANCELLATION_REASONS.TOO_LONG_DELIVERY_TIME,
+    displayName_transKey: 'DeliveryTimeIsTooLong',
+  },
+  {
+    value: ORDER_CANCELLATION_REASONS.MERCHANT_CALLED_TO_CANCEL,
+    displayName_transKey: 'MerchantCalledToCancel',
+  },
+  {
+    value: ORDER_CANCELLATION_REASONS.WRONG_DELIVERY_INFORMATION,
+    displayName_transKey: 'WrongDeliveryInformation',
+  },
+  {
+    value: ORDER_CANCELLATION_REASONS.ORDERED_WRONG_ITEM,
+    displayName_transKey: 'OrderedWrongItem',
+  },
+  {
+    value: ORDER_CANCELLATION_REASONS.OTHERS,
+    displayName_transKey: 'Others',
+  },
+];
+
 function OrderCancellationReasonsAside() {
-  return <div className="order-cancellation-reasons-aside__container"></div>;
+  const { t } = useTranslation('OrderingThankYou');
+
+  return (
+    <aside className="order-cancellation-reasons-aside aside fixed-wrapper active">
+      <div className="order-cancellation-reasons-aside__container aside__content absolute-wrapper">
+        <div className="order-cancellation-reasons-aside__title text-size-big padding-normal">
+          {t('PleaseSelectCancellationReason')}
+        </div>
+        <div class="order-cancellation-reasons-aside__content-wrapper padding-normal">
+          <ul className="order-cancellation-reasons-aside__content margin-left-right-small">
+            {orderCancellationReasons.map(reason => (
+              <li className="flex flex-space-between flex-middle padding-small" key={reason.value}>
+                <div>{t(reason.displayName_transKey)}</div>
+                <Radio checked />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="order-cancellation-reasons-aside__button-wrapper padding-normal margin-left-right-small">
+          <button className="button button__fill button__block text-weight-bolder text-uppercase">
+            {t('CancelOrder')}
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
 }
 
 export default OrderCancellationReasonsAside;
---
+++ src/ordering/containers/order-status/containers/ThankYou/constants.js
@@ -0,0 +1,8 @@
+export const ORDER_CANCELLATION_REASONS = {
+  TOO_LONG_DELIVERY_TIME: 'tooLongDeliveryTime',
+  MERCHANT_CALLED_TO_CANCEL: 'merchantCalledToCancel',
+  WRONG_DELIVERY_INFORMATION: 'wrongDeliveryInformation',
+  ORDERED_WRONG_ITEM: 'orderedWrongItem',
+  CHANGE_OF_MIND: 'changeOfMind',
+  OTHERS: 'others',
+};
