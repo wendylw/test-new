fbda648f9ac6b1e58ba8d6af6c9c41fa55c7ca1a
wendylw <wendylw2013@gmail.com>
2021-05-11 16:41:25 +0800

    FB-2783: Update switching self pickup modal UI

--- src/ordering/containers/order-status/containers/ThankYou/components/SelfPickUp.jsx
+++ src/ordering/containers/order-status/containers/ThankYou/components/SelfPickUp.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 import { compose } from 'redux';
+
+import Modal from '../../../../../../components/Modal';
 import './SelfPickUp.scss';
 
 function SelfPickUp({ t }) {
+  // const [selfPickUpInfo, setSelfPickUp] = useState;
+
   return (
     <section className="self-pickup">
       <div className="card text-center padding-normal margin-normal">
@@ -16,6 +20,24 @@ function SelfPickUp({ t }) {
           {t('SwitchToSelfPickUp')}
         </button>
       </div>
+      <Modal className="self-pickup__modal" show={false}>
+        <Modal.Body className="text-center padding-small">
+          <h2 className="padding-small text-size-biggest text-line-height-base text-weight-bolder">
+            {t('SwitchToSelfPickUp')}
+          </h2>
+          <p className="self-pickup__modal-description padding-small text-size-big text-line-height-higher">
+            {t('SelfPickUpPromptDescription')}
+          </p>
+        </Modal.Body>
+        <Modal.Footer className="flex flex-stretch">
+          <button className="self-pickup__modal-default-button button button__link flex__fluid-content text-weight-bolder text-uppercase">
+            {t('GoBack')}
+          </button>
+          <button className="self-pickup__modal-fill-button button button__fill flex__fluid-content text-weight-bolder text-uppercase">
+            {t('SelfPickUpPromptConfirmedText')}
+          </button>
+        </Modal.Footer>
+      </Modal>
     </section>
   );
 }
