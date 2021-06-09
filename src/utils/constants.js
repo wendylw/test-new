6c2841b654b4b247069cc7c0e3bd7b65e6d42049
wendylw <wendylw2013@gmail.com>
2021-06-09 16:43:31 +0800

    FB-2875: Remove 54407

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -418,9 +418,6 @@ const PROMOTION_ERROR_CODES = {
   54406: {
     desc: '54406FreePromotionNotMatchCondition',
   },
-  54407: {
-    desc: '54407NotMatchCondition',
-  },
   54403: {
     desc: '54403NotStartOrExpired',
   },
