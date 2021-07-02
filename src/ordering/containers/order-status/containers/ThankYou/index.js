2d830792c384a69b5cf3e1960935567e89da683e
Huaicheng Liu <huaicheng.liu@storehub.com>
2021-07-02 18:49:34 +0800

    BEEP-721: Thank u page, should show 'back' icon, then back to menu page

--- src/ordering/containers/order-status/containers/ThankYou/index.js
+++ src/ordering/containers/order-status/containers/ThankYou/index.js
@@ -1421,7 +1421,7 @@ export class ThankYou extends PureComponent {
       return (
         <NativeHeader
           headerRef={ref => (this.headerEl = ref)}
-          isPage={false}
+          isPage={true}
           title={`#${orderId}`}
           navFunc={() => {
             NativeMethods.gotoHome();
