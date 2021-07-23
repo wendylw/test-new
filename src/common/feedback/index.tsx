f911a4384b3d7ed255f147d8e1730861b3ed7fe1
wendylw <wendylw2013@gmail.com>
2021-07-23 11:28:52 +0800

    FB-2727: Update feeback interface

---
+++ src/common/feedback/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+declare type FEEDBACK_TYPES = 'alert' | 'confirm' | 'toast' | 'fullScreen';
+declare type FeedbackContent = React.ReactNode | string;
+declare type FeedbackOptions = {
+  duration: number | null;
+  type: FEEDBACK_TYPES;
+  onClose?: () => void;
+  icon?: React.ReactNode;
+  key?: string | number;
+};
+export interface FeedbackInstance {
+  alert(content: FeedbackContent, options?: FeedbackOptions);
+}
