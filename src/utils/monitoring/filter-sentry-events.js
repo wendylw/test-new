2fff3879ced06b4a1f136daed0fc1dfa9de0b73a
wendylw <wendylw2013@gmail.com>
2021-07-23 12:43:37 +0800

    FB-2874: [Sentry] TypeError: null is not an object (evaluating g.readyState)

--- src/utils/monitoring/filter-sentry-events.js
+++ src/utils/monitoring/filter-sentry-events.js
@@ -85,6 +85,16 @@ const isTokenExpired = (event, hint) => {
   }
 };
 
+const isGoogleAnalytics = event => {
+  // null is not an object (evaluating 'g.readyState') from https://www.google-analytics.com/analytics.js
+  try {
+    const err = event.exception.values[0];
+    return /null is not an object (evaluating 'g.readyState')/.test(err.value);
+  } catch {
+    return false;
+  }
+};
+
 const shouldFilter = (event, hint) => {
   try {
     return (
