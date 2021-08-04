e8f2baf80c811e6f58adabdb73ac012f714995e0
wendylw <wendylw2013@gmail.com>
2021-08-04 11:52:19 +0800

    FB-2727: Update alert component

---
+++ src/common/feedback/Alert.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { IconClose } from '../../components/Icons';
+import './Feedback.scss';
+
+function Alert(props) {
+  const { content, buttonContent, className, style, close } = props;
+
+  return (
+    <div className={`${className ? ` ${className}` : ''}`} style={style}>
+      {content}
+      <button onClick={() => close()}>{buttonContent || <IconClose />}</button>
+    </div>
+  );
+}
+
+Alert.displayName = 'Alert';
+
+Alert.propTypes = {
+  content: PropTypes.string,
+  buttonContent: PropTypes.node,
+  className: PropTypes.string,
+  // eslint-disable-next-line react/forbid-prop-types
+  style: PropTypes.object,
+  close: PropTypes.func,
+};
+
+Alert.defaultProps = {
+  content: null,
+  buttonContent: null,
+  className: null,
+  style: {},
+  close: () => {},
+};
+
+export default Alert;
