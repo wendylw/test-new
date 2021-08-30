3e66e7358da764b590398baef2775d25de8905bc
Huaicheng Liu <huaicheng.liu@storehub.com>
2021-08-30 13:12:16 +0800

    rename initialDevTools to initDevTools

--- src/utils/dev-tools.js
+++ src/utils/dev-tools.js
@@ -23,7 +23,7 @@ const isScriptLoaded = () => !!window.eruda;
 // eslint-disable-next-line no-underscore-dangle
 const isDevToolsInitiated = () => !!(window.eruda && window.eruda._isInit === true);
 
-export const initialDevTools = () => {
+export const initDevTools = () => {
   const expireDate = new Date();
   expireDate.setDate(expireDate.getDate() + 7);
   document.cookie = `sh_devtools_enabled=true; domain=${mainDomain}; expires=${expireDate.toUTCString()}`;
@@ -46,5 +46,5 @@ export const toggleDevTools = () => {
     return;
   }
 
-  initialDevTools();
+  initDevTools();
 };
