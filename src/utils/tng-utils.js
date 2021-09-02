aca4baa1aaecdef87332d434452ce16bd3ae8f81
wendylw <wendylw2013@gmail.com>
2021-09-02 14:25:05 +0800

    FB-3047: Update form post

--- src/utils/tng-utils.js
+++ src/utils/tng-utils.js
@@ -88,11 +88,35 @@ export const callTradePay = () => {
   try {
     window.my.tradePay({
       paymentUrl:
-        'https://m-sd.tngdigital.com.my/s/cashier/index.html?bizNo=20210902111212800110171537101302190&timestamp=1630549491093&merchantId=217120000000855805277&sign=m9oeZKCz%252FP3auNNUId2JAhDAkiEAj0VdK4YBy2UnFpkZaV9buHffKdm1w%252BTnNR%252FG4GCbVDefPNIuwIDrB%252B5s8l%252Fkl3wur2os4PTfnKnWsIKd8%252FFoVDy5lN%252FfDxmXhoHFkuXiQSQxvamthLkS42ttIuH1ZOhvDYMoZOxeQVIV2UQpeF3fiXq3V1MQLNBnNh%252FCaY4dBqhcnqlUNL424hXjcT90cIER2SoGyK7y9NJsYXEPWGZ%252FhQD0cilbBsAkX6TEOQwm2EtbOU%252BU0PeyItAGm3JuN7USGxaxlqNPN8pZIEx%252FanNcrFUOn4VK6ZbfPyY6WpsICLDvTgdl6D4Oa%252FzvmQ%253D%253D',
+        'https://m-sd.tngdigital.com.my/s/cashier/index.html?bizNo=20210902111212800110171912901303918&timestamp=1630556110815&merchantId=217120000000855805277&sign=WNaoo1NV7Vhx9oxfK%252Bbis9bd2x9Oi0MTDVmkZHsvdHdqvOxCin%252BJgipzCZSeSAbTQt22Fk1vc%252Bb0zVTWMa%252FOrYzpE76%252FxIKBGhgk5ZTz9xIdamS6BQX8kLUnJL5KgEmYL7m1HV8qzRIkPMcoh2Uwvt9gLyVQdwzyDkFtUjsdIVemObC9xjUynxW0%252F9QAUdUlYJdvUq6GakmJYaozcuqBF7xsj4qc6NKa5TxuiAr%252FZKbQ0JuAoZ4cUY0lQisSOXMixkXycip7Jso5R52a%252B0MAnnLZzJwMuafPf8R939%252BuCppayesxdOoC5zhhFLDmnJqwrS0RK3PBFAZU7D72EYsgzg%253D%253D',
       success: res => {
         window.my.alert({
           content: JSON.stringify(res),
         });
+
+        const form = document.createElement('form');
+        const data = {
+          receiptNumber: '1630556109711',
+          paymentId: '20210902111212800110171912901303918',
+          paymentMethod: 'TngMiniProgram',
+          status: 'Success',
+          amount: '0.01',
+        };
+        form.action = process.env.REACT_APP_STOREHUB_PAYMENT_RESPONSE_URL;
+        form.method = 'POST';
+        form.style.height = 0;
+        form.style.width = 0;
+        form.style.overflow = 'hidden';
+        form.style.visibility = 'hidden';
+        Object.keys(data).forEach(key => {
+          const input = document.createElement('input');
+          input.name = key;
+          input.value = data[key];
+          input.type = 'hidden';
+          form.appendChild(input);
+        });
+        document.body.appendChild(form);
+        form.submit();
       },
       fail: res => {
         window.my.alert({
