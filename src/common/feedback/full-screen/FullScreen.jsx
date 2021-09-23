0ab0290bc95c4ed18af0b61cd4ffff716818194e
wendylw <wendylw2013@gmail.com>
2021-09-23 17:05:22 +0800

    FB-3062: initial full-screen commit

---
+++ src/common/feedback/full-screen/FullScreen.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useImperativeHandle, forwardRef } from 'react';
+import { usePrevious } from 'react-use';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import { withBackButtonSupport } from '../../../utils/modal-back-button-support';
+import './FullScreen.scss';
+
+const Alert = forwardRef((props, ref) => {
+  const { t } = useTranslation();
+  const { content, show, closeButtonContent, className, style, onClose, onModalVisibilityChanged } = props;
+  useImperativeHandle(ref, () => ({
+    onHistoryBackReceived: () => false,
+  }));
+  const prevShow = usePrevious(show);
+  useEffect(() => {
+    if (show !== prevShow) {
+      onModalVisibilityChanged(show);
+    }
+  }, [show, onModalVisibilityChanged, prevShow]);
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className={`alert absolute-wrapper flex flex-column flex-middle flex-center ${className}`} style={style}>
+      <div className="alert__content border-radius-large">
+        <div className="alert__body text-center padding-small">{content}</div>
+        <div className="padding-small">
+          {/* TODO： close button UI will be customize */}
+          <button className="button button__fill button__block text-weight-bolder" onClick={onClose}>
+            {closeButtonContent || t('OK')}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+Alert.displayName = 'Alert';
+
+Alert.propTypes = {
+  content: PropTypes.node,
+  show: PropTypes.bool,
+  closeButtonContent: PropTypes.node,
+  className: PropTypes.string,
+  // eslint-disable-next-line react/forbid-prop-types
+  style: PropTypes.object,
+  onClose: PropTypes.func,
+  onModalVisibilityChanged: PropTypes.func,
+};
+
+Alert.defaultProps = {
+  content: null,
+  show: false,
+  closeButtonContent: null,
+  className: '',
+  style: {},
+  onClose: () => {},
+  onModalVisibilityChanged: () => {},
+};
+
+export default withBackButtonSupport(Alert);
---
+++ src/common/feedback/full-screen/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { render } from 'react-dom';
+import { destroyTarget } from '../utils';
+import Alert from './FullScreen';
+import '../Feedback.scss';
+
+const STATUS_LIST = ['error', 'warning', 'info', 'success'];
+
+const AlertStandardContent = ({ content, title }) => (
+  // eslint-disable-next-line react/jsx-filename-extension
+  <>
+    {title ? <h4 className="padding-small text-size-biggest text-weight-bolder">{title}</h4> : null}
+    {content ? <div className="padding-top-bottom-small">{content}</div> : null}
+  </>
+);
+AlertStandardContent.displayName = 'AlertStandardContent';
+AlertStandardContent.propTypes = {
+  content: PropTypes.node,
+  title: PropTypes.string,
+};
+AlertStandardContent.defaultProps = {
+  content: null,
+  title: null,
+};
+const normalizeAlertOptions = options => ({
+  container: document.body,
+  show: true,
+  closeButtonContent: null,
+  className: '',
+  style: {},
+  onClose: () => {},
+  ...options,
+});
+const createAlert = (content, options) =>
+  new Promise(resolve => {
+    const { container, onClose, ...restOptions } = options;
+    const rootDOM = document.createElement('div');
+
+    rootDOM.setAttribute('class', 'feedback__container fixed-wrapper');
+    container.appendChild(rootDOM);
+
+    const alertInstance = React.createElement(Alert, {
+      content,
+      ...restOptions,
+      onClose: () => {
+        render(React.cloneElement(alertInstance, { show: false }), rootDOM, () => {
+          destroyTarget(rootDOM);
+          onClose();
+          resolve();
+        });
+      },
+    });
+
+    render(alertInstance, rootDOM);
+  });
+
+export const alert = (content, options = {}) => {
+  const { title, ...restOptions } = options;
+
+  return createAlert(<AlertStandardContent content={content} title={title} />, normalizeAlertOptions(restOptions));
+};
+
+alert.raw = (content, options = {}) => createAlert(content, normalizeAlertOptions(options));
