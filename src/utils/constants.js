d3746f948b8fb0c19db191db94d5a76348107f6c
Huaicheng Liu <huaicheng.liu@storehub.com>
2021-09-23 14:26:25 +0800

    change tng source of registration to "BeepTngMiniProgram"

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -662,7 +662,7 @@ export const REGISTRATION_SOURCE = {
   RECEIPT: 'Receipt',
   BEEP_STORE: 'BeepStore',
   BEEP_SITE: 'BeepSite',
-  TNGD_MINI_PROGRAM: 'TNGD Mini Program',
+  TNGD_MINI_PROGRAM: 'BeepTngMiniProgram',
 };
 
 export const API_REQUEST_STATUS = {
