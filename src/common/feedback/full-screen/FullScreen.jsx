3d62c6638810032a6bca0274be4b9dd674e63103
wendylw <wendylw2013@gmail.com>
2021-09-28 11:52:09 +0800

    FB-3062: Update button group

--- src/common/feedback/full-screen/FullScreen.jsx
+++ src/common/feedback/full-screen/FullScreen.jsx
@@ -3,7 +3,7 @@ import { usePrevious } from 'react-use';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { withBackButtonSupport } from '../../../utils/modal-back-button-support';
-import { FEEDBACK_STATUS, BUTTONS_STYLE_TYPES } from '../utils';
+import { BUTTONS_STYLE_TYPES } from '../utils';
 import './FullScreen.scss';
 
 const FullScreen = forwardRef((props, ref) => {
@@ -28,23 +28,27 @@ const FullScreen = forwardRef((props, ref) => {
       <div className="full-screen__content">
         <div className="full-screen__body text-center">{content}</div>
       </div>
-      <div className="full-screen__button-group padding-small text-center">
+      <div className="full-screen__button-group">
         {buttons.map(({ type, content: buttonContent, onClick }) => (
-          <button
-            key={`full-screen-button-${content}-type`}
-            className={`button button__${type} button__block padding-left-right-normal text-uppercase text-weight-bolder`}
-            onClick={onClick}
-          >
-            {buttonContent}
-          </button>
+          <div className="margin-small">
+            <button
+              key={`full-screen-button-${content}-type`}
+              className={`button button__${type} button__block padding-left-right-normal text-uppercase text-weight-bolder`}
+              onClick={onClick}
+            >
+              {buttonContent}
+            </button>
+          </div>
         ))}
         {/* TODO： close button UI will be customize */}
-        <button
-          className="button button__fill button__block padding-left-right-normal text-uppercase text-weight-bolder"
-          onClick={onClose}
-        >
-          {closeButtonContent || t('BackToHome')}
-        </button>
+        <div className="margin-small">
+          <button
+            className="button button__fill button__block padding-left-right-normal text-uppercase text-weight-bolder"
+            onClick={onClose}
+          >
+            {closeButtonContent || t('BackToHome')}
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -53,8 +57,6 @@ const FullScreen = forwardRef((props, ref) => {
 FullScreen.displayName = 'FullScreen';
 
 FullScreen.propTypes = {
-  status: PropTypes.oneOf(Object.values(FEEDBACK_STATUS)),
-  image: PropTypes.node,
   content: PropTypes.node,
   buttons: PropTypes.arrayOf(
     PropTypes.shape({
@@ -73,8 +75,6 @@ FullScreen.propTypes = {
 };
 
 FullScreen.defaultProps = {
-  status: null,
-  image: null,
   content: null,
   buttons: [],
   show: false,
