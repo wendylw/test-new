0693c2081dfd62dcffb65907881ce8b7c43dacaf
Haojun Fu <haojun.fu@storehub.com>
2021-09-29 08:10:36 +0800

    remove if choose a new location's if

--- src/ordering/containers/Customer/containers/AddressDetail/redux/thunk.js
+++ src/ordering/containers/Customer/containers/AddressDetail/redux/thunk.js
@@ -1,12 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getDeliveryDetails } from '../../../../../redux/modules/app';
 import Utils from '../../../../../../utils/utils';
-import { getAddressInfo } from './selectors';
 
 export const init = createAsyncThunk('ordering/customer/addressDetail', ({ actionType }, { getState }) => {
   const state = getState();
   const deliveryDetails = getDeliveryDetails(state);
-  const addressInfo = getAddressInfo(state);
   const {
     addressId,
     addressName,
@@ -19,7 +17,6 @@ export const init = createAsyncThunk('ordering/customer/addressDetail', ({ actio
     deliveryToCity,
   } = deliveryDetails || {};
 
-  const { address: savedAddress, coords: savedCoords, addressComponents: savedAddressComponents } = addressInfo || {};
   const { address, coords, addressComponents } = JSON.parse(Utils.getSessionVariable('deliveryAddress') || '{}');
   const payload = {
     id: '',
@@ -35,19 +32,12 @@ export const init = createAsyncThunk('ordering/customer/addressDetail', ({ actio
   };
 
   // if choose a new location, update the addressInfo
-  if (
-    address !== savedAddress ||
-    coords.lat !== savedCoords.latitude ||
-    coords.lng !== savedCoords.longitude ||
-    addressComponents.city !== savedAddressComponents.city
-  ) {
-    payload.address = address;
-    payload.coords = {
-      latitude: coords.lat,
-      longitude: coords.lng,
-    };
-    payload.addressComponents = addressComponents;
-  }
+  payload.address = address;
+  payload.coords = {
+    latitude: coords.lat,
+    longitude: coords.lng,
+  };
+  payload.addressComponents = addressComponents;
 
   if (actionType === 'edit') {
     payload.id = addressId;
