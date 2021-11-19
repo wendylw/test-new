7c8605243de3816a237782db877546f286356a04
Charles Wang <charles.wang@storehub.com>
2021-11-19 11:02:08 +0800

    init commit

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -2,175 +2,57 @@ import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import './LiveChat.scss';
-import Utils from '../utils/utils';
-import _isFunction from 'lodash/isFunction';
-
-const zendeskDepartmentId = Number(process.env.REACT_APP_ZENDESK_DEPARTMENT_ID);
 class LiveChat extends Component {
-  state = { waitingZendeskScript: false };
+  state = { waitingZendeskScript: false, orderSent: false };
 
   componentDidMount() {
-    this.loadZendeskWidget().then(() => {
-      window.zE('webWidget', 'show');
-      this.prefillUserInfo();
-    });
+    window.intercomSettings = {
+      app_id: 'v2axofpf',
+      custom_launcher_selector: '#beep-live-chat-launcher',
+    };
+    (function() {
+      var w = window;
+      var ic = w.Intercom;
+      if (typeof ic === 'function') {
+        ic('reattach_activator');
+        ic('update', w.intercomSettings);
+      } else {
+        var d = document;
+        var i = function() {
+          i.c(arguments);
+        };
+        i.q = [];
+        i.c = function(args) {
+          i.q.push(args);
+        };
+        w.Intercom = i;
+        var l = function() {
+          var s = d.createElement('script');
+          s.type = 'text/javascript';
+          s.async = true;
+          s.src = 'https://widget.intercom.io/widget/v2axofpf';
+          var x = d.getElementsByTagName('script')[0];
+          x.parentNode.insertBefore(s, x);
+        };
+        if (document.readyState === 'complete') {
+          l();
+        } else if (w.attachEvent) {
+          w.attachEvent('onload', l);
+        } else {
+          w.addEventListener('load', l, false);
+        }
+      }
+    })();
   }
 
-  componentDidUpdate(prevProps) {
-    const { name, phone, email } = this.props;
-    if (prevProps.name !== name || prevProps.phone !== phone || prevProps.email !== email) {
-      this.prefillUserInfo();
-    }
-  }
-
-  prefillUserInfo = () => {
-    const { name, phone, email } = this.props;
-
-    window.zE &&
-      window.zE('webWidget', 'prefill', {
-        name: {
-          value: name || '',
-        },
-        phone: {
-          value: phone || '',
-        },
-        email: {
-          value: email || '',
-        },
-      });
-  };
-
-  componentWillUnmount() {
-    window.zE && window.zE('webWidget', 'hide');
-  }
-
-  sendOrderInfo = () => {
-    const { orderId, name, phone } = this.props;
-    window.zE('webWidget', 'chat:send', `Order number: ${orderId}\r\nCustomer name: ${name}\r\nPhone number: ${phone}`);
-  };
-
-  setOfflineFormGreeting = () => {
-    this.setState({ waitingZendeskScript: false });
-    const department = window.zE('webWidget:get', 'chat:department', zendeskDepartmentId);
-    if (department.status !== 'online') {
-      window.zE('webWidget', 'updateSettings', {
-        webWidget: {
-          chat: {
-            prechatForm: {
-              greeting: {
-                '*': `Sorry, we aren't online at the moment. Leave a message and we'll get back to you.`,
-              },
-            },
-          },
-        },
-      });
-    }
-  };
-
-  initZendesk = () => {
-    const { t } = this.props;
-    const isMobile = Utils.getUserAgentInfo().isMobile;
-    if (isMobile) {
-      window.zESettings = {
-        webWidget: {
-          navigation: {
-            popoutButton: {
-              enabled: false,
-            },
-          },
-          position: {
-            horizontal: 'right',
-            vertical: 'top',
-          },
-          offset: {
-            mobile: {
-              horizontal: '-200px',
-            },
-          },
-          color: {
-            launcher: '#FFFFFF',
-            launcherText: '#00b0ff',
-          },
-          launcher: {
-            mobile: {
-              labelVisible: true,
-            },
-            chatLabel: {
-              '*': `${t('NeedHelp')}?`,
-            },
-          },
-          chat: {
-            connectOnPageLoad: false,
-            title: {
-              '*': 'Beep Live Chat',
-            },
-            departments: {
-              enabled: [zendeskDepartmentId],
-              select: zendeskDepartmentId,
-            },
-          },
-        },
-      };
-    } else {
-      window.zESettings = {
-        webWidget: {
-          navigation: {
-            popoutButton: {
-              enabled: false,
-            },
-          },
-          launcher: {
-            chatLabel: {
-              '*': `${t('NeedHelp')}?`,
-            },
-          },
-          chat: {
-            title: {
-              '*': 'Beep Live Chat',
-            },
-            departments: {
-              enabled: [zendeskDepartmentId],
-              select: zendeskDepartmentId,
-            },
-          },
-        },
-      };
-    }
-    window.zE('webWidget:on', 'chat:start', this.sendOrderInfo);
-    window.zE('webWidget:on', 'chat:connected', this.setOfflineFormGreeting);
-  };
-
-  loadZendeskWidget = () => {
-    if (!window.zE) {
-      const zendeskScript = document.createElement('script');
-      zendeskScript.src = process.env.REACT_APP_ZENDESK_SCRIPT_URL;
-      zendeskScript.id = process.env.REACT_APP_ZENDESK_SCRIPT_ID;
-      zendeskScript.async = true;
-      zendeskScript.defer = true;
-      const promisify = new Promise((resolve, reject) => {
-        zendeskScript.onload = resolve;
-        zendeskScript.onerror = reject;
-      }).then(() => {
-        this.initZendesk();
-      });
-      document.body.appendChild(zendeskScript);
-      return promisify;
-    }
-
-    return Promise.resolve();
-  };
+  componentWillUnmount() {}
 
   handleClick = () => {
-    const { onClick } = this.props;
-    const { waitingZendeskScript } = this.state;
-
-    _isFunction(onClick) && onClick();
-
-    if (!waitingZendeskScript) {
-      window.zE('webWidget', 'toggle');
-      if (!window.$zopim?.livechat) {
-        this.setState({ waitingZendeskScript: true });
-      }
+    const { orderId, name, phone } = this.props;
+    const { orderSent } = this.state;
+    if (!orderSent) {
+      window.Intercom('showNewMessage', `Order number: ${orderId}\r\nCustomer name: ${name}\r\nPhone number: ${phone}`);
+      this.setState({ orderSent: true });
     }
   };
 
@@ -184,6 +66,7 @@ class LiveChat extends Component {
           waitingZendeskScript ? 'text-opacity' : ''
         }`}
         onClick={this.handleClick}
+        id="beep-live-chat-launcher"
       >
         {waitingZendeskScript && <div className="loader live-chat__loader margin-left-right-smaller"></div>}
         <div className="live-chat__loading-text margin-left-right-smaller">{`${t('NeedHelp')}?`}</div>
