5b8de507dcac595edda254ead5e52d27855c1a54
wendylw <wendylw2013@gmail.com>
2021-11-22 16:11:55 +0800

    FB-3266: Update table summary ui file

---
+++ src/ordering/containers/order-status/containers/TableSummary/index.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+export class TableSummary extends React.Component {
+  render() {
+    return <section className="flex flex-column" data-heap-name="ordering.order-status.table-summary.container" />;
+  }
+}
+
+TableSummary.displayName = 'TableSummary';
+
+export default TableSummary;
