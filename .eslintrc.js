0849423fe280e8b15e122a092b981c867f83a1b3
wendylw <wendylw2013@gmail.com>
2021-12-06 15:16:42 +0800

    FB-3266: Update PayFirst component eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -88,7 +88,6 @@ const legacyFiles = [
   'src/ordering/containers/shopping-cart/components/CartList.jsx',
   'src/ordering/containers/shopping-cart/components/ProductSoldOutModal/index.js',
   'src/ordering/containers/shopping-cart/components/CartEmptyResult/index.js',
-  'src/ordering/containers/shopping-cart/containers/Cart/PayFirst.jsx',
   'src/ordering/containers/shopping-cart/containers/Cart/PayLater.jsx',
   'src/ordering/containers/shopping-cart/containers/CartSubmissionStatus/index.jsx',
   'src/ordering/containers/Customer/components/AddressChangeModal/index.js',
