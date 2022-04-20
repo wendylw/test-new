162e2cb3adbbd7c3d8b9832174d9508a8cb4bb54
wendylin <wendylw2013@gmail.com>
2022-04-20 16:31:41 +0800

    FB-3777: Update store list

--- src/ordering/containers/food-court/redux/common/store-list.js
+++ src/ordering/containers/food-court/redux/common/store-list.js
@@ -1,5 +1,44 @@
 export const AllFoodCourtStoreList = [
   {
+    id: '5e12c66eee8fd200068b50d7',
+    image: 'https://d2ncjxd2rk2vpl.cloudfront.net/mariassteakcafe/beep/logo/6e9ee2ba-9585-4718-ae1d-e7ed425bafdf',
+    title: 'jw',
+    tags: ['Pork-Free', 'Grill', 'Steak', 'Pasta', 'Chicken'],
+  },
+  {
+    id: '608983ec761b420006b90f80',
+    image:
+      'https://d2ncjxd2rk2vpl.cloudfront.net/salakualalumpur/product/5c2612b8b331de69c87a1bf5/64d491a5-b4e5-47c9-dc4d-22e8c6f1dff1',
+    title: 'jw',
+    tags: ['Fusion', 'Nasi Lemak', 'Cold Desserts', 'Taco'],
+  },
+  {
+    id: '60ed669916c14b0006720d09',
+    image:
+      'https://d2ncjxd2rk2vpl.cloudfront.net/okonomi/product/611cad4155f0e8000606d099/495d616f-28eb-488d-c4a8-9c1994da8529',
+    title: 'jw',
+  },
+  {
+    id: '5e15be72470b493f40485311',
+    image:
+      'https://d2ncjxd2rk2vpl.cloudfront.net/doplohtiga/product/6020bfe8c3b3c4000634ce40/0d995e6c-ddf7-44d2-90da-4c5b22f41307',
+    title: 'hc',
+    tags: [],
+  },
+  {
+    id: '5e15be72470b493f40485323',
+    image: 'https://d2ncjxd2rk2vpl.cloudfront.net/haidilao/beep/logo/2eb0e76a-0635-4a7b-9240-cf40b1f8fecc',
+    title: 'hc',
+    tags: ['Pork-Free', 'Fast Food', 'Burgers', 'Beef', 'Chicken'],
+  },
+  {
+    id: '5f0ff2f4830934000640b230',
+    image:
+      'https://d2ncjxd2rk2vpl.cloudfront.net/finchkl/product/615f1335046c6700066df6df/30ca06d6-a7c7-492d-9ca1-ac8424076534',
+    title: 'hc',
+    tags: ['Chinese', 'Hot pot', 'Pork', 'Seafood'],
+  },
+  {
     id: '5fe1980b9645e00006d01323',
     image: 'https://d2ncjxd2rk2vpl.cloudfront.net/mariassteakcafe/beep/logo/6e9ee2ba-9585-4718-ae1d-e7ed425bafdf',
     title: "Maria's Signature KLCC",
