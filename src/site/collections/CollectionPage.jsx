e3e06f7a82366b58f5d32ac8058c6e916f2fd7d6
Philly Cai <philly.cai@storehub.com>
2022-05-31 14:52:03 +0800

    BEEP-2482: Make sort & filter workable on collection page

--- src/site/collections/CollectionPage.jsx
+++ src/site/collections/CollectionPage.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { bindActionCreators, compose } from 'redux';
 import { connect } from 'react-redux';
-import { withTranslation } from 'react-i18next';
+import { withTranslation, Trans } from 'react-i18next';
 import { CaretLeft, X } from 'phosphor-react';
 import _get from 'lodash/get';
 import StoreList from '../components/StoreList';
 import StoreListAutoScroll from '../components/StoreListAutoScroll';
-import { collectionsActions, getPageInfo, getStoreList, getShippingType } from '../redux/modules/collections';
+import { actions as collectionsActions } from '../redux/modules/collections';
+import {
+  getPageInfo,
+  getStoreList,
+  getShippingType,
+  getShouldLoadStoreList,
+  getShouldShowPageLoader,
+  getShouldShowStoreListLoader,
+  getShouldShowNoFilteredResultPage,
+} from '../redux/modules/collections/selectors';
+import { resetPageInfo, setShippingType, loadStoreList } from '../redux/modules/collections/thunks';
 import { submitStoreMenu } from '../home/utils';
 import { rootActionCreators } from '../redux/modules';
 import { getStoreLinkInfo, homeActionCreators } from '../redux/modules/home';
@@ -30,8 +40,8 @@ import {
 } from '../redux/modules/filter/selectors';
 import { loadSearchOptionList, backUpSearchOptionList, resetSearchOptionList } from '../redux/modules/filter/thunks';
 import { TYPES, IDS, FILTER_DRAWER_SUPPORT_TYPES, FILTER_BACKUP_STORAGE_KEYS } from '../redux/modules/filter/constants';
+import { SHIPPING_TYPES } from '../../common/utils/constants';
 import { isSameAddressCoords, scrollTopPosition } from '../utils';
-import constants from '../../utils/constants';
 import CleverTap from '../../utils/clevertap';
 import FilterBar from '../components/FilterBar';
 import Drawer from '../../common/components/Drawer';
@@ -39,9 +49,8 @@ import Button from '../../common/components/Button';
 import OptionSelector from '../components/OptionSelector';
 import ErrorComponent from '../../components/Error';
 import PageLoader from '../../../src/components/PageLoader';
+import BeepNotResultImage from '../../images/beep-no-results.svg';
 import styles from './CollectionPage.module.scss';
-
-const { API_REQUEST_STATUS } = constants;
 
 class CollectionPage extends React.Component {
   renderId = `${Date.now()}`;
@@ -71,11 +80,13 @@ class CollectionPage extends React.Component {
     }
 
     const { currentCollection } = this.props;
-    const { urlPath, name, beepCollectionId } = currentCollection;
+    const { name, beepCollectionId } = currentCollection;
     if (!hasReduxCache) {
-      this.resetCollectionData();
+      await this.resetCollectionData();
     }
-    this.props.collectionsActions.getStoreList(urlPath);
+
+    this.loadStoreListIfNeeded();
+
     CleverTap.pushEvent('Collection Page - View Collection Page', {
       'collection name': name,
       'collection id': beepCollectionId,
@@ -83,53 +94,90 @@ class CollectionPage extends React.Component {
   };
 
   componentDidUpdate = async prevProps => {
-    const { addressCoords: prevAddressCoords, filterOptionParams: prevFilterOptionParams } = prevProps;
-    const { addressCoords: currAddressCoords, filterOptionParams: currFilterOptionParams } = this.props;
+    const {
+      addressCoords: prevAddressCoords,
+      filterOptionParams: prevFilterOptionParams,
+      shippingType: prevShippingType,
+    } = prevProps;
+    const {
+      addressCoords: currAddressCoords,
+      filterOptionParams: currFilterOptionParams,
+      shippingType: currShippingType,
+    } = this.props;
 
+    const hasAddressCoordsChanged = !isSameAddressCoords(prevAddressCoords, currAddressCoords);
     const hasFilterOptionParamsChanged = prevFilterOptionParams !== currFilterOptionParams;
+    // Exclude shipping type is null for avoiding store list reloading sake.
+    const hasShippingTypeChanged = !!prevShippingType && prevShippingType !== currShippingType;
+    const shouldReloadStoreList = hasAddressCoordsChanged || hasFilterOptionParamsChanged || hasShippingTypeChanged;
 
-    if (!isSameAddressCoords(prevAddressCoords, currAddressCoords)) {
-      scrollTopPosition(this.sectionRef.current);
-      await this.reloadStoreListIfNeeded();
+    if (shouldReloadStoreList) {
+      if (hasAddressCoordsChanged) {
+        const { match, collectionCardActions } = this.props;
+        await collectionCardActions.getCurrentCollection(match.params.urlPath);
+      }
+
+      const { currentCollection, resetPageInfo } = this.props;
+
+      if (currentCollection) {
+        scrollTopPosition(this.sectionRef.current);
+        await resetPageInfo();
+        this.loadStoreListIfNeeded();
+      }
     }
 
-    if (hasFilterOptionParamsChanged) {
+    // Pickup filter is not included in the filter option params so we need to check shipping type separately
+    if (hasFilterOptionParamsChanged || hasShippingTypeChanged) {
       this.props.backUpSearchOptionList({ key: FILTER_BACKUP_STORAGE_KEYS.COLLECTION });
     }
   };
 
-  resetCollectionData = () => {
-    const { currentCollection, collectionsActions } = this.props;
+  resetCollectionData = async () => {
+    const { categoryFilterList, currentCollection, resetPageInfo, setShippingType } = this.props;
+    const pickupFilter = categoryFilterList.find(filter => filter.id === IDS.PICK_UP);
     const { shippingType } = currentCollection;
-    const type = shippingType.length === 1 ? shippingType[0].toLowerCase() : 'delivery';
-    collectionsActions.setShippingType(type);
-    collectionsActions.resetPageInfo(type);
+
+    // Forward compatibility:
+    // If pickup filter has been selected, we will set shipping type to pickup
+    // Backward compatibility:
+    // If no shipping type is set and only one shipping type is available then we will use the existing one.
+    // Otherwise, we will set shipping type to delivery
+    const type = pickupFilter?.selected
+      ? SHIPPING_TYPES.PICKUP
+      : shippingType.length === 1
+      ? shippingType[0].toLowerCase()
+      : SHIPPING_TYPES.DELIVERY;
+
+    await setShippingType({ shippingType: type });
+    await resetPageInfo();
   };
 
-  reloadStoreListIfNeeded = async () => {
-    const { match, collectionsActions, collectionCardActions } = this.props;
+  loadStoreListIfNeeded = () => {
+    const {
+      shouldLoadStoreList,
+      loadStoreList,
+      currentCollection: { urlPath },
+    } = this.props;
 
-    await collectionCardActions.getCurrentCollection(match.params.urlPath);
+    if (!shouldLoadStoreList) return;
 
-    const { currentCollection } = this.props;
-
-    if (!currentCollection) return;
-
-    const { urlPath } = currentCollection;
-
-    collectionsActions.getStoreList(urlPath);
+    loadStoreList(urlPath);
   };
 
   backToPreviousPage = () => {
     CleverTap.pushEvent('Collection Page - Click back');
 
-    const { history, location, resetSearchOptionList } = this.props;
+    const { history, location, resetSearchOptionList, collectionsActions, resetPageInfo } = this.props;
     const pathname = (location.state && location.state.from) || '/home';
 
     history.push({
       pathname,
     });
 
+    // Reset all collection & filter redux data state
+    collectionsActions.resetShippingType();
+    collectionsActions.resetStoreListInfo();
+    resetPageInfo();
     resetSearchOptionList({ key: FILTER_BACKUP_STORAGE_KEYS.COLLECTION });
   };
 
@@ -139,7 +187,7 @@ class CollectionPage extends React.Component {
 
   backLeftPosition = async store => {
     const { shippingType, collectionsActions, addressInfo } = this.props;
-    collectionsActions.setPageInfo({ shippingType: shippingType, scrollTop: this.scrollTop });
+    collectionsActions.setPageInfo({ scrollTop: this.scrollTop });
     this.backupState();
     await submitStoreMenu({
       deliveryAddress: addressInfo,
@@ -150,9 +198,30 @@ class CollectionPage extends React.Component {
   };
 
   renderStoreList = () => {
-    const { stores, pageInfo, currentCollection } = this.props;
+    const {
+      t,
+      stores,
+      pageInfo,
+      currentCollection,
+      shouldShowStoreListLoader,
+      shouldShowNoFilteredResultPage,
+    } = this.props;
     const { scrollTop } = pageInfo;
-    const { urlPath } = currentCollection;
+
+    if (shouldShowStoreListLoader) {
+      return <PageLoader />;
+    }
+
+    if (shouldShowNoFilteredResultPage) {
+      return (
+        <div className={styles.CollectionPageInfoCard}>
+          <img className={styles.CollectionPageInfoCardImage} src={BeepNotResultImage} alt="store not found" />
+          <p className={styles.CollectionPageInfoCardHeading}>
+            <Trans t={t} i18nKey="FilterNotFoundStoreDescription" components={[<br />]} />
+          </p>
+        </div>
+      );
+    }
 
     return (
       <div className="sm:tw-py-4px tw-py-4 tw-bg-white">
@@ -166,9 +235,7 @@ class CollectionPage extends React.Component {
             stores={stores}
             hasMore={pageInfo.hasMore}
             getScrollParent={() => this.sectionRef.current}
-            loadMoreStores={() => {
-              this.props.collectionsActions.getStoreList(urlPath);
-            }}
+            loadMoreStores={() => this.loadStoreListIfNeeded()}
             onStoreClicked={(store, index) => {
               CleverTap.pushEvent('Collection Page - Click Store Card', {
                 'Collection Name': currentCollection.name,
@@ -211,7 +278,7 @@ class CollectionPage extends React.Component {
   }
 
   handleClickCategoryButton = category => {
-    const { id, type } = category;
+    const { id, type, selected } = category;
 
     if (id === IDS.SORT_BY) {
       CleverTap.pushEvent('Collection Page - Click sort by button');
@@ -219,15 +286,23 @@ class CollectionPage extends React.Component {
       CleverTap.pushEvent('Collection Page - Click quick filter button');
     }
 
+    const { setShippingType, filterActions } = this.props;
+
+    if (id === IDS.PICK_UP) {
+      setShippingType({ shippingType: selected ? SHIPPING_TYPES.DELIVERY : SHIPPING_TYPES.PICKUP });
+    }
+
     if (FILTER_DRAWER_SUPPORT_TYPES.includes(type)) {
       this.setState({ drawerInfo: { category } });
     } else {
-      this.props.filterActions.updateCategorySelectStatus({ id });
+      filterActions.updateCategorySelectStatus({ id });
     }
   };
 
   handleClickResetAllCategoryButton = () => {
-    this.props.resetSearchOptionList({ key: FILTER_BACKUP_STORAGE_KEYS.COLLECTION });
+    const { resetSearchOptionList, setShippingType } = this.props;
+    resetSearchOptionList({ key: FILTER_BACKUP_STORAGE_KEYS.COLLECTION });
+    setShippingType({ shippingType: SHIPPING_TYPES.DELIVERY });
     CleverTap.pushEvent('Collection Page - Click reset quick sort and filter button');
   };
 
@@ -319,8 +394,8 @@ class CollectionPage extends React.Component {
   };
 
   render() {
-    const { currentCollection, currentCollectionStatus, categoryFilterList, shouldShowResetButton } = this.props;
-    if (!currentCollectionStatus || currentCollectionStatus === API_REQUEST_STATUS.PENDING) {
+    const { currentCollection, categoryFilterList, shouldShowPageLoader, shouldShowResetButton } = this.props;
+    if (shouldShowPageLoader) {
       return <PageLoader />;
     }
 
@@ -381,9 +456,16 @@ export default compose(
       addressCoords: getAddressCoords(state),
       categoryFilterList: getCategoryFilterList(state),
       shouldShowResetButton: getHasAnyCategorySelected(state),
+      shouldShowPageLoader: getShouldShowPageLoader(state),
+      shouldShowStoreListLoader: getShouldShowStoreListLoader(state),
       filterOptionParams: getFilterOptionSearchParams(state),
+      shouldLoadStoreList: getShouldLoadStoreList(state),
+      shouldShowNoFilteredResultPage: getShouldShowNoFilteredResultPage(state),
     }),
     dispatch => ({
+      resetPageInfo: bindActionCreators(resetPageInfo, dispatch),
+      setShippingType: bindActionCreators(setShippingType, dispatch),
+      loadStoreList: bindActionCreators(loadStoreList, dispatch),
       fetchAddressInfo: bindActionCreators(fetchAddressInfo, dispatch),
       loadSearchOptionList: bindActionCreators(loadSearchOptionList, dispatch),
       backUpSearchOptionList: bindActionCreators(backUpSearchOptionList, dispatch),
