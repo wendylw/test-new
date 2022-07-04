da1aa143d65bcc3c0d79983c46ac7be1c252d8c6
wendylw <wendylw2013@gmail.com>
2022-07-05 01:28:56 +0800

    FB-3905: Update DOM

---
+++ src/ordering/containers/common/AddressDropdown/AddressDropdownDrawer.jsx
@@ -0,0 +1,94 @@
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import { X } from 'phosphor-react';
+import Drawer from '../../../../common/components/Drawer';
+import DrawerHeader from '../../../../common/components/Drawer/DrawerHeader';
+import Search from '../../../../common/components/Input/Search';
+import Tag from '../../../../common/components/Tag';
+import Loader from '../../../../common/components/Loader';
+import 'swiper/components/pagination/pagination.scss';
+import styles from './AddressDropdownDrawer.module.scss';
+
+const AddressDropdownDrawer = ({ isInitializing, onClose }) => {
+  const { t } = useTranslation();
+  const searchInputRef = useRef(null);
+
+  return (
+    <Drawer
+      className={isInitializing ? styles.timeSlotDrawerInitializing : styles.timeSlotDrawer}
+      fullScreen
+      show
+      header={
+        <DrawerHeader left={<X weight="light" className="tw-flex-shrink-0 tw-text-2xl tw-text-gray" />}>
+          <div className="tw-flex tw-flex-col tw-items-center">
+            <span className="tw-font-bold tw-text-lg tw-leading-relaxed">{t('DeliverTo')}</span>
+          </div>
+        </DrawerHeader>
+      }
+      onClose={onClose}
+    >
+      {isInitializing ? (
+        <Loader className={styles.loader} weight="bold" />
+      ) : (
+        <div>
+          <Search
+            ref={searchInputRef}
+            placeholder={t('SearchYourLocation')}
+            defaultSearchKeyword=""
+            onChangeInputValue={value => {}}
+            onClearInput={() => {}}
+          />
+          <div>
+            <img src="" alt="" />
+            <p>{t('AddressListEmptyDescription')}</p>
+          </div>
+          <div>
+            <section>
+              <h3>{t('SavedAddress')}</h3>
+              <ul>
+                <li>
+                  <i>icon</i>
+                  <div>
+                    <h4>
+                      <span>Apartment</span>
+                      <Tag>{t('OutOfRange')}</Tag>
+                    </h4>
+                    <p>5, Jalan Kukuh 25/42, Taman Sri Mudim</p>
+                  </div>
+                </li>
+              </ul>
+            </section>
+            <section>
+              <ul>
+                <li>
+                  <i>icon</i>
+                  <div>
+                    <h4>
+                      <span>KYMCO Malaysia Motorcycle Bestbuy Sdn Bhd</span>
+                    </h4>
+                    <p>8, Jalan PJU 7/6 Mutiara Damansara, Selangor</p>
+                  </div>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </div>
+      )}
+    </Drawer>
+  );
+};
+
+AddressDropdownDrawer.displayName = 'AddressDropdownDrawer';
+
+AddressDropdownDrawer.propTypes = {
+  isInitializing: PropTypes.bool,
+  onClose: PropTypes.func,
+};
+
+AddressDropdownDrawer.defaultProps = {
+  isInitializing: true,
+  onClose: () => {},
+};
+
+export default AddressDropdownDrawer;
