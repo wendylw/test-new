7d726dcb67c60a2bf1abfc574911d1e01b0a1140
wendylw <wendylw2013@gmail.com>
2022-07-07 17:32:21 +0800

    FB-3907: Update loader for store list

--- src/ordering/components/StoreListDrawer/index.jsx
+++ src/ordering/components/StoreListDrawer/index.jsx
@@ -7,6 +7,7 @@ import Card from '../../../common/components/Card';
 import CardGroup from '../../../common/components/Card/CardGroup';
 import Tag from '../../../common/components/Tag';
 import DrawerHeader from '../../../common/components/Drawer/DrawerHeader';
+import Loader from '../../../common/components/Loader';
 import { StoreIcon } from '../../../common/components/Icons';
 import styles from './StoreListDrawer.module.scss';
 
@@ -37,39 +38,43 @@ const StoreListDrawer = ({
       }
       onClose={onClose}
     >
-      <CardGroup className="tw-px-16 sm:tw-px-16px tw-py-24 sm:tw-py-24px" spacing="16">
-        {storeList.map(store => (
-          <Card
-            key={store.id}
-            contentClassName="tw-flex tw-items-start"
-            onClick={selectStoreBranch}
-            active={store.selected}
-            disabled={!store.available}
-          >
-            <StoreIcon />
-            <div className="tw-ml-8 sm:tw-ml-8px">
-              <h4 className="tw-font-bold tw-leading-relaxed">{store.title}</h4>
-              <p className="tw-my-6 sm: tw-my-6px tw-text-sm tw-leading-loose">{store.location}</p>
-              <div className="tw-flex tw-items-center">
-                <ul className={styles.storeListDrawerInfoList}>
-                  <li className="tw-flex tw-items-center">
-                    <MapPin className="tw-text-sm tw-text-gray-600" weight="light" />
-                    <span className="tw-mx-4 sm:tw-mx-4px tw-text-xs">{store.displayDistance}</span>
-                  </li>
-                  <li className="tw-flex tw-items-center">
-                    <Clock className="tw-text-sm tw-text-gray-600" weight="light" />
-                    <span className="tw-mx-4 sm:tw-mx-4px tw-text-xs">{store.displayOpeningTime}</span>
-                  </li>
-                </ul>
-                <Tag className="tw-mx-8 sm:tw-mx-8px" color="red">
-                  {t('Closed')}
-                </Tag>
-                <Tag className="tw-mx-8 sm:tw-mx-8px">{t('OutOfRange')}</Tag>
+      {isInitializing ? (
+        <Loader className={styles.loader} weight="bold" />
+      ) : (
+        <CardGroup className="tw-px-16 sm:tw-px-16px tw-py-24 sm:tw-py-24px" spacing="16">
+          {storeList.map(store => (
+            <Card
+              key={store.id}
+              contentClassName="tw-flex tw-items-start"
+              onClick={selectStoreBranch}
+              active={store.selected}
+              disabled={!store.available}
+            >
+              <StoreIcon />
+              <div className="tw-ml-8 sm:tw-ml-8px">
+                <h4 className="tw-font-bold tw-leading-relaxed">{store.title}</h4>
+                <p className="tw-my-6 sm: tw-my-6px tw-text-sm tw-leading-loose">{store.location}</p>
+                <div className="tw-flex tw-items-center">
+                  <ul className={styles.storeListDrawerInfoList}>
+                    <li className="tw-flex tw-items-center">
+                      <MapPin className="tw-text-sm tw-text-gray-600" weight="light" />
+                      <span className="tw-mx-4 sm:tw-mx-4px tw-text-xs">{store.displayDistance}</span>
+                    </li>
+                    <li className="tw-flex tw-items-center">
+                      <Clock className="tw-text-sm tw-text-gray-600" weight="light" />
+                      <span className="tw-mx-4 sm:tw-mx-4px tw-text-xs">{store.displayOpeningTime}</span>
+                    </li>
+                  </ul>
+                  <Tag className="tw-mx-8 sm:tw-mx-8px" color="red">
+                    {t('Closed')}
+                  </Tag>
+                  <Tag className="tw-mx-8 sm:tw-mx-8px">{t('OutOfRange')}</Tag>
+                </div>
               </div>
-            </div>
-          </Card>
-        ))}
-      </CardGroup>
+            </Card>
+          ))}
+        </CardGroup>
+      )}
     </Drawer>
   );
 };
