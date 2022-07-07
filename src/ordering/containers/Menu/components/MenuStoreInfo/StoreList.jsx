4e95fec580d25c577e01562aab63c4eb51c7df2e
wendylw <wendylw2013@gmail.com>
2022-07-07 17:08:16 +0800

    FB-3907: Update fixing

--- src/ordering/containers/Menu/components/MenuStoreInfo/StoreList.jsx
+++ src/ordering/containers/Menu/components/MenuStoreInfo/StoreList.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react';
 import { CaretDown } from 'phosphor-react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getStoreDisplaySubTitle, getIsStoreListDrawerVisible } from '../../redux/common/selectors';
+import {
+  getStoreDisplaySubTitle,
+  getIsStoreListDrawerVisible,
+  getIsQrOrderingShippingType,
+} from '../../redux/common/selectors';
 import { showStoreListDrawer, hideStoreInfoDrawer } from '../../redux/common/thunks';
 import { getHasStoreListInitialized, getStoreList, getTotalOutletDisplayTitle } from '../../redux/stores/selectors';
 import { storeDrawerShown, storeDrawerHidden, selectStoreBranch } from '../../redux/stores/thunks';
@@ -10,6 +14,7 @@ import styles from './StoreList.module.scss';
 
 const StoreList = () => {
   const dispatch = useDispatch();
+  const isQrOrderingShippingType = useSelector(getIsQrOrderingShippingType);
   const isStoreListDrawerVisible = useSelector(getIsStoreListDrawerVisible);
   const storeList = useSelector(getStoreList);
   // the subtitle display on drawer
@@ -29,28 +34,33 @@ const StoreList = () => {
 
   return (
     <div className="tw-flex-1">
-      <button
-        className={styles.storeListDropdownButton}
-        disabled={storeList.length <= 1}
-        onClick={() => {
-          dispatch(showStoreListDrawer());
-        }}
-      >
-        <span className={styles.storeListSubtitle}>{storeDisplaySubTitle}</span>
-        {storeList.length > 1 ? <CaretDown className="tw-mx-4 sm:tw-mx-4px tw-text-xs tw-text-gray-600" /> : null}
-      </button>
-      <StoreListDrawer
-        isInitializing={!hasStoreListInitialized}
-        isStoreListDrawerVisible={isStoreListDrawerVisible}
-        totalOutletDisplayTitle={totalOutletDisplayTitle}
-        storeList={storeList}
-        onClose={() => {
-          dispatch(hideStoreInfoDrawer());
-        }}
-        selectStoreBranch={value => {
-          dispatch(selectStoreBranch(value));
-        }}
-      />
+      {isQrOrderingShippingType ? (
+        <p className={styles.storeListSubtitle}>storeDisplaySubTitle</p>
+      ) : (
+        <>
+          <button
+            className={styles.storeListDropdownButton}
+            onClick={() => {
+              dispatch(showStoreListDrawer());
+            }}
+          >
+            <span className={styles.storeListSubtitle}>{storeDisplaySubTitle}</span>
+            <CaretDown className="tw-mx-4 sm:tw-mx-4px tw-text-xs tw-text-gray-600" />
+          </button>
+          <StoreListDrawer
+            isInitializing={!hasStoreListInitialized}
+            isStoreListDrawerVisible={isStoreListDrawerVisible}
+            totalOutletDisplayTitle={totalOutletDisplayTitle}
+            storeList={storeList}
+            onClose={() => {
+              dispatch(hideStoreInfoDrawer());
+            }}
+            selectStoreBranch={value => {
+              dispatch(selectStoreBranch(value));
+            }}
+          />
+        </>
+      )}
     </div>
   );
 };
