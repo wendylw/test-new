5dc57222e7509b1d91cf21fe50f2e77ceb209b38
Philly Cai <philly.cai@storehub.com>
2022-07-14 00:17:38 +0800

    FB-4022: update getHasStoreInfoInitialized selector

--- src/ordering/containers/Menu/redux/address/selectors.js
+++ src/ordering/containers/Menu/redux/address/selectors.js
@@ -7,7 +7,6 @@ export const getErrorToast = state => state.menu.address.errorToast.data;
 
 export const getStoreInfoData = createSelector(getStoreInfo, storeInfo => storeInfo.data);
 
-export const getHasStoreInfoInitialized = createSelector(
-  getStoreInfo,
-  storeInfo => storeInfo.status === API_REQUEST_STATUS.PENDING
+export const getHasStoreInfoInitialized = createSelector(getStoreInfo, storeInfo =>
+  [API_REQUEST_STATUS.FULFILLED, API_REQUEST_STATUS.REJECTED].includes(storeInfo.status)
 );
