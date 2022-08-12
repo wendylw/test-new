92abd5eb0f873233c1a8e8b2a16028e900166b8d
wendylw <wendylw2013@gmail.com>
2022-08-12 18:59:15 +0800

    FB-4011: Update alert function

---
+++ src/common/utils/feedback/alert.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { destroyTarget } from './index';
+import Alert from '../../components/Alert';
+import AlertContent from '../../components/Alert/AlertContent';
+
+const alertOptions = options => ({
+  container: document.body,
+  show: true,
+  customizeContent: false,
+  animation: true,
+  closeButtonContent: null,
+  className: '',
+  zIndex: 300,
+  style: {},
+  onClose: () => {},
+  ...options,
+});
+
+const createAlert = (content, options) =>
+  new Promise(resolve => {
+    const { container, customizeContent, title, onClose, ...restOptions } = options;
+    const rootDOM = document.createElement('div');
+    // eslint-disable-next-line react/jsx-filename-extension
+    const children = customizeContent ? content : <AlertContent content={content} title={title} />;
+    const alertInstance = React.createElement(
+      Alert,
+      {
+        title,
+        ...restOptions,
+        mountAtRoot: false,
+        onClose: () => {
+          destroyTarget(rootDOM);
+          resolve();
+          onClose();
+        },
+      },
+      children
+    );
+
+    container.appendChild(rootDOM);
+
+    render(alertInstance, rootDOM);
+  });
+
+export const alert = (content, options = {}) => createAlert(content, alertOptions(options));
