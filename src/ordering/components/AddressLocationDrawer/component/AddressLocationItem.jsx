087f7f9e94d80c711befc2d03dded1a1cbf258a7
wendylw <wendylw2013@gmail.com>
2022-08-24 17:35:14 +0800

    FB-4011: Update callback for location item

--- src/ordering/components/AddressLocationDrawer/component/AddressLocationItem.jsx
+++ src/ordering/components/AddressLocationDrawer/component/AddressLocationItem.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import styles from './AddressLocationItem.module.scss';
 
-const AddressLocationItem = React.memo(({ disabled, icon, title, tag, description, onSelect }) => (
-  <button className={styles.addressLocationItemButton} disabled={disabled} onClick={onSelect}>
-    {icon}
-    <div className="beep-line-clamp-flex-container tw-flex-col">
-      <h4 className="tw-flex tw-items-center tw-justify-start tw-my-2 sm:tw-my-2px beep-line-clamp-flex-container">
-        <span className={styles.addressLocationItemButtonTitle}>{title}</span>
-        {tag}
-      </h4>
-      <p className={styles.addressLocationItemButtonDescription}>{description}</p>
-    </div>
-  </button>
-));
+const AddressLocationItem = React.memo(({ disabled, icon, title, tag, description, onSelect }) => {
+  const onHandleSelectItem = useCallback(
+    itemInfo => {
+      onSelect(itemInfo);
+    },
+    [onSelect]
+  );
+
+  return (
+    <button className={styles.addressLocationItemButton} disabled={disabled} onClick={onHandleSelectItem}>
+      {icon}
+      <div className="beep-line-clamp-flex-container tw-flex-col">
+        <h4 className="tw-flex tw-items-center tw-justify-start tw-my-2 sm:tw-my-2px beep-line-clamp-flex-container">
+          <span className={styles.addressLocationItemButtonTitle}>{title}</span>
+          {tag}
+        </h4>
+        <p className={styles.addressLocationItemButtonDescription}>{description}</p>
+      </div>
+    </button>
+  );
+});
 
 AddressLocationItem.displayName = 'AddressLocationItem';
 
