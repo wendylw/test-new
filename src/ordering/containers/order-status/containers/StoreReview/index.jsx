a71814d0788eabe9af69771eb1b004dcfe8a5389
Philly Cai <philly.cai@storehub.com>
2022-09-19 12:05:59 +0800

    BEEP-3034: rename store review folder

---
+++ src/ordering/containers/order-status/containers/StoreReview/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useMount } from 'react-use';
+import Frame from '../../../../../common/components/Frame';
+
+const StoreReview = () => {
+  useMount(() => {
+    // do nothing
+  });
+
+  return (
+    <Frame>
+      <p>Hey, this is store review test!</p>
+    </Frame>
+  );
+};
+
+StoreReview.displayName = 'StoreReview';
+
+export default StoreReview;
