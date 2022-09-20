b0ea14ce81bd695eb89bf4e0bba7d09da0d12e38
siyuan <siyuan.peng@storehub.com>
2022-09-20 11:21:07 +0800

    Develop store feedback

--- src/common/components/Tag/index.jsx
+++ src/common/components/Tag/index.jsx
@@ -5,11 +5,11 @@ import styles from './Tag.module.scss';
 const getClassName = classList => classList.filter(className => !!className).join(' ');
 
 const Tag = props => {
-  const { children, className = '', style = {}, color, radiusSize } = props;
+  const { children, className = '', style = {}, color, radiusSize, innerClassName = '' } = props;
 
   return (
     <i className={getClassName([styles.tag, className, color, radiusSize])} style={style}>
-      <span className={styles.tagInner}>{children}</span>
+      <span className={`${styles.tagInner} ${innerClassName}`}>{children}</span>
     </i>
   );
 };
@@ -21,6 +21,7 @@ Tag.propTypes = {
   // If radiusSize is set to 'xs', the tag's border radius will be 2px and padding will change to 4px.
   radiusSize: PropTypes.oneOf(['xs']),
   className: PropTypes.string,
+  innerClassName: PropTypes.string,
   // eslint-disable-next-line react/forbid-prop-types
   style: PropTypes.object,
 };
@@ -29,6 +30,7 @@ Tag.defaultProps = {
   color: null,
   radiusSize: null,
   className: '',
+  innerClassName: '',
   style: {},
 };
 
--- src/ordering/containers/order-status/containers/StoreReview/index.jsx
+++ src/ordering/containers/order-status/containers/StoreReview/index.jsx
@@ -5,7 +5,6 @@ import { useTranslation } from 'react-i18next';
 import { Info } from 'phosphor-react';
 import TextareaAutosize from 'react-textarea-autosize';
 import Utils from '../../../../../utils/utils';
-import PageFooter from '../../../../../common/components/PageFooter';
 import Button from '../../../../../common/components/Button';
 import HybridHeader from '../../../../../components/HybridHeader';
 import CheckBox from '../../../../../common/components/CheckBox';
@@ -25,11 +24,11 @@ const StoreReview = () => {
   const headerEl = useRef(0);
   const footerEl = useRef(0);
 
-  const [cartContainerHeight, setCartContainerHeight] = useState('100%');
-
   const { t, i18n } = useTranslation('OrderingThankYou');
 
   const storeName = useSelector(getStoreName);
+
+  const [TextExcess, setTextExcess] = useState(false);
 
   const [review, setReview] = useState('');
   const saveReview = () => {
@@ -39,30 +38,33 @@ const StoreReview = () => {
   const handleReviewChange = useCallback(
     event => {
       setReview(event.target.value);
+      if (event.target.value.length > 4050) {
+        setTextExcess(true);
+      } else {
+        setTextExcess(false);
+      }
     },
     [setReview]
   );
-
   useEffect(() => {
     setReview(reviewContents);
   }, [reviewContents]);
 
   const initialRating = useSelector(getStoreRating);
 
-  const calculateCartContainerHeight = preContainerHeight => {
+  const [reviewContainerHeight, setReviewContainerHeight] = useState('100%');
+  const calculateReviewContainerHeight = preContainerHeight => {
     const containerHeight = Utils.containerHeight({
       headerEls: [headerEl.current],
       footerEls: [footerEl.current],
     });
-    console.log('containerHeight:', containerHeight);
 
     if (preContainerHeight !== containerHeight) {
-      setCartContainerHeight(containerHeight);
+      setReviewContainerHeight(containerHeight);
     }
   };
-
   useEffect(() => {
-    calculateCartContainerHeight(cartContainerHeight);
+    calculateReviewContainerHeight(reviewContainerHeight);
   });
 
   const handleClick = () => {
@@ -85,7 +87,7 @@ const StoreReview = () => {
             top: `${Utils.mainTop({
               headerEls: [headerEl],
             })}px`,
-            height: cartContainerHeight,
+            height: reviewContainerHeight,
           }}
         >
           <img className="store-review__container-img sm:tw-my-16px tw-my-16" src={StoreReviewImg} alt="Store Review" />
@@ -118,27 +120,34 @@ const StoreReview = () => {
               </span>
             </Tag>
           </div>
-          <div className="store-review__container-textarea-title tw-mx-16 sm:tw-mx-16px tw-mt-16 sm:tw-mt-16px tw-border tw-border-solid tw-rounded-t-sm tw-text-lg tw-font-bold tw-align-middle tw-leading-normal tw-py-4 sm:py-4px tw-pl-12 sm:tw-pl-12px">
+          <div className="store-review__container-textarea-title tw-mx-16 sm:tw-mx-16px tw-mt-16 sm:tw-mt-16px tw-border tw-border-b-0 tw-border-solid tw-rounded-t-sm tw-text-lg tw-font-bold tw-align-middle tw-leading-normal tw-py-4 sm:py-4px tw-pl-12 sm:tw-pl-12px">
             {t('WriteReview')}
           </div>
           <div className="store-review__container-textarea tw-flex tw-mx-16 sm:tw-mx-16px">
             <TextareaAutosize
-              className="store-review__container-textarea-input tw-w-full tw-mb-16 sm:tw-mb-16px tw-border tw-border-t-0 tw-border-solid tw-rounded-b-sm tw-p-12 sm:tw-p-12px tw-overflow-hidden"
+              className={`store-review__container-textarea-input tw-w-full tw-border tw-border-solid tw-rounded-b-sm tw-p-12 sm:tw-p-12px tw-overflow-hidden ${
+                TextExcess ? 'tw-border-red' : 'tw-border-gray-400'
+              }`}
               onChange={handleReviewChange}
               minRows={4}
               value={review}
               placeholder={t('TellExperience')}
             />
           </div>
-          <div className="store-review__container-allow tw-ml-16 sm:tw-ml-16px">
+          {TextExcess && (
+            <div className="tw-text-red tw-font-bold tw-mt-12 sm:tw-mt-12px tw-ml-16 sm:tw-ml-16px">
+              {t('ExceedMaximum')}
+            </div>
+          )}
+          <div className="store-review__container-allow tw-mt-16 sm:tw-mt-16px tw-ml-16 sm:tw-ml-16px">
             <CheckBox />
             <span className="tw-ml-4 sm:tw-ml-4px tw-leading-relaxed">{t('AllowContact')}</span>
           </div>
         </div>
         <div className="store-review__footer tw-flex tw-p-8 sm:tw-p-8px" ref={footerEl}>
           <Button
-            // disabled={isSaveButtonDisabled}
-            // loading={isSaveButtonLoaderVisible}
+            // disabled={isSubmitButtonDisabled}
+            // loading={isSubmitButtonLoaderVisible}
             onClick={handleClick}
             className="tw-w-full tw-uppercase"
           >
