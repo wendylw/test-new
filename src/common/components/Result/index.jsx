aad3d64ebe622fb8733acb05869c469a4e73cb61
wendylw <wendylw2013@gmail.com>
2022-09-22 15:44:56 +0800

    BEEP-3034: Update button groups

--- src/common/components/Result/index.jsx
+++ src/common/components/Result/index.jsx
@@ -60,6 +60,17 @@ const Result = props => {
       {children}
       <div className={styles.resultFooter}>
         <Button
+          type="secondary"
+          className={`${styles.closeButtonClassName} tw-uppercase${
+            closeButtonClassName ? ` ${closeButtonClassName}` : ''
+          }`}
+          onClick={onClose}
+          style={closeButtonStyle}
+          size="small"
+        >
+          {closeButtonContent || t('OK')}
+        </Button>
+        <Button
           type="primary"
           className={`${styles.closeButtonClassName} tw-uppercase${
             closeButtonClassName ? ` ${closeButtonClassName}` : ''
