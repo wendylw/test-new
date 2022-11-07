2746262689484ed39be162b728395e7698ef2f9a
Philly Cai <philly.cai@storehub.com>
2022-11-07 08:17:24 +0800

    BEEP-3396: remove legacy components on site folder

--- src/site/Routes.jsx
+++ src/site/Routes.jsx
@@ -1,11 +1,8 @@
 import React, { lazy } from 'react';
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
-import Account from './account';
 import QRScan from './qrscan';
 import Error from './error';
 import NotFound from './components/NotFound';
-import Auth from './auth';
-import ProtectedRoute from './components/ProtectedRoute';
 import Location from './ordering/containers/Location';
 import CollectionPage from './collections/CollectionPage';
 import SearchPage from './search/SearchPage';
@@ -24,14 +21,10 @@ const SiteRoute = () => {
       <Route path={`/collections/:urlPath`} component={CollectionPage} />
       <Route path={`/search`} component={SearchPage} />
       <Route path={`/qrscan`} component={QRScan} />
-      <Route path={`/auth`} component={Auth} />
       <Route path={`/ordering/location`} component={Location} />
       <Route path={`/error/:type`} component={Error} />
       <Route path={`/terms-of-use`} render={props => <AsyncTermsPrivacy {...props} pageName="terms" />} />
       <Route path={`/privacy-policy`} render={props => <AsyncTermsPrivacy {...props} pageName="privacy" />} />
-      <ProtectedRoute path={`/account`}>
-        <Account />
-      </ProtectedRoute>
       <Route path={ROUTER_PATHS.ORDER_HISTORY} component={OrderHistory} />
       <Redirect from={`/`} to={`/home`} />
       <Route component={NotFound} />
--- src/site/account/containers/AccountOrders/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const AccountOrders = () => <h3>AccountOrders/index.jsx page</h3>;
-AccountOrders.displayName = 'AccountOrders';
-
-export default AccountOrders;
