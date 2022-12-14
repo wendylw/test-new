2c26ae864a85c577d5afad3aa02e0bf8e95e90cc
Philly Cai <philly.cai@storehub.com>
2022-12-14 16:31:18 +0800

    WB-4699: update action name for ReCaptcha scripts

--- src/cashback/components/Login/index.js
+++ src/cashback/components/Login/index.js
@@ -163,7 +163,7 @@ class Login extends React.Component {
     const hasLoadSuccess = !!window.grecaptcha;
     const scriptName = 'google-recaptcha';
 
-    window.newrelic?.addPageAction(`common.script-load-${hasLoadSuccess ? 'succeeded' : 'failed'}`, {
+    window.newrelic?.addPageAction(`third-party-lib.load-script-${hasLoadSuccess ? 'succeeded' : 'failed'}`, {
       scriptName: scriptName,
     });
 
