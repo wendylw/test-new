a8f730b52cc7058240df17102dd71fa5a06c0775
siyuan <siyuan.peng@storehub.com>
2023-01-19 21:57:42 +0800

    WB-4889: Store review enables offline orders, don't go to Menu page

--- src/ordering/containers/order-status/containers/StoreReview/components/ErrorResult/index.jsx
+++ src/ordering/containers/order-status/containers/StoreReview/components/ErrorResult/index.jsx
@@ -1,15 +1,22 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import Result from '../../../../../../../common/components/Result';
 import PageHeader from '../../../../../../../common/components/PageHeader';
 import ResultContent from '../../../../../../../common/components/Result/ResultContent';
 import { goBack } from '../../redux/thunks';
+import { gotoHome } from '../../../../../../../utils/native-methods';
+import { isWebview } from '../../../../../../../common/utils';
+import { getOffline } from '../../redux/selectors';
+
+const isInWebview = isWebview();
 
 const ErrorResult = ({ title, content, buttonText, imageSrc, onCloseButtonClick }) => {
   const dispatch = useDispatch();
   const { t } = useTranslation('OrderingThankYou');
+
+  const offline = useSelector(getOffline);
 
   const handleClickBackButton = useCallback(() => dispatch(goBack()), [dispatch]);
 
@@ -19,7 +26,13 @@ const ErrorResult = ({ title, content, buttonText, imageSrc, onCloseButtonClick 
       mountAtRoot
       closeButtonContent={buttonText}
       onClose={onCloseButtonClick}
-      header={<PageHeader title={t('StoreReview')} onBackArrowClick={handleClickBackButton} />}
+      header={
+        <PageHeader
+          title={t('StoreReview')}
+          onBackArrowClick={isInWebview ? gotoHome : handleClickBackButton}
+          isShowBackButton={!offline || isInWebview}
+        />
+      }
       disableBackButtonSupport
     >
       <ResultContent content={content} title={title} imageSrc={imageSrc} />
