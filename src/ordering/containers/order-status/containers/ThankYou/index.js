3894da702e9e7719a5ddeb244ffee28ea8651450
wendylw <wendylw2013@gmail.com>
2023-03-05 19:55:42 +0800

    WB-4979: Update settings

--- src/ordering/containers/order-status/containers/ThankYou/index.js
+++ src/ordering/containers/order-status/containers/ThankYou/index.js
@@ -984,7 +984,7 @@ export class ThankYou extends PureComponent {
         className={`ordering-thanks flex flex-middle flex-column ${match.isExact ? '' : 'hide'}`}
         data-heap-name="ordering.thank-you.container"
       >
-        {orderStatus && <Profile onClose={hideProfileModal} show={true} />}
+        {orderStatus && <Profile onClose={hideProfileModal} show={profileModalVisibility} />}
         <>
           <HybridHeader
             headerRef={ref => (this.headerEl = ref)}
