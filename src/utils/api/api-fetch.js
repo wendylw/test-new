21c2e9d4f4fba87955f38f838816f0aa225c36d0
wendylw <wendylw2013@gmail.com>
2023-04-13 16:49:05 +0800

    WB-4109: Update logic for type payload and payload type

--- src/utils/api/api-fetch.js
+++ src/utils/api/api-fetch.js
@@ -127,63 +127,65 @@ async function _fetch(url, opts) {
  *
  * @param {object} options : {type = 'json', payload, headers, queryParams, ...othersOptions}
  * @returns {{
-*  body | json: any | json string,
-*  searchParams: string,
-*  hooks: {
-*    beforeRequest: Function[]
-*  },
-*  headers: object
-* }}
-*/
+ *  body | json: any | json string,
+ *  searchParams: string,
+ *  hooks: {
+ *    beforeRequest: Function[]
+ *  },
+ *  headers: object
+ * }}
+ */
 function convertOptions(options) {
- // include is general credential value. if api fetching need Cross-domain without cookie that the value needs set `omit` in others.
- const { type = 'json', payload, headers, queryParams, hooks, ...others } = options;
+  // include is general credential value. if api fetching need Cross-domain without cookie that the value needs set `omit` in others.
+  const { type = 'json', payload, headers, queryParams, hooks, ...others } = options;
 
- if (headers && typeof headers !== 'object') {
-   logger.error('Tool_ApiFetch_convertOptionsHeaderTypeError', {
-     message: 'headers should be an object',
-   });
+  if (headers && typeof headers !== 'object') {
+    logger.error('Tool_ApiFetch_convertOptionsHeaderTypeError', {
+      message: 'headers should be an object',
+    });
 
-   throw new APIError('headers should be an object', { status: 400, code: 80002, extra: 'requestHeadersNotObject' });
- }
+    throw new APIError('headers should be an object', { status: 400, code: 80002, extra: 'requestHeadersNotObject' });
+  }
 
- const currentHooks = hooks || {};
- const currentOptions = {
-   ...others,
-   hooks: {
-     ...currentHooks,
-     beforeRequest: currentHooks.beforeRequest || [],
-   },
- };
+  const currentHooks = hooks || {};
+  const currentOptions = {
+    ...others,
+    hooks: {
+      ...currentHooks,
+      beforeRequest: currentHooks.beforeRequest || [],
+    },
+  };
 
- if (type === 'json' && (!payload || (payload && typeof payload === 'object'))) {
-   currentOptions.json = payload;
- } else {
-   currentOptions.body = payload;
+  if (type === 'json') {
+    if (!payload || (payload && typeof payload === 'object')) {
+      currentOptions.json = payload;
+    } else {
+      currentOptions.body = payload;
 
-   if (type === 'json' && payload && typeof payload !== 'object') {
-     logger.error('Tool_ApiFetch_convertOptionsTypePayloadNotMatch', {
-       message: `Server only accepts array or object for json request. You provide "${typeof payload}". Won't send as json.`,
-     });
+      logger.error('Tool_ApiFetch_convertOptionsTypePayloadNotMatch', {
+        message: `Server only accepts array or object for json request. You provide "${typeof payload}". Won't send as json.`,
+      });
 
-     console.error('Common ApiFetch http payload & type not match');
-   }
- }
+      console.error('Common ApiFetch http payload & type not match');
+    }
+  } else {
+    currentOptions.body = payload;
+  }
 
- if (queryParams) {
-   currentOptions.searchParams = queryParams;
- }
+  if (queryParams) {
+    currentOptions.searchParams = queryParams;
+  }
 
- if (headers) {
-   // adding headers should be at the front of the hooks, so that other hooks have chance to modify the headers.
-   currentOptions.hooks.beforeRequest.unshift(req => {
-     Object.keys(headers).forEach(key => {
-       req.headers.set(key, headers[key]);
-     });
-   });
- }
+  if (headers) {
+    // adding headers should be at the front of the hooks, so that other hooks have chance to modify the headers.
+    currentOptions.hooks.beforeRequest.unshift(req => {
+      Object.keys(headers).forEach(key => {
+        req.headers.set(key, headers[key]);
+      });
+    });
+  }
 
- return currentOptions;
+  return currentOptions;
 }
 
 export function get(url, options = {}) {
