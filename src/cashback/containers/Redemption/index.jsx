3f621b5ae62d032245c7b501d696fc7796e1885f
wendylw <wendylw2013@gmail.com>
2023-05-09 14:35:11 +0800

    WB-5281: Update page DOM

--- src/cashback/containers/Redemption/index.jsx
+++ src/cashback/containers/Redemption/index.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import Frame from '../../../common/components/Frame';
+import StoreInfoBanner from './components/StoreInfoBanner';
+import CashbackBlock from './components/CashbackBlock';
 
-const Redemption = () => <div />;
+const Redemption = () => {
+  const storeName = 'test';
+  return (
+    <Frame>
+      <StoreInfoBanner storeName={storeName} />
+      <ul>
+        <li>
+          <CashbackBlock />
+        </li>
+      </ul>
+    </Frame>
+  );
+};
 
 Redemption.displayName = 'Redemption';
 
