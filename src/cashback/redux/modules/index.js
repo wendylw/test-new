de8a6d9315dfd61fd639a337f060fa8ae7eefff9
wendylw <wendylw2013@gmail.com>
2023-05-15 13:17:14 +0800

    WB-5320: Update redux

--- src/cashback/redux/modules/index.js
+++ src/cashback/redux/modules/index.js
@@ -3,12 +3,14 @@ import entities from '../../../redux/modules/entities';
 import app from './app';
 import home from './home';
 import claim from './claim';
+import storeRedemption from '../../containers/StoreRedemption/redux';
 
 const rootReducer = combineReducers({
   entities,
   app,
   home,
   claim,
+  storeRedemption,
 });
 
 export default rootReducer;
