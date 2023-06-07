5eb3b5c1b412bfe6ea9a6ccd58965403e7f85681
Wendy Lin <wendylw2013@gmail.com>
2023-06-07 14:54:09 +0800

    WB-5434: Update loader for app or mp login

--- src/cashback/containers/App/components/RequestLogin/index.js
+++ src/cashback/containers/App/components/RequestLogin/index.js
@@ -2,26 +2,39 @@ import React from 'react';
 import { withTranslation } from 'react-i18next';
 import beepLoginImage from '../../../../../images/login.svg';
 import './RequestLogin.scss';
+import Loader from '../../../../../common/components/Loader';
 
 function RequestLogin(props) {
   const { t, onClick, isLoginRequestStatusPending } = props;
-  return (
-    <section className="request-login flex flex-column flex-center">
-      <section className="page-login__content text-center">
-        <figure className="page-login__image-container">
-          <img src={beepLoginImage} alt="otp" />
-        </figure>
-      </section>
 
-      <p className="text-center text-size-big font-weight-bold padding-top-bottom-small">{t('CashbackLoginTip')}</p>
-      <button
-        disabled={isLoginRequestStatusPending}
-        onClick={onClick}
-        className="login-button login-button__fill text-uppercase"
-        data-heap-name="cashback.common.request-login.btn"
-      >
-        {isLoginRequestStatusPending ? t('Processing') : t('Login')}
-      </button>
+  return (
+    <section className="request-login flex flex-column flex-middle flex-center">
+      {isLoginRequestStatusPending ? (
+        <>
+          <Loader className="request-login__prompt-loader circle-loader" />
+          {!isLoginRequestStatusPending ? (
+            <span className="margin-top-bottom-smaller">{t('Redirecting...')}</span>
+          ) : null}
+        </>
+      ) : (
+        <>
+          <section className="page-login__content text-center">
+            <figure className="page-login__image-container">
+              <img src={beepLoginImage} alt="otp" />
+            </figure>
+          </section>
+
+          <p className="text-center text-size-big font-weight-bold padding-top-bottom-small">{t('CashbackLoginTip')}</p>
+          <button
+            disabled={isLoginRequestStatusPending}
+            onClick={onClick}
+            className="login-button login-button__fill text-uppercase"
+            data-heap-name="cashback.common.request-login.btn"
+          >
+            {isLoginRequestStatusPending ? t('Processing') : t('Login')}
+          </button>
+        </>
+      )}
     </section>
   );
 }
