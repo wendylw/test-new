f7ba9b8654f23af7959b414df283e3cfad8bff99
Philly Cai <philly.cai@storehub.com>
2023-06-16 16:30:05 +0800

    WB-5586: Fix missing isTakeaway in Pay Later add to cart API payload

--- src/ordering/containers/Menu/redux/productDetail/thunks.js
+++ src/ordering/containers/Menu/redux/productDetail/thunks.js
@@ -442,6 +442,7 @@ export const addToCart = createAsyncThunk(
             quantityChange: quantity,
             comments,
             variations,
+            isTakeaway,
           })
         );
       } else {
