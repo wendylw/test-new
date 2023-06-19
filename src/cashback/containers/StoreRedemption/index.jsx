83bdd8b615aa6ca11557aaf44a9ead6b29818828
Wendy Lin <wendylw2013@gmail.com>
2023-06-19 11:53:17 +0800

    WB-5574: Update logic

--- src/cashback/containers/StoreRedemption/index.jsx
+++ src/cashback/containers/StoreRedemption/index.jsx
@@ -115,7 +115,7 @@ const StoreRedemption = () => {
   const isDisplayWebResult = !isWebview() && !isTNGMiniProgram();
 
   useMount(() => {
-    if (!isDisplayWebResult) {
+    if (isDisplayWebResult) {
       CleverTap.pushEvent('POS Redemption Landing Page - View Page', {
         country: userCountry,
         page: 'When users scan QR with phone camera',
@@ -123,7 +123,7 @@ const StoreRedemption = () => {
     }
   });
 
-  if (!isDisplayWebResult) {
+  if (isDisplayWebResult) {
     // Use createPortal to load the page because the Login Modal in App/index level DOM needs to be covered
     return createPortal(
       <div className={`${styles.StoreRedemptionWeb} tw-flex tw-flex-col`}>
