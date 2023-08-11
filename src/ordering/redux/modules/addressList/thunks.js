4531ca99fcf19dee45c80372ca4f524d45374db3
Wendy Lin <wendylw2013@gmail.com>
2023-08-11 16:01:18 +0800

    WB-2932: Update address list

--- src/ordering/redux/modules/addressList/thunks.js
+++ src/ordering/redux/modules/addressList/thunks.js
@@ -4,21 +4,11 @@ import { fetchAddressList } from './api-request';
 import logger from '../../../../utils/monitoring/logger';
 
 export const loadAddressList = createAsyncThunk('ordering/addressList/loadAddressList', async (_, { getState }) => {
-  try {
-    const state = getState();
-    const consumerId = getUserConsumerId(state);
-    const storeId = getStoreId(state);
+  const state = getState();
+  const consumerId = getUserConsumerId(state);
+  const storeId = getStoreId(state);
 
-    if (!storeId) {
-      throw new Error('loadAddressList Store Id is not found');
-    }
-
-    return fetchAddressList(consumerId, storeId);
-  } catch (error) {
-    logger.error('Ordering_AddressList_loadAddressListFailed', { message: error?.message || '' });
-
-    throw error;
-  }
+  return fetchAddressList(consumerId, storeId);
 });
 
 export const resetAddressListStatus = createAsyncThunk('ordering/addressList/resetAddressListStatus', async () => {});
