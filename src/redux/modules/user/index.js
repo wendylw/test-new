51319dd33d71b6eb275c882ff21b873193716f2a
Wendy Lin <wendylw2013@gmail.com>
2023-12-05 15:52:45 +0800

    WB-6908: Update user changes for membership

--- src/redux/modules/user/index.js
+++ src/redux/modules/user/index.js
@@ -1,12 +1,45 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { fetchUserLoginStatus } from './thunks';
-import { API_REQUEST_STATUS } from '../../../utils/constants';
+import config from '../../../config';
+import { API_REQUEST_STATUS } from '../../../common/utils/constants';
+import Utils from '../../../utils/utils';
+import { fetchUserLoginStatus, fetchUserProfileInfo, syncUserPhoneNumber, loginUser, loginUserAsGuest } from './thunks';
 
+const localStoragePhoneNumber = Utils.getLocalStorageVariable('user.p');
 const initialState = {
   checkLoginRequest: {
     data: {
       consumerId: null,
       login: false,
+    },
+    status: null,
+    error: null,
+  },
+  loginRequest: {
+    data: {
+      isFirstLogin: false,
+    },
+    status: null,
+    error: null,
+  },
+  guestLoginRequest: {
+    data: {
+      isGuest: config.isGuest,
+    },
+    status: null,
+    error: null,
+  },
+  loadProfileRequest: {
+    data: {
+      id: null,
+      phone: localStoragePhoneNumber,
+      birthday: null,
+      email: null,
+      firstName: null,
+      lastName: null,
+      birthdayChangeAllowed: false,
+      birthdayModifiedTime: null,
+      gender: null,
+      notificationSettings: null,
     },
     status: null,
     error: null,
@@ -25,14 +58,83 @@ const { reducer, actions } = createSlice({
     [fetchUserLoginStatus.fulfilled.type]: (state, { payload }) => {
       const { consumerId, login } = payload;
 
-      state.checkLoginRequest.status = API_REQUEST_STATUS.FULFILLED;
       state.checkLoginRequest.data.consumerId = consumerId;
       state.checkLoginRequest.data.login = login;
+
+      state.checkLoginRequest.status = API_REQUEST_STATUS.FULFILLED;
       state.checkLoginRequest.error = null;
     },
     [fetchUserLoginStatus.rejected.type]: (state, { error }) => {
       state.checkLoginRequest.status = API_REQUEST_STATUS.REJECTED;
       state.checkLoginRequest.error = error;
+    },
+    [syncUserPhoneNumber.fulfilled.type]: (state, { payload }) => {
+      const { phone } = payload;
+
+      state.loadProfileRequest.phone = phone;
+    },
+    [loginUser.pending.type]: state => {
+      state.loginRequest.status = API_REQUEST_STATUS.PENDING;
+      state.loginRequest.error = null;
+    },
+    [loginUser.fulfilled.type]: (state, { payload }) => {
+      const { isFirstLogin } = payload;
+
+      state.loginRequest.data.isFirstLogin = isFirstLogin;
+
+      state.loginRequest.status = API_REQUEST_STATUS.FULFILLED;
+      state.loginRequest.error = null;
+    },
+    [loginUser.rejected.type]: (state, { error }) => {
+      state.loginRequest.status = API_REQUEST_STATUS.REJECTED;
+      state.loginRequest.error = error;
+    },
+    [fetchUserProfileInfo.pending.type]: state => {
+      state.loadProfileRequest.status = API_REQUEST_STATUS.PENDING;
+      state.loadProfileRequest.error = null;
+    },
+    [fetchUserProfileInfo.fulfilled.type]: (state, { payload }) => {
+      const {
+        phone,
+        birthday,
+        email,
+        firstName,
+        lastName,
+        birthdayChangeAllowed,
+        birthdayModifiedTime,
+        gender,
+        notificationSettings,
+      } = payload;
+
+      state.loadProfileRequest.data.phone = phone;
+      state.loadProfileRequest.data.birthday = birthday;
+      state.loadProfileRequest.data.email = email;
+      state.loadProfileRequest.data.firstName = firstName;
+      state.loadProfileRequest.data.lastName = lastName;
+      state.loadProfileRequest.data.birthdayChangeAllowed = birthdayChangeAllowed;
+      state.loadProfileRequest.data.birthdayModifiedTime = birthdayModifiedTime;
+      state.loadProfileRequest.data.gender = gender;
+      state.loadProfileRequest.data.notificationSettings = notificationSettings;
+
+      state.loadProfileRequest.status = API_REQUEST_STATUS.FULFILLED;
+      state.loadProfileRequest.error = null;
+    },
+    [fetchUserProfileInfo.rejected.type]: (state, { error }) => {
+      state.loadProfileRequest.status = API_REQUEST_STATUS.REJECTED;
+      state.loadProfileRequest.error = error;
+    },
+    [loginUserAsGuest.pending.type]: state => {
+      state.guestLoginRequest.status = API_REQUEST_STATUS.PENDING;
+      state.guestLoginRequest.error = null;
+    },
+    [loginUserAsGuest.fulfilled.type]: state => {
+      state.guestLoginRequest.data.isGuest = true;
+      state.guestLoginRequest.status = API_REQUEST_STATUS.FULFILLED;
+      state.guestLoginRequest.error = null;
+    },
+    [loginUserAsGuest.rejected.type]: (state, { error }) => {
+      state.guestLoginRequest.status = API_REQUEST_STATUS.REJECTED;
+      state.guestLoginRequest.error = error;
     },
   },
 });
--- src/redux/modules/user/thunks.js
+++ src/redux/modules/user/thunks.js
@@ -1,7 +1,140 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { getUserLoginStatus } from './api-request';
+import { getApiRequestShippingType } from '../../../common/utils';
+import Utils from '../../../utils/utils';
+import { KEY_EVENTS_FLOWS, KEY_EVENTS_STEPS } from '../../../utils/monitoring/constants';
+import logger from '../../../utils/monitoring/logger';
+import CleverTap from '../../../utils/clevertap';
+import { getUserLoginStatus, postUserLogin, getUserProfile, postLoginGuest } from './api-request';
+import { getConsumerId, getIsLogin } from './selectors';
 
+const { getRegistrationTouchPoint, getRegistrationSource } = Utils;
+
+/**
+ * @param {undefined}
+ * @return {Object} { consumerId, login }
+ */
 export const fetchUserLoginStatus = createAsyncThunk('app/user/getUserLoginStatus', async () => {
-  const { consumerId = null, login = false } = await getUserLoginStatus();
-  return { consumerId, login };
+  try {
+    const { consumerId = null, login = false } = await getUserLoginStatus();
+
+    return { consumerId, login };
+  } catch (error) {
+    logger.error('User_FetchUserLoginStatusFailed', { message: error?.message });
+
+    throw error;
+  }
+});
+
+export const syncUserPhoneNumber = createAsyncThunk('app/user/syncUserPhoneNumber', phone => {
+  Utils.setLocalStorageVariable('user.p', phone);
+
+  return phone;
+});
+
+/**
+ * @param {undefined}
+ * @return {Object} {id, phone, firstName, lastName, email, gender, birthday, birthdayModifiedTime, notificationSettings, birthdayChangeAllowed }
+ */
+export const fetchUserProfileInfo = createAsyncThunk(
+  'app/user/fetchUserProfileInfo',
+  async (_, { dispatch, getState }) => {
+    try {
+      const state = getState();
+      const consumerId = getConsumerId(state);
+
+      const result = await getUserProfile(consumerId);
+      const { phone } = result || {};
+
+      if (phone) {
+        dispatch(syncUserPhoneNumber(phone));
+      }
+
+      return result;
+    } catch (error) {
+      logger.error('User_FetchUserProfileInfoFailed', { message: error?.message });
+
+      throw error;
+    }
+  }
+);
+
+export const initUserInfo = createAsyncThunk('app/user/initUserInfo', async (_, { dispatch, getState }) => {
+  await dispatch(fetchUserLoginStatus());
+
+  const isLogin = getIsLogin(getState());
+
+  if (isLogin) {
+    await dispatch(fetchUserProfileInfo());
+  }
+});
+
+/**
+ * @param {Object} { accessToken, refreshToken, shippingType }
+ * @return {Object} { consumerId, user }
+ */
+export const loginUser = createAsyncThunk('app/user/loginUser', async ({ accessToken, refreshToken }) => {
+  const shippingType = getApiRequestShippingType();
+
+  try {
+    const { consumerId = null, user = null } = await postUserLogin({
+      accessToken,
+      refreshToken,
+      shippingType,
+      registrationTouchpoint: getRegistrationTouchPoint(),
+      registrationSource: getRegistrationSource(),
+    });
+
+    if (!consumerId) {
+      throw new Error('User login Response does not contain consumerId');
+    }
+
+    const { phone, firstName, email, birthday, isFirstLogin } = user || {};
+
+    CleverTap.pushEvent('Login - login successful', {
+      'new user': isFirstLogin,
+    });
+
+    CleverTap.onUserLogin({
+      Name: firstName,
+      Phone: phone,
+      Identity: consumerId,
+      Email: email,
+      DOB: birthday ? new Date(birthday) : undefined,
+    });
+
+    return { isFirstLogin };
+  } catch (error) {
+    CleverTap.pushEvent('Login - login failed');
+
+    logger.error(
+      'User_LoginFailed',
+      {
+        message: error?.message,
+        shippingType,
+      },
+      {
+        bizFlow: {
+          flow: KEY_EVENTS_FLOWS.LOGIN,
+          step: KEY_EVENTS_STEPS[KEY_EVENTS_FLOWS.LOGIN].SIGN_INTO_APP,
+        },
+        errorCategory: error?.category,
+      }
+    );
+
+    throw error;
+  }
+});
+
+export const syncUserLoginInfo = createAsyncThunk(
+  'app/user/syncUserLoginInfo',
+  async ({ accessToken, refreshToken }, { dispatch }) => {
+    await dispatch(loginUser({ accessToken, refreshToken }));
+    await dispatch(initUserInfo());
+  }
+);
+
+export const loginUserAsGuest = createAsyncThunk('app/user/loginUserAsGuest', async () => {
+  const result = await postLoginGuest();
+
+  return result;
 });
