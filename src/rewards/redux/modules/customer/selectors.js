782e6b64d46b9039b92e16b8267bf2ece07156a4
Wendy Lin <wendylw2013@gmail.com>
2023-12-14 16:46:37 +0800

    WB-6446: Update customer redux

---
+++ src/rewards/redux/modules/customer/selectors.js
@@ -0,0 +1,29 @@
+import _get from 'lodash/get';
+import { createSelector } from 'reselect';
+import { getIsAfterDateTime } from '../../../../utils/datetime-lib';
+
+export const getCustomerData = state => state.customer.loadCustomerRequest.data;
+
+export const getLoadCustomerRequestStatus = state => state.customer.loadCustomerRequest.status;
+
+export const getLoadCustomerRequestError = state => state.customer.loadCustomerRequest.error;
+
+export const getCustomerCashback = createSelector(getCustomerData, customerData =>
+  _get(customerData, 'storeCreditInfo.storeCreditsBalance', 0)
+);
+
+export const getCashbackExpiredDate = createSelector(getCustomerData, customerData =>
+  _get(customerData, 'storeCreditInfo.cashbackExpirationDate', null)
+);
+
+export const getIsCashbackExpired = createSelector(getCashbackExpiredDate, cashbackExpiredDate =>
+  getIsAfterDateTime(new Date(), cashbackExpiredDate)
+);
+
+export const getCustomerTierLevel = createSelector(getCustomerData, customerData =>
+  _get(customerData, 'customerTier.level', null)
+);
+
+export const getCustomerTierLevelName = createSelector(getCustomerData, customerData =>
+  _get(customerData, 'customerTier.name', null)
+);
---
+++ src/rewards/redux/modules/customer/thunks.js
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { getCustomerInfo } from './api-request';
+import { getConsumerId } from '../../../../redux/modules/user/selectors';
+
+export const fetchCustomerInfo = createAsyncThunk(
+  'app/merchant/fetchCustomerInfo',
+  async ({ business }, { getState }) => {
+    const state = getState();
+    const consumerId = getConsumerId(state);
+    const result = await getCustomerInfo({ consumerId, business });
+
+    return result;
+  }
+);
