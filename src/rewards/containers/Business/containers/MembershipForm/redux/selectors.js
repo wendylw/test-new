5abbab624ff6d127dd8d56af84d9316a631a6131
Philly Cai <philly.cai@storehub.com>
2023-12-20 11:28:23 +0800

    feat(WB-6937): optimize skeleton loader display condition

--- src/rewards/containers/Business/containers/MembershipForm/redux/selectors.js
+++ src/rewards/containers/Business/containers/MembershipForm/redux/selectors.js
@@ -5,7 +5,7 @@ import { getIsWebview } from '../../../../../redux/modules/common/selectors';
 import { CUSTOMER_NOT_FOUND_ERROR_CODE } from '../constants';
 import { FEATURE_KEYS } from '../../../../../../redux/modules/growthbook/constants';
 import { getFeatureFlagResult } from '../../../../../../redux/modules/growthbook/selectors';
-import { getIsLogin, getIsCheckLoginRequestCompleted } from '../../../../../../redux/modules/user/selectors';
+import { getIsLogin } from '../../../../../../redux/modules/user/selectors';
 import {
   getCustomerData,
   getLoadCustomerRequestStatus,
@@ -73,10 +73,8 @@ export const getCongratulationUrl = state =>
   getFeatureFlagResult(state, FEATURE_KEYS.FOUNDATION_OF_TIERED_MEMBERSHIP).congratsURL;
 
 export const getShouldShowSkeletonLoader = createSelector(
-  getIsCheckLoginRequestCompleted,
   getIsBusinessInfoRequestStatusCompleted,
-  (isCheckLoginRequestCompleted, isBusinessInfoRequestStatusCompleted) =>
-    !(isCheckLoginRequestCompleted && isBusinessInfoRequestStatusCompleted)
+  isBusinessInfoRequestStatusCompleted => !isBusinessInfoRequestStatusCompleted
 );
 
 export const getShouldShowUnsupportedError = createSelector(
