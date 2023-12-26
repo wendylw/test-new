6ade719f4a6ad1f3f131af11476eccc7e9f08429
Wendy Lin <wendylw2013@gmail.com>
2023-12-26 19:38:51 +0800

    WB-6888: Remove extra code

--- src/rewards/containers/Business/containers/SeamlessLoyalty/SeamlessLoyaltyProxy.jsx
+++ src/rewards/containers/Business/containers/SeamlessLoyalty/SeamlessLoyaltyProxy.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { getMerchantBusiness } from '../../../../redux/modules/merchant/selectors';
-// import { getIsWeb } from '../../../../redux/modules/common/selectors';
 
 const SeamlessLoyaltyProxy = () => {
-  // const merchantBusiness = useSelector(getMerchantBusiness);
-  // const isWeb = useSelector(getIsWeb);
-  // const seamlessLoyaltyURL = `${process.env.REACT_APP_MERCHANT_STORE_URL.replace(
-  //   '%business%',
-  //   merchantBusiness
-  // )}/loyalty/store-redemption`;
-
   return <div>SeamlessLoyaltyProxy</div>;
 };
 
