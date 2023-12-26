fb6d9518da1539d1e7697f1041fa96e2e3303b4b
Wendy Lin <wendylw2013@gmail.com>
2023-12-26 19:37:26 +0800

    WB-6888: Reset seamless loyalty proxy page

--- src/rewards/containers/Business/containers/SeamlessLoyalty/SeamlessLoyaltyProxy.jsx
+++ src/rewards/containers/Business/containers/SeamlessLoyalty/SeamlessLoyaltyProxy.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { getMerchantBusiness } from '../../../../redux/modules/merchant/selectors';
-import { getIsWeb } from '../../../../redux/modules/common/selectors';
+// import { useSelector } from 'react-redux';
+// import { getMerchantBusiness } from '../../../../redux/modules/merchant/selectors';
+// import { getIsWeb } from '../../../../redux/modules/common/selectors';
 
 const SeamlessLoyaltyProxy = () => {
-  const merchantBusiness = useSelector(getMerchantBusiness);
-  const isWeb = useSelector(getIsWeb);
-  const seamlessLoyaltyURL = `${process.env.REACT_APP_MERCHANT_STORE_URL.replace(
-    '%business%',
-    merchantBusiness
-  )}/loyalty/store-redemption`;
-  // get IsWeb
-  // Member Business (for seamless loyalty page, ${business}${process.env.REACT_APP_MERCHANT_STORE_URL}/loyalty/store-redemption)
+  // const merchantBusiness = useSelector(getMerchantBusiness);
+  // const isWeb = useSelector(getIsWeb);
+  // const seamlessLoyaltyURL = `${process.env.REACT_APP_MERCHANT_STORE_URL.replace(
+  //   '%business%',
+  //   merchantBusiness
+  // )}/loyalty/store-redemption`;
+
+  return <div>SeamlessLoyaltyProxy</div>;
 };
 
 SeamlessLoyaltyProxy.displayName = 'SeamlessLoyaltyProxy';
