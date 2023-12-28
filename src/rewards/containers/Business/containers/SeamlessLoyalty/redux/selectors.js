b176616aa1d731c8fa1b4d70db31207c30e09fb9
Wendy Lin <wendylw2013@gmail.com>
2023-12-28 17:50:03 +0800

    WB-6888: Update selector for page

--- src/rewards/containers/Business/containers/SeamlessLoyalty/redux/selectors.js
+++ src/rewards/containers/Business/containers/SeamlessLoyalty/redux/selectors.js
@@ -1,5 +1,22 @@
-import { getCookieVariable } from '../../../../../../common/utils';
+import { createSelector } from 'reselect';
+import { getQueryString } from '../../../../../../common/utils';
+import { getIsMerchantEnabledMembership } from '../../../../../redux/modules/merchant/selectors';
+import { getCustomerTier, getLoadCustomerRequestCompleted } from '../../../../../redux/modules/customer/selectors';
 
-export const getSource = () => getQueryString('source');
+export const getSeamlessLoyaltyRequestId = getQueryString('shareInfoReqId');
 
-export const getSeamlessLoyaltyRequestId = () => getCookieVariable('__sh_rdm_reqId');
+export const getIsRedirectToSeamlessLoyalty = createSelector(
+  getIsMerchantEnabledMembership,
+  getLoadCustomerRequestCompleted,
+  (isMerchantEnabledMembership, loadMerchantRequestCompleted) =>
+    loadMerchantRequestCompleted && !isMerchantEnabledMembership
+);
+
+export const getIsRedirectToMembershipDetail = createSelector(
+  getIsMerchantEnabledMembership,
+  getLoadMerchantRequestCompleted,
+  getCustomerTier,
+  getLoadCustomerRequestCompleted,
+  (isMerchantEnabledMembership, loadMerchantRequestCompleted, customerTier, loadCustomerRequestCompleted) =>
+    loadMerchantRequestCompleted && isMerchantEnabledMembership && loadCustomerRequestCompleted && customerTier
+);
