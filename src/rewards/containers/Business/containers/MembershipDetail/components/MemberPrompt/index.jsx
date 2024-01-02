76f5333d86fe987d81a9070b798e1b716ee62d30
Wendy Lin <wendylw2013@gmail.com>
2024-01-02 14:39:38 +0800

    WB-6888: Update merchant prompt UI

--- src/rewards/containers/Business/containers/MembershipDetail/components/MemberPrompt/index.jsx
+++ src/rewards/containers/Business/containers/MembershipDetail/components/MemberPrompt/index.jsx
@@ -2,23 +2,58 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useMount } from 'react-use';
 import { useTranslation } from 'react-i18next';
+import { BECOME_MERCHANT_MEMBER_METHODS } from '../../../../../../../common/utils/constants';
 import MembershipLevelIcon from '../../../../../../../images/membership-level.svg';
-import { getIsNewMember } from '../../../../redux/common/selectors';
+import RewardsEarnedCashbackIcon from '../../../../../../../images/rewards-earned-cashback.svg';
+import { getSource, getIsNewMember } from '../../../../redux/common/selectors';
 import { alert, toast } from '../../../../../../../common/utils/feedback';
 import { ObjectFitImage } from '../../../../../../../common/components/Image';
 import styles from './MemberPrompt.module.scss';
 
+const NEW_MEMBER_I18N_KEYS = {
+  [BECOME_MERCHANT_MEMBER_METHODS.SEAMLESS_LOYALTY_QR_SCAN]: {
+    titleKey: 'SeamlessLoyaltyNewMemberTitle',
+    descriptionKey: 'SeamlessLoyaltyNewMemberDescription',
+  },
+  [BECOME_MERCHANT_MEMBER_METHODS.JOIN_MEMBERSHIP_URL_CLICK]: {
+    titleKey: 'DefaultNewMemberTitle',
+    descriptionKey: 'DefaultNewMemberDescription',
+  },
+};
+
+const NEW_MEMBER_ICONS = {
+  [BECOME_MERCHANT_MEMBER_METHODS.SEAMLESS_LOYALTY_QR_SCAN]: RewardsEarnedCashbackIcon,
+  [BECOME_MERCHANT_MEMBER_METHODS.JOIN_MEMBERSHIP_URL_CLICK]: MembershipLevelIcon,
+};
+
+const RETURNING_MEMBER_I18N_KEYS = {
+  [BECOME_MERCHANT_MEMBER_METHODS.SEAMLESS_LOYALTY_QR_SCAN]: {
+    messageKey: 'SeamlessLoyaltyReturningMemberTitle',
+  },
+  [BECOME_MERCHANT_MEMBER_METHODS.JOIN_MEMBERSHIP_URL_CLICK]: {
+    messageKey: 'DefaultReturningMemberTitle',
+  },
+};
+
+const RETURNING_MEMBER_ICONS = {
+  [BECOME_MERCHANT_MEMBER_METHODS.SEAMLESS_LOYALTY_QR_SCAN]: RewardsEarnedCashbackIcon,
+};
+
 const NewMember = () => {
   const { t } = useTranslation(['Rewards']);
+  const source = useSelector(getSource);
+  const newMemberIcon = NEW_MEMBER_ICONS[source] || MembershipLevelIcon;
+  const newMemberTitle = t(NEW_MEMBER_I18N_KEYS[source]?.titleKey || 'DefaultNewMemberTitle');
+  const newMemberDescription = t(NEW_MEMBER_I18N_KEYS[source]?.descriptionKey || 'DefaultNewMemberDescription');
 
   useMount(() => {
     const content = (
       <div className={styles.NewMemberContent}>
         <div className={styles.NewMemberIcon}>
-          <ObjectFitImage noCompression src={MembershipLevelIcon} alt="Store New Member Icon in StoreHub" />
+          <ObjectFitImage noCompression src={newMemberIcon} alt="Store New Member Icon in StoreHub" />
         </div>
-        <h4 className={styles.NewMemberTitle}>{t('DefaultNewMemberTitle')}</h4>
-        <p className={styles.NewMemberContent}>{t('DefaultNewMemberDescription')}</p>
+        <h4 className={styles.NewMemberTitle}>{newMemberTitle}</h4>
+        <p className={styles.NewMemberDescription}>{newMemberDescription}</p>
       </div>
     );
 
@@ -32,11 +67,24 @@ NewMember.displayName = 'NewMember';
 
 const ReturningMember = () => {
   const { t } = useTranslation(['Rewards']);
+  const source = useSelector(getSource);
+  const isFromJoinMembershipUrlClick = source === BECOME_MERCHANT_MEMBER_METHODS.JOIN_MEMBERSHIP_URL_CLICK;
+  const returningMemberIcon = RETURNING_MEMBER_ICONS[source];
+  const returningMemberMessage = t(RETURNING_MEMBER_I18N_KEYS[source]?.titleKey || 'DefaultReturningMemberTitle');
 
   useMount(() => {
-    const content = t('DefaultReturningMemberMessage');
+    const content = (
+      <div className={styles.ReturningMemberContent}>
+        {returningMemberIcon && (
+          <div className={styles.ReturningMemberIcon}>
+            <ObjectFitImage noCompression src={returningMemberIcon} alt="Store Returning Member Icon in StoreHub" />
+          </div>
+        )}
+        <h4 className={styles.ReturningMemberTitle}>{returningMemberMessage}</h4>
+      </div>
+    );
 
-    toast.success(content);
+    isFromJoinMembershipUrlClick ? toast.success(t('DefaultReturningMemberMessage')) : alert(content);
   });
 
   return <></>;
