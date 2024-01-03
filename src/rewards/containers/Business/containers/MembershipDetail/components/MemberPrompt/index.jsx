c12c843b34522cf91a4a4b89cb9ad1fe0e39d723
Wendy Lin <wendylw2013@gmail.com>
2024-01-03 17:29:50 +0800

    WB-6446: remove is new member

--- src/rewards/containers/Business/containers/MembershipDetail/components/MemberPrompt/index.jsx
+++ src/rewards/containers/Business/containers/MembershipDetail/components/MemberPrompt/index.jsx
@@ -62,7 +62,7 @@ const ReturningMember = () => {
 ReturningMember.displayName = 'ReturningMember';
 
 const MemberPrompt = () => {
-  const isNewMember = useSelector(getIsNewMember) || true;
+  const isNewMember = useSelector(getIsNewMember);
 
   return isNewMember ? <NewMember /> : <ReturningMember />;
 };
