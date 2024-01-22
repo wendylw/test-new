43f07a9e2731054a5fe529b3b114f94e48987f90
Wendy Lin <wendylw2013@gmail.com>
2024-01-22 19:10:41 +0800

    WB-6630: Update api-request

--- src/rewards/containers/Business/containers/ClaimUniquePromo/redux/api-request.js
+++ src/rewards/containers/Business/containers/ClaimUniquePromo/redux/api-request.js
@@ -1,5 +1,3 @@
 import { post } from '../../../../../../utils/api/api-fetch';
 
-// export const postClaimUniquePromo = ({ id }) => post(`/api/v3/rewards-sets/${id}/redeem`);
-export const postClaimUniquePromo = () =>
-  post(`http://127.0.0.1:4523/m1/2755399-0-default/api/v3/rewards-sets/1/redeem`);
+export const postClaimUniquePromo = ({ id }) => post(`/api/v3/rewards-sets/${id}/redeem`);
