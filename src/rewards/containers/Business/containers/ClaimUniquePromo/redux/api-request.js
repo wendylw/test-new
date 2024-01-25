ffd1f2ceb106db50cfc409ff4405a2c5be8b7e57
Wendy Lin <wendylw2013@gmail.com>
2024-01-25 16:41:57 +0800

    WB-6630: reset mock api

--- src/rewards/containers/Business/containers/ClaimUniquePromo/redux/api-request.js
+++ src/rewards/containers/Business/containers/ClaimUniquePromo/redux/api-request.js
@@ -1,11 +1,8 @@
 import { post } from '../../../../../../utils/api/api-fetch';
 
-// export const postClaimUniquePromo = ({ id, consumerId, source, business: businessName }) =>
-//   post(`/api/v3/rewards-sets/${id}/redeem`, {
-//     consumerId,
-//     source,
-//     businessName,
-//   });
-
-export const postClaimUniquePromo = () =>
-  post('http://127.0.0.1:4523/m1/2755399-0-0a18a6d8/api/v3/rewards-sets/1/redeem?apifoxResponseId=338581286');
+export const postClaimUniquePromo = ({ id, consumerId, source, business: businessName }) =>
+  post(`/api/v3/rewards-sets/${id}/redeem`, {
+    consumerId,
+    source,
+    businessName,
+  });
