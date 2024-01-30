4bef2ba567e00659abc71419fcc51b3488d73130
Wendy Lin <wendylw2013@gmail.com>
2024-01-30 16:44:10 +0800

    WB-6450: remove login reference

--- src/cashback/containers/ClaimCashback/index.jsx
+++ src/cashback/containers/ClaimCashback/index.jsx
@@ -22,7 +22,6 @@ import Frame from '../../../common/components/Frame';
 import PageHeader from '../../../common/components/PageHeader';
 import MerchantInfo from './components/MerchantInfo';
 import CashbackBlock from './components/CashbackBlock';
-import PhoneNumberInput from '../../../common/components/Input/PhoneNumberInput/PhoneNumberInput';
 import styles from './ClaimCashback.module.scss';
 
 const ClaimCashback = () => {
@@ -82,7 +81,6 @@ const ClaimCashback = () => {
       />
       <MerchantInfo />
       <CashbackBlock />
-      <PhoneNumberInput />
       {!isLogin && <div className={styles.ClaimCashbackNotLoginBackground} />}
     </Frame>
   );
