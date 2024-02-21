8c7b8122815b84a3a4fdb91721f67b0c7e52b3dc
Wendy Lin <wendylw2013@gmail.com>
2024-02-21 18:42:03 +0800

    WB-6938: Revert api to production

--- src/redux/modules/membership/api-request.js
+++ src/redux/modules/membership/api-request.js
@@ -6,19 +6,9 @@ export const postUserMembership = ({ consumerId, business, source }) =>
     source,
   });
 
-// export const getMembershipsInfo = business =>
-//   get('/api/v3/memberships', {
-//     queryParams: {
-//       business,
-//     },
-//   });
-
-export const getMembershipsInfo = async business => {
-  const result = await get('http://127.0.0.1:4523/m1/2755399-0-default/api/v3/memberships', {
+export const getMembershipsInfo = business =>
+  get('/api/v3/memberships', {
     queryParams: {
       business,
     },
   });
-
-  return result.data;
-};
