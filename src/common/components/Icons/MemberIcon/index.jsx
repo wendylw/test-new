a166cf4f0e625b18567320dcd1237bf4cf40d6e7
Wendy Lin <wendylw2013@gmail.com>
2024-02-22 14:50:06 +0800

    WB-6938: Update membership progress styles

--- src/common/components/Icons/MemberIcon/index.jsx
+++ src/common/components/Icons/MemberIcon/index.jsx
@@ -4,44 +4,44 @@ import PropTypes from 'prop-types';
 // Must be added an id to the svg to avoid the same id error, when the component is used multiple times
 export const MemberIcon = ({
   className,
-  id,
+  style,
+  iconBackgroundId,
+  iconCrownId,
   crownStartColor,
   crownEndColor,
   backgroundStartColor,
   backgroundEndColor,
-}) => {
-  const paint0Id = `paint0_linear_3397_27287_${id}`;
-  const paint1Id = `paint1_linear_3397_27287_${id}`;
-
-  return (
-    <i className={className}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64" fill="none">
-        <path
-          d="M32 61.7464C48.4285 61.7464 61.7464 48.4285 61.7464 32C61.7464 15.5716 48.4285 2.25366 32 2.25366C15.5715 2.25366 2.25363 15.5716 2.25363 32C2.25363 48.4285 15.5715 61.7464 32 61.7464Z"
-          fill={`url(#${paint0Id})`}
-        />
-        <path
-          d="M18.4699 43.8388H45.5301L48.9126 26.9262L38.765 30.3087L32 20.1611L25.235 30.3087L15.0874 27.2154L18.4699 43.8388Z"
-          fill={`url(#${paint1Id})`}
-        />
-        <defs>
-          <linearGradient id={paint0Id} x1="32" y1="2.69277" x2="32" y2="61.024" gradientUnits="userSpaceOnUse">
-            <stop stopColor={backgroundStartColor} />
-            <stop offset="1" stopColor={backgroundEndColor} />
-          </linearGradient>
-          <linearGradient id={paint1Id} x1="16" y1="19.9999" x2="48" y2="43.9999" gradientUnits="userSpaceOnUse">
-            <stop stopColor={crownStartColor} />
-            <stop offset="1" stopColor={crownEndColor} />
-          </linearGradient>
-        </defs>
-      </svg>
-    </i>
-  );
-};
+}) => (
+  <i className={className} style={style}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64" fill="none">
+      <path
+        d="M32 61.7464C48.4285 61.7464 61.7464 48.4285 61.7464 32C61.7464 15.5716 48.4285 2.25366 32 2.25366C15.5715 2.25366 2.25363 15.5716 2.25363 32C2.25363 48.4285 15.5715 61.7464 32 61.7464Z"
+        fill={`url(#${iconBackgroundId})`}
+      />
+      <path
+        d="M18.4699 43.8388H45.5301L48.9126 26.9262L38.765 30.3087L32 20.1611L25.235 30.3087L15.0874 27.2154L18.4699 43.8388Z"
+        fill={`url(#${iconCrownId})`}
+      />
+      <defs>
+        <linearGradient id={iconBackgroundId} x1="32" y1="2.69277" x2="32" y2="61.024" gradientUnits="userSpaceOnUse">
+          <stop stopColor={backgroundStartColor} />
+          <stop offset="1" stopColor={backgroundEndColor} />
+        </linearGradient>
+        <linearGradient id={iconCrownId} x1="16" y1="19.9999" x2="48" y2="43.9999" gradientUnits="userSpaceOnUse">
+          <stop stopColor={crownStartColor} />
+          <stop offset="1" stopColor={crownEndColor} />
+        </linearGradient>
+      </defs>
+    </svg>
+  </i>
+);
 
 MemberIcon.propTypes = {
   className: PropTypes.string,
-  id: PropTypes.string,
+  // eslint-disable-next-line react/forbid-prop-types
+  style: PropTypes.object,
+  iconBackgroundId: PropTypes.string,
+  iconCrownId: PropTypes.string,
   crownStartColor: PropTypes.string,
   crownEndColor: PropTypes.string,
   backgroundStartColor: PropTypes.string,
@@ -49,7 +49,9 @@ MemberIcon.propTypes = {
 };
 MemberIcon.defaultProps = {
   className: null,
-  id: '0',
+  style: null,
+  iconBackgroundId: 'iconBackground',
+  iconCrownId: 'iconCrown',
   crownStartColor: '#91F7E7',
   crownEndColor: '#52A1FF',
   backgroundStartColor: '#DCFCFF',
--- src/rewards/containers/Business/containers/MembershipDetail/components/MemberCard/index.jsx
+++ src/rewards/containers/Business/containers/MembershipDetail/components/MemberCard/index.jsx
@@ -23,8 +23,6 @@ const MemberCard = () => {
   const memberCardTierProgressBarStyles = useSelector(getMemberCardTierProgressBarStyles);
   const customerMembershipTierList = useSelector(getCustomerMembershipTierList);
   const { crownStartColor, crownEndColor, backgroundStartColor, backgroundEndColor } = memberCardIconColors;
-  const progressContainerLevelNameClassName = getClassName([styles.MemberCardLevelName]);
-  const progressContainerPromptClassName = getClassName([styles.MemberCardLevelPrompt]);
 
   return (
     <section className={styles.MemberCardSection}>
@@ -32,27 +30,26 @@ const MemberCard = () => {
         <h1 className={styles.MemberCardStoreName}>{merchantDisplayName}</h1>
         {isCustomerMembershipProgressBarShow ? (
           <div className={styles.MemberCardLevelProgressContainer}>
-            <span className={progressContainerLevelNameClassName}>{customerTierLevelName}</span>
-            <p className={progressContainerPromptClassName}>
+            <span className={styles.MemberCardLevelName}>{customerTierLevelName}</span>
+            <p className={styles.MemberCardLevelProgressPrompt}>
               TODO: prompt will be replaced <Info size={18} />
             </p>
-            <ul className={styles.MemberCardMembershipTierProgress}>
+            <ul className={styles.MemberCardLevelProgress}>
               {customerMembershipTierList.map((tier, index) => {
                 const { level, name, iconColors, active, progress } = tier;
                 const iconKey = `membership-level-progress-icon-${level}`;
                 const progressBarKey = `membership-level-progress-bar-${level}`;
-                const memberCardLevelProgressFillClassName = getClassName([
-                  styles.MemberCardTierItemLevelProgressFill,
-                  progress !== '100%' && styles.MemberCardTierItemLevelProgressFill__notAchieved,
-                ]);
                 const MemberIconElement = (
                   <MemberIcon
-                    id={iconKey}
-                    className={styles.MemberCardTierLevelItemIcon}
+                    iconBackgroundId={`iconBackground_${iconKey}`}
+                    iconCrownId={`iconCrown_${iconKey}`}
+                    className={styles.MemberCardLevelProgressItemIcon}
                     style={
-                      active && {
-                        backgroundColor: memberCardTierProgressBarStyles.activeBackground,
-                      }
+                      active
+                        ? {
+                            backgroundColor: memberCardTierProgressBarStyles.activeBackground,
+                          }
+                        : null
                     }
                     crownStartColor={iconColors.crownStartColor}
                     crownEndColor={iconColors.crownEndColor}
@@ -65,9 +62,12 @@ const MemberCard = () => {
                 return (
                   <Fragment key={`membership-level-progress-item-${level}`}>
                     {index > 0 && (
-                      <li key={progressBarKey} className={styles.MemberCardTierLevelItemProgress}>
+                      <li key={progressBarKey} className={styles.MemberCardLevelProgressItemProgress}>
                         <span
-                          className={memberCardLevelProgressFillClassName}
+                          className={getClassName([
+                            styles.MemberCardLevelProgressItemFill,
+                            progress !== '100%' && styles.MemberCardLevelProgressItemFill__notAchieved,
+                          ])}
                           style={{
                             width: progress,
                             backgroundColor: memberCardTierProgressBarStyles.progressBackground,
@@ -75,10 +75,10 @@ const MemberCard = () => {
                         />
                       </li>
                     )}
-                    <li key={iconKey} className={styles.MemberCardTierLevelItem}>
+                    <li key={iconKey} className={styles.MemberCardLevelProgressItem}>
                       {MemberIconElement}
                       <span
-                        className="tw-absolute tw-bottom-0"
+                        className={styles.MemberCardLevelProgressItemLevelName}
                         style={{
                           color: memberCardTierProgressBarStyles.color,
                         }}
