7dcdd03b02eb5c93afdaece979e98862facf1424
Wendy Lin <wendylw2013@gmail.com>
2024-02-23 12:33:44 +0800

    WB-7496: Update valid phone numbers

--- src/common/utils/phone-number-constants.js
+++ src/common/utils/phone-number-constants.js
@@ -247,3 +247,13 @@ export const COUNTRY_PHONE_CODES = {
 };
 
 export const COUNTRIES = Object.keys(COUNTRY_PHONE_CODES);
+
+export const PHONE_NUMBER_COUNTRIES = {
+  MY: '60',
+  TH: '66',
+  PH: '63',
+  SG: '65',
+  CN: '86',
+};
+
+export const VALIDATION_COUNTRIES = Object.keys(PHONE_NUMBER_COUNTRIES);
