278f0c4e97281c057a450de9bca6ecafe888744b
Wendy Lin <wendylw2013@gmail.com>
2024-03-06 19:21:06 +0800

    WB-7625: Update benefit description for tabs

--- src/rewards/containers/Business/components/MembershipTiersInfoTabs/index.jsx
+++ src/rewards/containers/Business/components/MembershipTiersInfoTabs/index.jsx
@@ -7,12 +7,37 @@ import {
 } from '../../../../redux/modules/common/selectors';
 import styles from './MembershipTiersInfoTabs.module.scss';
 
+const getCurrentActiveBlockInfo = activeIndex => {
+  const buttonElement = document.getElementById(`membership-tier-button-${activeIndex}`);
+
+  if (buttonElement) {
+    return {
+      offsetLeft: buttonElement.offsetLeft,
+      width: buttonElement.offsetWidth,
+    };
+  }
+
+  return null;
+};
+
 const MembershipTiersInfoTabs = () => {
   const isMembershipBenefitTabsShown = useSelector(getIsMembershipBenefitTabsShown);
   const membershipTiersBenefit = useSelector(getMerchantMembershipTiersBenefit);
   const [activeIndex, setActiveIndex] = useState(0);
+  const [activeBlockInfo, setActiveBlockInfo] = useState({
+    offsetLeft: 0,
+    width: 0,
+  });
   const handleClickMembershipTierButton = index => {
     setActiveIndex(index);
+    const activeInfo = getCurrentActiveBlockInfo(index);
+
+    if (activeInfo) {
+      setActiveBlockInfo({
+        offsetLeft: activeInfo.offsetLeft,
+        width: activeInfo.width,
+      });
+    }
   };
 
   return (
@@ -22,20 +47,36 @@ const MembershipTiersInfoTabs = () => {
         {isMembershipBenefitTabsShown ? (
           <>
             <ul className={styles.MembershipTiersInfoTabs}>
-              {membershipTiersBenefit.map((tier, index) => (
-                <li key={`membership-tier-name-${tier.level}`} className={styles.MembershipTiersInfoTab}>
-                  <button
-                    className={styles.MembershipTiersInfoTabName}
-                    data-test-id="rewards.business.membership-tiers-info-tabs.tier-button"
-                    onClick={() => {
-                      handleClickMembershipTierButton(index);
-                    }}
-                  >
-                    {tier.name}
-                  </button>
-                </li>
-              ))}
+              {membershipTiersBenefit.map((tier, index) => {
+                const membershipTiersBenefitButtonClassName = getClassName([
+                  styles.MembershipTiersInfoTabName,
+                  activeIndex === index && styles.MembershipTiersInfoTabName__active,
+                ]);
+
+                return (
+                  <li key={`membership-tier-name-${tier.level}`} className={styles.MembershipTiersInfoTab}>
+                    <button
+                      id={`membership-tier-button-${index}`}
+                      className={membershipTiersBenefitButtonClassName}
+                      data-test-id="rewards.business.membership-tiers-info-tabs.tier-button"
+                      onClick={() => {
+                        handleClickMembershipTierButton(index);
+                      }}
+                    >
+                      {tier.name}
+                    </button>
+                  </li>
+                );
+              })}
             </ul>
+            <div
+              className={styles.MembershipTiersInfoTabsActiveBlock}
+              style={{
+                left: `${activeBlockInfo.offsetLeft}px`,
+                width: `${activeBlockInfo.width}px`,
+                transition: 'left 0.3s, width 0.3s',
+              }}
+            />
             {membershipTiersBenefit.map((benefit, index) => {
               const benefitDescriptionClassName = getClassName([
                 styles.MembershipTiersInfoTabContent,
@@ -53,9 +94,14 @@ const MembershipTiersInfoTabs = () => {
               );
             })}
           </>
-        ) : (
-          'test'
-        )}
+        ) : membershipTiersBenefit[0] ? (
+          <div
+            className={styles.MembershipTiersInfoTabContent__active}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: membershipTiersBenefit[0].description }}
+            data-test-id="rewards.business.membership-tiers-info-tabs.benefit-description"
+          />
+        ) : null}
       </div>
     </section>
   );
