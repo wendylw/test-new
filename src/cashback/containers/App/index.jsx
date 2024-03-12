f4c366bc86aaa877cb2d4f588b383e75857f8004
Wendy Lin <wendylw2013@gmail.com>
2024-03-12 17:06:26 +0800

    WB-6994: Remove TNGD login

--- src/cashback/containers/App/index.jsx
+++ src/cashback/containers/App/index.jsx
@@ -3,85 +3,37 @@ import PropTypes from 'prop-types';
 import { bindActionCreators, compose } from 'redux';
 import { connect } from 'react-redux';
 import { withTranslation } from 'react-i18next';
+import faviconImage from '../../../images/favicon.ico';
 import {
   actions as appActionCreators,
-  getUserCountry,
   getError,
   getLoginBannerPrompt,
   getIsUserLogin,
   getIsLoginRequestStatusPending,
   getIsLoginModalShown,
   getIsClaimCashbackPage,
-  getIsSeamlessLoyaltyPage,
-  getIsHomePage,
-  getLoginAlipayMiniProgramRequestError,
 } from '../../redux/modules/app';
 import { getPageError } from '../../../redux/modules/entities/error';
 import Constants from '../../../utils/constants';
 import { isWebview } from '../../../common/utils';
 import { isAlipayMiniProgram } from '../../../common/utils/alipay-miniprogram-client';
-import faviconImage from '../../../images/favicon.ico';
 import '../../../Common.scss';
 import './Loyalty.scss';
 import Routes from '../Routes';
 import ErrorToast from '../../../components/ErrorToast';
 import Message from '../../components/Message';
 import Login from '../../components/Login';
-import { confirm } from '../../../common/utils/feedback';
 import DocumentFavicon from '../../../components/DocumentFavicon';
 import logger from '../../../utils/monitoring/logger';
-import Clevertap from '../../../utils/clevertap';
 
 class App extends Component {
   async componentDidMount() {
-    const { t, appActions, userCountry, isClaimCashbackPage, isSeamlessLoyaltyPage, isHomePage } = this.props;
+    const { appActions } = this.props;
 
     this.visitErrorPage();
 
     try {
       await appActions.fetchCashbackBusiness();
-
-      // 2. login
-      // TNGD code is executed at the very beginning.
-      // Because the MP and Beep accounts are not synchronized,
-      // it is impossible to determine that the accounts are the same
-      if (isAlipayMiniProgram() && !(isClaimCashbackPage || isSeamlessLoyaltyPage || isHomePage)) {
-        // the user information of the 3rd MiniProgram may be different, so synchronize the data of the consumer once
-        await appActions.loginByAlipayMiniProgram();
-
-        const { loginAlipayMiniProgramRequestError } = this.props;
-
-        // TNGD & GCash code is executed at the very beginning.
-        // Because the MP and Beep accounts are not synchronized,
-        // it is impossible to determine that the accounts are the same
-        // https://opendocs.alipay.com/support/01rb4b?ant_source=opendoc_recommend
-        if (loginAlipayMiniProgramRequestError) {
-          confirm(t('UnexpectedErrorOccurred'), {
-            closeByBackButton: false,
-            closeByBackDrop: false,
-            cancelButtonContent: t('Cancel'),
-            confirmButtonContent: t('TryAgain'),
-            onSelection: async confirmStatus => {
-              if (confirmStatus) {
-                // try again
-                Clevertap.pushEvent('Loyalty Page (Login Error Pop-up) - Click Try Again', {
-                  country: userCountry,
-                });
-                await appActions.loginByAlipayMiniProgram();
-              } else {
-                // cancel
-                if (window.my.exitMiniProgram) {
-                  window.my.exitMiniProgram();
-                }
-                Clevertap.pushEvent('Loyalty Page (Login Error Pop-up) - Click Cancel', {
-                  country: userCountry,
-                });
-              }
-            },
-          });
-        }
-      }
-
       await appActions.loadConsumerLoginStatus();
 
       const { isUserLogin } = this.props;
@@ -161,12 +113,9 @@ class App extends Component {
 App.displayName = 'CashbackApp';
 
 App.propTypes = {
-  userCountry: PropTypes.string,
   loginBannerPrompt: PropTypes.string,
   isUserLogin: PropTypes.bool,
   isClaimCashbackPage: PropTypes.bool,
-  isSeamlessLoyaltyPage: PropTypes.bool,
-  isHomePage: PropTypes.bool,
   error: PropTypes.shape({
     message: PropTypes.string,
   }),
@@ -174,7 +123,6 @@ App.propTypes = {
     code: PropTypes.number,
   }),
   isLoginModalShown: PropTypes.bool,
-  loginAlipayMiniProgramRequestError: PropTypes.object || PropTypes.string,
   appActions: PropTypes.shape({
     loadConsumerLoginStatus: PropTypes.func,
     resetConsumerLoginStatus: PropTypes.func,
@@ -191,16 +139,12 @@ App.propTypes = {
 };
 
 App.defaultProps = {
-  userCountry: null,
   loginBannerPrompt: null,
   isUserLogin: false,
   isClaimCashbackPage: false,
-  isSeamlessLoyaltyPage: false,
-  isHomePage: false,
   error: {},
   pageError: {},
   isLoginModalShown: false,
-  loginAlipayMiniProgramRequestError: null,
   appActions: {},
 };
 
@@ -212,13 +156,9 @@ export default compose(
       isLoginRequestStatusPending: getIsLoginRequestStatusPending(state),
       isUserLogin: getIsUserLogin(state),
       isClaimCashbackPage: getIsClaimCashbackPage(state),
-      isSeamlessLoyaltyPage: getIsSeamlessLoyaltyPage(state),
-      isHomePage: getIsHomePage(state),
       isLoginModalShown: getIsLoginModalShown(state),
       error: getError(state),
       pageError: getPageError(state),
-      loginAlipayMiniProgramRequestError: getLoginAlipayMiniProgramRequestError(state),
-      userCountry: getUserCountry(state),
     }),
     dispatch => ({
       appActions: bindActionCreators(appActionCreators, dispatch),
