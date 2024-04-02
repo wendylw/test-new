32c30bfc66bd7ad7d67cce5c981b4ad999c92b83
Wendy Lin <wendylw2013@gmail.com>
2024-04-02 10:45:07 +0800

    WB-7420: Remove testing code

--- src/rewards/containers/Business/containers/PointsHistory/redux/api-request.js
+++ src/rewards/containers/Business/containers/PointsHistory/redux/api-request.js
@@ -1,7 +1,7 @@
 import { get } from '../../../../../../utils/api/api-fetch';
 
 export const getPointsHistoryList = async ({ consumerId, business: merchantName, page, limit }) =>
-  get('http://127.0.0.1:4523/m1/2755399-0-default/api/v3/points/history', {
+  get('/api/v3/points/history', {
     queryParams: {
       consumerId,
       merchantName,
