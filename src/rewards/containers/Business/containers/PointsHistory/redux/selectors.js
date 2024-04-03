0be583eaa563b045cc675b2fd0169df147d6a082
Wendy Lin <wendylw2013@gmail.com>
2024-04-03 17:48:00 +0800

    WB-7420: Update redux

--- src/rewards/containers/Business/containers/PointsHistory/redux/selectors.js
+++ src/rewards/containers/Business/containers/PointsHistory/redux/selectors.js
@@ -1,15 +1,11 @@
+import i18next from 'i18next';
 import { createSelector } from 'reselect';
+import { API_REQUEST_STATUS } from '../../../../../../common/utils/constants';
 import { toLocaleDateString } from '../../../../../../utils/datetime-lib';
-import { POINTS_HISTORY_LOG_I18N_KEYS, DATE_OPTIONS } from '../utils/constatns';
+import { POINTS_HISTORY_LOG_I18N_KEYS, DATE_OPTIONS } from '../utils/constants';
 import { getMerchantCountry } from '../../../../../../redux/modules/merchant/selectors';
 
 export const getIsEarnedPointsPromptDrawerShow = state => state.business.pointsHistory.isEarnedPointsPromptDrawerShow;
-
-export const getPointsHistoryListPage = state => state.business.pointsHistory.page;
-
-export const getPointsHistoryListLimit = state => state.business.pointsHistory.limit;
-
-export const getIsPointsHistoryListEnded = state => state.business.pointsHistory.end;
 
 export const getLoadPointsHistoryListData = state =>
   state.business.pointsHistory.loadPointsHistoryListRequest.data || [];
@@ -28,20 +24,28 @@ export const getPointsHistoryList = createSelector(
     getLoadPointsHistoryList.map(pointsHistoryItem => {
       const { id, type, eventTime, changeAmount } = pointsHistoryItem || {};
       const isReduce = changeAmount < 0;
+      const changeValue = `${isReduce ? '-' : '+'}${Math.abs(changeAmount)}`;
 
       return {
         id,
         nameI18nKey: POINTS_HISTORY_LOG_I18N_KEYS[type],
         logDateTime: toLocaleDateString(eventTime, merchantCountry, DATE_OPTIONS),
-        changePoints: `${isReduce ? '-' : '+'}${Math.abs(changeAmount)}`,
+        changeValueText: i18next.t('Rewards:ChangePointsText', { changeValue }),
+        changeValue,
         isReduce,
       };
     })
 );
 
+export const getIsLoadPointsHistoryListCompleted = createSelector(
+  getLoadPointsHistoryListStatus,
+  loadPointsHistoryListStatus =>
+    [API_REQUEST_STATUS.FULFILLED, API_REQUEST_STATUS.REJECTED].includes(loadPointsHistoryListStatus)
+);
+
 export const getIsPointsHistoryListEmpty = createSelector(
   getLoadPointsHistoryListData,
-  getIsPointsHistoryListEnded,
-  (loadPointsHistoryListData, isPointsHistoryListEnded) =>
-    loadPointsHistoryListData.length === 0 && isPointsHistoryListEnded
+  getIsLoadPointsHistoryListCompleted,
+  (loadPointsHistoryListData, isLoadPointsHistoryListCompleted) =>
+    loadPointsHistoryListData.length === 0 && isLoadPointsHistoryListCompleted
 );
