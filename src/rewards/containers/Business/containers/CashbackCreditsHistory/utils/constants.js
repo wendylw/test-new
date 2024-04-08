0eb1dca72b4c40996aa3d86685bb544765fd6f98
Wendy Lin <wendylw2013@gmail.com>
2024-04-08 14:10:06 +0800

    WB-7511: Update utils constatns for cashback

---
+++ src/rewards/containers/Business/containers/CashbackCreditsHistory/utils/constants.js
@@ -0,0 +1,22 @@
+export const CASHBACK_HISTORY_TYPES = {
+  EARNED: 'earned',
+  EXPENSE: 'expense',
+  PENDING: 'pending',
+  EXPIRED: 'expired',
+  ADJUSTMENT: 'adjustment',
+};
+
+export const CASHBACK_HISTORY_LOG_I18N_KEYS = {
+  [CASHBACK_HISTORY_TYPES.EARNED]: 'Earned',
+  [CASHBACK_HISTORY_TYPES.EXPENSE]: 'Redeemed',
+  [CASHBACK_HISTORY_TYPES.PENDING]: 'Pending',
+  [CASHBACK_HISTORY_TYPES.EXPIRED]: 'Expired',
+  [CASHBACK_HISTORY_TYPES.ADJUSTMENT]: 'Adjustment',
+};
+
+export const DATE_OPTIONS = {
+  weekday: 'short',
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+};
