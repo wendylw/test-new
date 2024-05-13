755c9b17ad28944beb69368acd895a743e8db990
Wendy Lin <wendylw2013@gmail.com>
2024-05-13 18:42:56 +0800

    WB-8130: Update cashback UI

--- src/rewards/containers/Business/containers/MembershipForm/components/RewardsDescription/index.jsx
+++ src/rewards/containers/Business/containers/MembershipForm/components/RewardsDescription/index.jsx
@@ -1,51 +1,52 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import RewardsJoinMembershipImage from '../../../../../../../images/rewards-join-membership.svg';
-import RewardsPointsWhiteIcon from '../../../../../../../images/rewards-points-icon-white.svg';
+// import RewardsPointsWhiteIcon from '../../../../../../../images/rewards-points-icon-white.svg';
 import RewardsCashbackWhiteIcon from '../../../../../../../images/rewards-cashback-icon-white.svg';
-import RewardsStoreCreditsWhiteIcon from '../../../../../../../images/rewards-store-credits-icon-white.svg';
+// import RewardsStoreCreditsWhiteIcon from '../../../../../../../images/rewards-store-credits-icon-white.svg';
 import RewardsDiscountWhiteIcon from '../../../../../../../images/rewards-discount-icon-white.svg';
 import RewardsVouchersWhiteIcon from '../../../../../../../images/rewards-vouchers-icon-white.svg';
 import { ObjectFitImage } from '../../../../../../../common/components/Image';
+import styles from './RewardsDescription.module.scss';
 
 const RewardsDescription = () => {
   const { t } = useTranslation(['Rewards']);
 
   return (
-    <div>
+    <div className={styles.RewardsDescription}>
       <div className={styles.RewardsDescriptionImageContainer}>
         <ObjectFitImage noCompression src={RewardsJoinMembershipImage} />
       </div>
-      <p>{t('JoinMembershipRewardsPrompt')}</p>
-      <ol>
-        {/* <li>
+      <p className={styles.RewardsDescriptionPrompt}>{t('JoinMembershipRewardsPrompt')}</p>
+      <ol className={styles.RewardsDescriptionIconList}>
+        {/* <li className={styles.RewardsDescriptionIconItem}>
           <div className={styles.RewardsDescriptionIconContainer}>
             <ObjectFitImage noCompression src={RewardsPointsWhiteIcon} />
           </div>
           <span></span>
         </li> */}
-        <li>
+        <li className={styles.RewardsDescriptionIconItem}>
           <div className={styles.RewardsDescriptionIconContainer}>
             <ObjectFitImage noCompression src={RewardsCashbackWhiteIcon} />
           </div>
-          <span>{t('Cashback')}</span>
+          <span className={styles.RewardsDescriptionItemText}>{t('Cashback')}</span>
         </li>
-        {/* <li>
+        {/* <li className={styles.RewardsDescriptionIconItem}>
           <div className={styles.RewardsDescriptionIconContainer}>
             <ObjectFitImage noCompression src={RewardsStoreCreditsWhiteIcon} />
           </div>
         </li> */}
-        <li>
+        <li className={styles.RewardsDescriptionIconItem}>
           <div className={styles.RewardsDescriptionIconContainer}>
             <ObjectFitImage noCompression src={RewardsDiscountWhiteIcon} />
           </div>
-          <span>{t('Discounts')}</span>
+          <span className={styles.RewardsDescriptionItemText}>{t('Discounts')}</span>
         </li>
-        <li>
+        <li className={styles.RewardsDescriptionIconItem}>
           <div className={styles.RewardsDescriptionIconContainer}>
             <ObjectFitImage noCompression src={RewardsVouchersWhiteIcon} />
           </div>
-          <span>{t('Vouchers')}</span>
+          <span className={styles.RewardsDescriptionItemText}>{t('Vouchers')}</span>
         </li>
       </ol>
     </div>
