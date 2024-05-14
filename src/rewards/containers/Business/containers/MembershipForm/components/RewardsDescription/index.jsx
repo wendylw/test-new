c7051feecb8b5941011b4cb99d56f64dccbcd045
Wendy Lin <wendylw2013@gmail.com>
2024-05-14 11:33:16 +0800

    WB-8132: remove cashback

--- src/rewards/containers/Business/containers/MembershipForm/components/RewardsDescription/index.jsx
+++ src/rewards/containers/Business/containers/MembershipForm/components/RewardsDescription/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+// import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import RewardsJoinMembershipImage from '../../../../../../../images/rewards-join-membership.svg';
 // import RewardsPointsWhiteIcon from '../../../../../../../images/rewards-points-icon-white.svg';
-import RewardsCashbackWhiteIcon from '../../../../../../../images/rewards-cashback-icon-white.svg';
+// import RewardsCashbackWhiteIcon from '../../../../../../../images/rewards-cashback-icon-white.svg';
 // import RewardsStoreCreditsWhiteIcon from '../../../../../../../images/rewards-store-credits-icon-white.svg';
 import RewardsDiscountWhiteIcon from '../../../../../../../images/rewards-discount-icon-white.svg';
 import RewardsVouchersWhiteIcon from '../../../../../../../images/rewards-vouchers-icon-white.svg';
 import { ObjectFitImage } from '../../../../../../../common/components/Image';
-import { getIsMerchantEnabledCashback } from '../../../../../../../redux/modules/merchant/selectors';
+// import { getIsMerchantEnabledCashback } from '../../../../../../../redux/modules/merchant/selectors';
 import styles from './RewardsDescription.module.scss';
-import { useSelector } from 'react-redux';
 
 const RewardsDescription = () => {
   const { t } = useTranslation(['Rewards']);
-  const isMerchantEnabledCashback = useSelector(getIsMerchantEnabledCashback);
+  // const isMerchantEnabledCashback = useSelector(getIsMerchantEnabledCashback);
 
   return (
     <div className={styles.RewardsDescription}>
@@ -28,14 +28,14 @@ const RewardsDescription = () => {
           </div>
           <span></span>
         </li> */}
-        {isMerchantEnabledCashback && (
+        {/* {isMerchantEnabledCashback && (
           <li className={styles.RewardsDescriptionIconItem}>
             <div className={styles.RewardsDescriptionIconContainer}>
               <ObjectFitImage noCompression src={RewardsCashbackWhiteIcon} />
             </div>
             <span className={styles.RewardsDescriptionItemText}>{t('Cashback')}</span>
           </li>
-        )}
+        )} */}
         {/* <li className={styles.RewardsDescriptionIconItem}>
           <div className={styles.RewardsDescriptionIconContainer}>
             <ObjectFitImage noCompression src={RewardsStoreCreditsWhiteIcon} />
