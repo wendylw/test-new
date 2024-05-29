34e67447e33784afba95cd4eb8654cde46f97f2c
Wendy Lin <wendylw2013@gmail.com>
2024-05-29 17:19:40 +0800

    WB-8224: Update new membership frame

---
+++ src/rewards/containers/Business/containers/NewMembershipDetail/components/MyRewards/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyRewards = () => <section>MyRewards</section>;
+
+MyRewards.displayName = 'MyRewards';
+
+export default MyRewards;
---
+++ src/rewards/containers/Business/containers/NewMembershipDetail/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Frame from '../../../../../common/components/Frame';
+
+const MembershipDetail = () => <Frame>MembershipDetail</Frame>;
+
+MembershipDetail.displayName = 'MembershipDetail';
+
+export default MembershipDetail;
