b8648e130bb70a0f11fb5a9df145a23bb3175d9e
Wendy Lin <wendylw2013@gmail.com>
2024-07-24 12:34:45 +0800

    Release: 2024-07-24 (#3731)
    * WB-8703: Remove wb-6329_foundation-of-tiered-membership Feature Flag in Codebase
    
    * WB-8703: revert backend
    
    * WB-8703: Remove ever business profile
    
    * Revert backend to release

--- src/redux/modules/growthbook/constants.js
+++ src/redux/modules/growthbook/constants.js
@@ -1,5 +1,4 @@
 export const FEATURE_KEYS = {
-  FOUNDATION_OF_TIERED_MEMBERSHIP: 'wb-6329_foundation-of-tiered-membership',
   CLAIM_UNIQUE_PROMO: 'wb-6632_claim-unique-promo',
   SHOW_TIERED_MEMBERSHIP_BENEFIT: 'wb-7625_show_tier_benefit_in_join_membership_and_details_page',
   JOIN_MEMBERSHIP_NEW_DESIGN: 'wb-7756_join-membership-new-design',
@@ -8,10 +7,6 @@ export const FEATURE_KEYS = {
 };
 
 export const DEFAULT_FEATURE_FLAG_RESULTS = {
-  [FEATURE_KEYS.FOUNDATION_OF_TIERED_MEMBERSHIP]: {
-    introURL: 'https://lp.storehub.com/beep-test-join-membership',
-    congratsURL: 'https://lp.storehub.com/beep-membership-welcome',
-  },
   [FEATURE_KEYS.CLAIM_UNIQUE_PROMO]: {
     introURL: 'https://lp.storehub.com/claim-promo-url-centreplacecafe',
     congratsURL: 'https://lp.storehub.com/promotion-claimed',
