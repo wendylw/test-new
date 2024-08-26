97de12aee44e8b309a35aea0b245058a2fd2d705
wendylw <wendylw2013@gmail.com>
2024-08-26 12:42:22 +0800

    WB-9043: Save consumer profile when claiming voucher, the page prompts ‘40002Title’

--- src/rewards/containers/Profile/ProfileFooter/index.jsx
+++ src/rewards/containers/Profile/ProfileFooter/index.jsx
@@ -36,7 +36,7 @@ const ProfileFooter = ({ onSave }) => {
       }
 
       // 40002 is common error for verification failed. BE set this code as profile common error
-      alert(t('ApiError:40002Description'), { title: t('ApiError:40002Title', { error_code: '40002' }) });
+      alert(t('40002Description'), { title: t('40002Title', { error_code: '40002' }) });
     }
   }, [dispatch, onConfirmDuplicatedEmailNextStep, onSave, t]);
 
