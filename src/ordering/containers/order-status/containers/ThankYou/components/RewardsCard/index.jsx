ccbac06f9f0c1a9742cd7d6255c57873823d155c
wendylw <wendylw2013@gmail.com>
2024-09-03 19:36:43 +0800

    WB-8769: Update rewards DOM

--- src/ordering/containers/order-status/containers/ThankYou/components/RewardsCard/index.jsx
+++ src/ordering/containers/order-status/containers/ThankYou/components/RewardsCard/index.jsx
@@ -1,3 +1,36 @@
 import React from 'react';
 
-const RewardsCard = () => {};
+const RewardsCard = () => (
+  <>
+    <section>
+      <div>
+        <img src="" alt="" />
+        <span>You have 3 Rewards!</span>
+      </div>
+    </section>
+    <section>
+      <div>
+        <h3>Claim Rewards</h3>
+        <a href="">Check Balance</a>
+      </div>
+      <ul>
+        <li>
+          <img src="" alt="" />
+          <span>3 Rewards</span>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <span>200 Points</span>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <span>RM 1.23 Cashback</span>
+        </li>
+      </ul>
+    </section>
+  </>
+);
+
+RewardsCard.displayName = 'RewardsCard';
+
+export default RewardsCard;
