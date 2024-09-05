949951b2dec4d8f9abf2c8522dcf3ebbc12a29e1
wendylw <wendylw2013@gmail.com>
2024-09-05 15:58:00 +0800

    WB-8769: Update rewards DOM

--- src/ordering/containers/order-status/containers/ThankYou/components/Rewards/index.jsx
+++ src/ordering/containers/order-status/containers/ThankYou/components/Rewards/index.jsx
@@ -10,13 +10,26 @@ import Button from '../../../../../../../common/components/Button';
 import { ObjectFitImage } from '../../../../../../../common/components/Image';
 import './Rewards.scss';
 
-const Rewards = ({ isNewMember, enabledCashback, enabledLoyalty }) => {
+const Rewards = ({
+  isNewMember,
+  enabledCashback,
+  enabledLoyalty,
+  enabledMembershipPoints,
+  rewardsAvailableCount,
+  points,
+  cashbackPrice,
+  storeCreditsPrice,
+}) => {
   const { t } = useTranslation('OrderingThankYou');
   const enabledCashbackOrStoreCredits = enabledCashback || enabledLoyalty;
+  const isRewardsCountAvailable = rewardsAvailableCount > 0;
+  const shouldShowRewardsBanner = isRewardsCountAvailable > 0 && !isNewMember;
+  const shouldShowRewardsItem = isRewardsCountAvailable && isNewMember;
+  const shouldShowPointsItem = enabledMembershipPoints && points > 0;
 
   return (
     <>
-      {!isNewMember && (
+      {shouldShowRewardsBanner && (
         <section className="card rewards-banner__card-wrapper margin-small">
           <Button
             type="text"
@@ -28,16 +41,20 @@ const Rewards = ({ isNewMember, enabledCashback, enabledLoyalty }) => {
             <div className="rewards-banner__card-icon">
               <ObjectFitImage noCompression src={RewardsIcon} alt="StoreHub Rewards Icon" />
             </div>
-            <p className="rewards-banner__card-title text-left padding-left-right-small">{t('RewardBannerTitle')}</p>
+            <p className="rewards-banner__card-title text-left padding-left-right-small">
+              {t('RewardBannerTitle', {
+                availableCount: rewardsAvailableCount,
+              })}
+            </p>
 
             <CaretRight size={32} weight="light" />
           </Button>
         </section>
       )}
 
-      <section>
+      <section className="card margin-small padding-smaller">
         <div className="flex flex-middle flex-space-between">
-          <h3 className="rewards-card__title">
+          <h3 className="rewards-card__title padding-small">
             {isNewMember ? t('RewardsCardNewMemberTitle') : t('RewardsCardReturningMemberTitle')}
           </h3>
           {!isNewMember && (
@@ -51,30 +68,56 @@ const Rewards = ({ isNewMember, enabledCashback, enabledLoyalty }) => {
             </Button>
           )}
         </div>
-        <ul>
-          <li className="flex flex-middle">
-            <div className="rewards-card__item-icon flex__shrink-fixed">
-              <ObjectFitImage noCompression src={RewardsIcon} alt="StoreHub Rewards Icon" />
-            </div>
-            <span className="flex__fluid-content">3 Rewards</span>
-          </li>
-          <li className="flex flex-middle">
-            <div className="rewards-card__item-icon flex__shrink-fixed">
-              <ObjectFitImage noCompression src={RewardsPointsIcon} alt="StoreHub Rewards Icon" />
-            </div>
-            <span className="flex__fluid-content">200 Points</span>
-          </li>
-          {enabledCashbackOrStoreCredits && (
-            <li className="flex flex-middle">
-              <div className="rewards-card__item-icon flex__shrink-fixed">
-                <ObjectFitImage noCompression src={RewardsCashbackIcon} alt="StoreHub Rewards Icon" />
-                <ObjectFitImage noCompression src={RewardsStoreCreditsIcon} alt="StoreHub Rewards Icon" />
-              </div>
-              <span className="flex__fluid-content">RM 1.23 Cashback</span>
-            </li>
+        <div className="flex flex-middle">
+          <ul className="flex__fluid-content">
+            {shouldShowRewardsItem && (
+              <li className="flex flex-middle padding-left-right-small margin-top-bottom-small">
+                <div className="rewards-card__item-icon flex__shrink-fixed">
+                  <ObjectFitImage noCompression src={RewardsIcon} alt="StoreHub Rewards Icon" />
+                </div>
+                <span className="rewards-card__item-text flex__fluid-content padding-left-right-small">
+                  {t('RewardsCardRewardsItemText', { availableCount: rewardsAvailableCount })}
+                </span>
+              </li>
+            )}
+
+            {shouldShowPointsItem && (
+              <li className="flex flex-middle padding-left-right-small margin-top-bottom-small">
+                <div className="rewards-card__item-icon flex__shrink-fixed">
+                  <ObjectFitImage noCompression src={RewardsPointsIcon} alt="StoreHub Points Icon" />
+                </div>
+                <span className="rewards-card__item-text flex__fluid-content padding-left-right-small">
+                  {t('RewardsCardPointsItemText', { points })}
+                </span>
+              </li>
+            )}
+
+            {enabledCashbackOrStoreCredits && (
+              <li className="flex flex-middle padding-left-right-small margin-top-bottom-small">
+                <div className="rewards-card__item-icon flex__shrink-fixed">
+                  <ObjectFitImage
+                    noCompression
+                    src={enabledCashback ? RewardsCashbackIcon : RewardsStoreCreditsIcon}
+                    alt="StoreHub Rewards Cashback Icon"
+                  />
+                </div>
+                <span className="rewards-card__item-text flex__fluid-content padding-left-right-small">
+                  {t('RewardsCardCashbackItemText', {
+                    cashbackPrice: enabledCashback ? cashbackPrice : storeCreditsPrice,
+                  })}
+                </span>
+              </li>
+            )}
+          </ul>
+
+          {isNewMember && (
+            <CaretRight
+              className="rewards-card__caret-right-icon flex__shrink-fixed margin-left-right-small"
+              size={32}
+              weight="light"
+            />
           )}
-        </ul>
-        <CaretRight size={32} weight="light" />
+        </div>
       </section>
     </>
   );
@@ -86,12 +129,16 @@ Rewards.propTypes = {
   isNewMember: PropTypes.bool,
   enabledCashback: PropTypes.bool,
   enabledLoyalty: PropTypes.bool,
+  enabledMembershipPoints: PropTypes.bool,
+  rewardsAvailableCount: PropTypes.number,
 };
 
 Rewards.defaultProps = {
   isNewMember: false,
   enabledCashback: false,
   enabledLoyalty: false,
+  enabledMembershipPoints: false,
+  rewardsAvailableCount: 0,
 };
 
 export default Rewards;
