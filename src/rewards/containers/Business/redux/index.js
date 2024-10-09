064f63007834d35d3488918ed26d52a5d68d2beb
Wendy Lin <wendylw2013@gmail.com>
2024-10-09 15:49:20 +0800

    WB-9316: Clear Membership Detail Old version (#3825)

--- src/rewards/containers/Business/redux/index.js
+++ src/rewards/containers/Business/redux/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import common from './common';
 import membershipForm from '../containers/MembershipForm/redux';
-import membershipDetail from '../containers/MembershipDetail/redux';
 import membershipDetailV2 from '../containers/MembershipDetailV2/redux';
 import pointsHistory from '../containers/PointsHistory/redux';
 import cashbackCreditsHistory from '../containers/CashbackCreditsHistory/redux';
@@ -12,7 +11,6 @@ import pointsRewards from '../containers/PointsRewardsPage/redux';
 export default combineReducers({
   common,
   membershipForm,
-  membershipDetail,
   membershipDetailV2,
   pointsHistory,
   cashbackCreditsHistory,
