08ea0099de73bb46fb9c74f16f3f9eac65b9ea2e
wendylw <wendylw2013@gmail.com>
2024-10-10 19:25:44 +0800

    WB-9280: Remove console

--- src/ordering/containers/Menu/components/MenuFooter/MenuViewOrderBar.jsx
+++ src/ordering/containers/Menu/components/MenuFooter/MenuViewOrderBar.jsx
@@ -13,8 +13,6 @@ const MenuViewOrderBar = () => {
   // has on going order (only available on pay later)
   const orderingOngoingBannerVisibility = useSelector(getOrderingOngoingBannerVisibility);
 
-  console.log(orderingOngoingBannerVisibility);
-
   if (!orderingOngoingBannerVisibility) {
     return null;
   }
