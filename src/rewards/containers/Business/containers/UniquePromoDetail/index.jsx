652a07108f41f5cd2f1a9219c9b2996d76ab000b
wendylw <wendylw2013@gmail.com>
2024-11-04 18:33:06 +0800

    WB-7917: Rename my rewards to unique promo detail

---
+++ src/rewards/containers/Business/containers/UniquePromoDetail/index.jsx
@@ -0,0 +1,142 @@
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useMount } from 'react-use';
+import { Trans, useTranslation } from 'react-i18next';
+import { UNIQUE_PROMO_STATUS_I18KEYS } from '../../utils/constants';
+import { UNIQUE_PROMO_APPLIED_SOURCE_I18KEYS } from './utils/constants';
+import {
+  getUniquePromoFormatDiscountValue,
+  getUniquePromoPromotionName,
+  getUniquePromoLimitations,
+  getIsUniquePromoUnAvailable,
+  getUniquePromoStatus,
+  getUniquePromoExpiringDaysI18n,
+  getUniquePromoFormatAppliedProductsText,
+  getUniquePromoFormatAppliedStoresText,
+  getUniquePromoRedeemOnlineList,
+  getIsUniquePromoRedeemOnlineShow,
+  getIsUniquePromoRedeemInStoreShow,
+} from './redux/selectors';
+import { backButtonClicked, mounted } from './redux/thunks';
+import Frame from '../../../../../common/components/Frame';
+import PageHeader from '../../../../../common/components/PageHeader';
+import Tag from '../../../../../common/components/Tag';
+import styles from './UniquePromoDetail.module.scss';
+
+const UniquePromoDetail = () => {
+  const { t } = useTranslation(['Rewards']);
+  const dispatch = useDispatch();
+  const formatDiscountValue = useSelector(getUniquePromoFormatDiscountValue);
+  const name = useSelector(getUniquePromoPromotionName);
+  const limitations = useSelector(getUniquePromoLimitations);
+  const isUniquePromoUnAvailable = useSelector(getIsUniquePromoUnAvailable);
+  const status = useSelector(getUniquePromoStatus);
+  const expiringDaysI18n = useSelector(getUniquePromoExpiringDaysI18n);
+  const formatAppliedProductsText = useSelector(getUniquePromoFormatAppliedProductsText);
+  const formatAppliedStoresText = useSelector(getUniquePromoFormatAppliedStoresText);
+  const redeemOnlineList = useSelector(getUniquePromoRedeemOnlineList);
+  const isUniquePromoRedeemOnlineShow = useSelector(getIsUniquePromoRedeemOnlineShow);
+  const isUniquePromoRedeemInStoreShow = useSelector(getIsUniquePromoRedeemInStoreShow);
+  const handleClickHeaderBackButton = useCallback(() => dispatch(backButtonClicked()), [dispatch]);
+
+  useMount(() => {
+    dispatch(mounted());
+  });
+
+  return (
+    <Frame>
+      <PageHeader title={t('UniquePromoDetails')} onBackArrowClick={handleClickHeaderBackButton} />
+      <section className={styles.UniquePromoDetailTicket}>
+        <div className={styles.UniquePromoDetailTicketMain}>
+          <data className={styles.UniquePromoDetailDiscountValue} value={formatDiscountValue}>
+            {t('DiscountValueText', { discount: formatDiscountValue })}
+          </data>
+          <h2 className={styles.UniquePromoDetailName}>{name}</h2>
+        </div>
+
+        <div className={styles.UniquePromoDetailTicketStub}>
+          <ul className={styles.UniquePromoDetailLimitations}>
+            {limitations.map(limitation => (
+              <li className={styles.UniquePromoDetailLimitation} key={limitation.key}>
+                {t(limitation.i18nKey, limitation.params)}
+              </li>
+            ))}
+          </ul>
+
+          {isUniquePromoUnAvailable ? (
+            <Tag className={styles.UniquePromoDetailTicketStatusTag}>{t(UNIQUE_PROMO_STATUS_I18KEYS[status])}</Tag>
+          ) : (
+            expiringDaysI18n && (
+              <Tag color="red" className={styles.UniquePromoStubRemainingExpiredDaysTag}>
+                <Trans
+                  t={t}
+                  i18nKey={expiringDaysI18n.i18nKey}
+                  values={expiringDaysI18n.params}
+                  components={[
+                    <span
+                      className={
+                        expiringDaysI18n.value === 1
+                          ? styles.UniquePromoStubRemainingExpiredDaysTagLetterHidden
+                          : styles.UniquePromoStubRemainingExpiredDaysTagLetter
+                      }
+                    />,
+                  ]}
+                />
+              </Tag>
+            )
+          )}
+        </div>
+      </section>
+
+      <section className={styles.UniquePromoDetailApplicableProducts}>
+        <h3 className={styles.UniquePromoDetailConditionTitle}>{t('UniquePromoApplicableProductsTitle')}</h3>
+        {formatAppliedProductsText && (
+          <p className={styles.UniquePromoDetailConditionContent}>{formatAppliedProductsText}</p>
+        )}
+      </section>
+
+      <section className={styles.UniquePromoDetailApplicableStores}>
+        <h3 className={styles.UniquePromoDetailConditionTitle}>{t('UniquePromoApplicableStoresTitle')}</h3>
+        {formatAppliedStoresText && (
+          <p className={styles.UniquePromoDetailConditionContent}>{formatAppliedStoresText}</p>
+        )}
+      </section>
+
+      <section className={styles.UniquePromoDetailHowToUse}>
+        <h3 className={styles.UniquePromoDetailHowToUseTitle}>{t('HowToUse')}</h3>
+
+        {isUniquePromoRedeemOnlineShow && (
+          <div>
+            <h4 className={styles.UniquePromoDetailHowToUseSubtitle}>{t('UniquePromoRedeemOnlineTitle')}</h4>
+            <p className={styles.UniquePromoDetailHowToUseContentDescription}>
+              {t('UniquePromoRedeemOnlineDescription')}
+            </p>
+            <ul className={styles.UniquePromoDetailHowToUseRedeemOnlineList}>
+              {redeemOnlineList.map(redeemOnlineChannel => (
+                <li
+                  key={`myRewardDetail-redeemOnlineChannel-${redeemOnlineChannel}`}
+                  className={styles.UniquePromoDetailHowToUseRedeemOnlineItem}
+                >
+                  {t(UNIQUE_PROMO_APPLIED_SOURCE_I18KEYS[redeemOnlineChannel])}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {isUniquePromoRedeemInStoreShow && (
+          <div>
+            <h4 className={styles.UniquePromoDetailHowToUseSubtitle}>{t('UniquePromoRedeemInStoreTitle')}</h4>
+            <p className={styles.UniquePromoDetailHowToUseContentDescription}>
+              {t('UniquePromoRedeemInStoreDescription')}
+            </p>
+          </div>
+        )}
+      </section>
+    </Frame>
+  );
+};
+
+UniquePromoDetail.displayName = 'UniquePromoDetail';
+
+export default UniquePromoDetail;
