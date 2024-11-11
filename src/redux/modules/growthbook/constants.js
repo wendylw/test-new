2c872c604c9acef4daf0b3a8c60be1aeb23e9734
wendylw <wendylw2013@gmail.com>
2024-11-11 19:21:57 +0800

    WB-8769: Remove 7625 feature flag

--- src/redux/modules/growthbook/constants.js
+++ src/redux/modules/growthbook/constants.js
@@ -1,6 +1,5 @@
 export const FEATURE_KEYS = {
   CLAIM_UNIQUE_PROMO: 'wb-6632_claim-unique-promo',
-  SHOW_TIERED_MEMBERSHIP_BENEFIT: 'wb-7625_show_tier_benefit_in_join_membership_and_details_page',
   JOIN_MEMBERSHIP_NEW_DESIGN: 'wb-7756_join-membership-new-design',
 };
 
@@ -9,6 +8,5 @@ export const DEFAULT_FEATURE_FLAG_RESULTS = {
     introURL: 'https://lp.storehub.com/claim-promo-url-centreplacecafe',
     congratsURL: 'https://lp.storehub.com/promotion-claimed',
   },
-  [FEATURE_KEYS.SHOW_TIERED_MEMBERSHIP_BENEFIT]: [],
   [FEATURE_KEYS.JOIN_MEMBERSHIP_NEW_DESIGN]: false,
 };
