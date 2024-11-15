5f42ac28ecc278aa30dc03c8e37bcb39be9e88e0
wendylw <wendylw2013@gmail.com>
2024-11-15 19:25:57 +0800

    WB-9319: Update ticket title UI

--- src/ordering/containers/rewards/containers/RewardList/components/TicketList/index.jsx
+++ src/ordering/containers/rewards/containers/RewardList/components/TicketList/index.jsx
@@ -8,37 +8,42 @@ const TicketList = () => {
   const { t } = useTranslation(['OrderingPromotion']);
 
   return (
-    <section>
-      <Ticket
-        className={styles.RewardTicketContainer}
-        ticketClassName={styles.RewardTicket}
-        orientation="vertical"
-        main={
-          <div className={styles.RewardTicketInfoTop}>
-            <div className={styles.RewardTicketDescription}>
-              <data className={styles.RewardTicketDiscount} value="100%">
-                {t('DiscountValueText', { discount: '100%' })}
-              </data>
-              <h5 className={styles.RewardTicketDiscountName}>test</h5>
-            </div>
-            {/* {isUnavailable ? (
+    <section className={styles.TicketListContainer}>
+      <h3 className={styles.TicketListTitle}>{t('YourVouchers')}</h3>
+      <ul className={styles.TicketList}>
+        <li>
+          <Ticket
+            className={styles.RewardTicketContainer}
+            ticketClassName={styles.RewardTicket}
+            orientation="vertical"
+            main={
+              <div className={styles.RewardTicketInfoTop}>
+                <div className={styles.RewardTicketDescription}>
+                  <data className={styles.RewardTicketDiscount} value="100%">
+                    {t('DiscountValueText', { discount: '100%' })}
+                  </data>
+                  <h5 className={styles.RewardTicketDiscountName}>test</h5>
+                </div>
+                {/* {isUnavailable ? (
               <Tag className={styles.RewardTicketStatusTag}>{t(UNIQUE_PROMO_STATUS_I18KEYS[status])}</Tag>
             ) : expiringDays ? (
               <Tag color="red">{t(expiringDays.i18nKey, expiringDays.params)}</Tag>
             ) : (
               <Tag className={styles.UniqueExpiringTag}>{t(expiringDate.i18nKey, expiringDate.params)}</Tag>
             )} */}
-          </div>
-        }
-        stub={
-          <div className={styles.RewardTicketInfoBottom}>
-            {/* <span className={styles.RewardTicketDiscountLimitation}>
+              </div>
+            }
+            stub={
+              <div className={styles.RewardTicketInfoBottom}>
+                {/* <span className={styles.RewardTicketDiscountLimitation}>
               {t(minSpend.i18nKey, minSpend.params)}
             </span> */}
-            <span className={styles.RewardTicketViewDetail}>{t('ViewDetails')}</span>
-          </div>
-        }
-      />
+                <span className={styles.RewardTicketViewDetail}>{t('ViewDetails')}</span>
+              </div>
+            }
+          />
+        </li>
+      </ul>
     </section>
   );
 };
